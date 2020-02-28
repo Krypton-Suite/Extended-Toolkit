b0be5206fd62668e35dd07dadbf26eccfb028659
Peter Wagner <peterwagner@live.co.uk>
2020-02-28 17:51:55 +0000

    * Files

---
+++ Source/Krypton Toolkit Suite Extended/Shared/Toolkit Core/Classes/Other/TypefaceHelper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Drawing;
+
+namespace Krypton.Toolkit.Extended.Core
+{
+    public static class TypefaceHelper
+    {
+        #region Methods
+        public static void InitialiseTypefaceFamilies(KryptonComboBox typefaceSelection)
+        {
+            foreach (FontFamily fontFamily in FontFamily.Families)
+            {
+                typefaceSelection.Items.Add(fontFamily.Name);
+            }
+        }
+
+        public static void InitialiseTypefaceStyles(KryptonListBox typefaceStyleSelection)
+        {
+            foreach (FontStyle style in Enum.GetValues(typeof(FontStyle)))
+            {
+                typefaceStyleSelection.Items.Add(style.ToString());
+            }
+        }
+
+        public static void UpdateTypefaceStyles()
+        {
+
+        }
+        #endregion
+    }
+}
