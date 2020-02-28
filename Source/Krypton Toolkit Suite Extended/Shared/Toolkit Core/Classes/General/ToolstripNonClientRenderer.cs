42dff71852d2c56b04f368d270fd7be661e25a36
Peter Wagner <peterwagner@live.co.uk>
2020-02-28 14:06:26 +0000

    * Files

--- Source/Krypton Toolkit Suite Extended/Shared/Toolkit Core/Classes/General/ToolstripNonClientRenderer.cs
+++ Source/Krypton Toolkit Suite Extended/Shared/Toolkit Core/Classes/General/ToolstripNonClientRenderer.cs
@@ -2,7 +2,7 @@
 using System.Drawing.Drawing2D;
 using System.Windows.Forms;
 
-namespace Krypton.Toolkit.Extended.Core
+namespace Krypton.Toolkit.Suite.Extended.Core
 {
     public class ToolstripNonClientRenderer : ToolStripSystemRenderer
     {
