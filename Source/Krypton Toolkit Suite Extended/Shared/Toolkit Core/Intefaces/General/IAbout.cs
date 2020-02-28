1f559a960c11eabddf76d9aad70e0c52992c53ec
Peter Wagner <peterwagner@live.co.uk>
2020-02-28 18:23:08 +0000

    * Files

---
+++ Source/Krypton Toolkit Suite Extended/Shared/Toolkit Core/Intefaces/General/IAbout.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Drawing;
+
+namespace Krypton.Toolkit.Extended.Core
+{
+    public interface IAboutStripped
+    {
+        string ApplicationName { get; set; }
+
+        Icon ApplicationIcon { get; set; }
+
+        Bitmap ApplicationIconImage { get; set; }
+
+        string AuthourName { get; set; }
+
+        Version ApplicationVersion { get; set; }
+
+        string InstallLocation { get; set; }
+    }
+}
