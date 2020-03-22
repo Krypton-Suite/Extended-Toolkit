439629db6cc993ff339546dba10a2c9e97eb199f
Peter Wagner <peterwagner@live.co.uk>
2020-03-22 19:31:13 +0000

    * Check-in

---
+++ Source/Krypton Toolkit Suite Extended/Libraries/Krypton Toolkit Suite Extended Base/Components/Calandar/Classes/CalendarTimeScaleUnit.cs
@@ -0,0 +1,18 @@
+﻿namespace Krypton.Toolkit.Extended.Base
+{
+    internal class CalendarTimeScaleUnit
+    {
+        private CalendarDay calendarDay;
+        private int i;
+        private int hourSum;
+        private int minSum;
+
+        public CalendarTimeScaleUnit(CalendarDay calendarDay, int i, int hourSum, int minSum)
+        {
+            this.calendarDay = calendarDay;
+            this.i = i;
+            this.hourSum = hourSum;
+            this.minSum = minSum;
+        }
+    }
+}
--- Source/Krypton Toolkit Suite Extended/Libraries/Krypton Toolkit Suite Extended Base/Enumerations/Enumerations.cs
+++ Source/Krypton Toolkit Suite Extended/Libraries/Krypton Toolkit Suite Extended Base/Enumerations/Enumerations.cs
@@ -74,6 +74,84 @@
         ALL = TOPLEFT | TOPRIGHT | BOTTOMLEFT | BOTTOMRIGHT
     }
 
+    /// <summary>
+    /// Enumerates the possible modes of the days visualization on the <see cref="KryptonCalendar"/>
+    /// </summary>
+    public enum CalendarDaysMode
+    {
+        /// <summary>
+        /// A short version of the day is visible without time scale.
+        /// </summary>
+        SHORT,
+
+        /// <summary>
+        /// The day is fully visible with time scale.
+        /// </summary>
+        EXPANDED
+    }
+
+    /// <summary>
+    /// Possible alignment for <see cref="CalendarItem"/> images
+    /// </summary>
+    public enum CalendarItemImageAlign
+    {
+        /// <summary>
+        /// Image is drawn at north of text
+        /// </summary>
+        NORTH,
+
+        /// <summary>
+        /// Image is drawn at south of text
+        /// </summary>
+        SOUTH,
+
+        /// <summary>
+        /// Image is drawn at east of text
+        /// </summary>
+        EAST,
+
+        /// <summary>
+        /// Image is drawn at west of text
+        /// </summary>
+        WEST,
+    }
+
+    /// <summary>
+    /// Enumerates possible timescales for <see cref="KryptonCalendar"/> control
+    /// </summary>
+    public enum CalendarTimeScale
+    {
+        /// <summary>
+        /// Makes calendar show intervals of 60 minutes
+        /// </summary>
+        SIXTYMINUTES = 60,
+
+        /// <summary>
+        /// Makes calendar show intervals of 30 minutes
+        /// </summary>
+        THIRTYMINUTES = 30,
+
+        /// <summary>
+        /// Makes calendar show intervals of 15 minutes
+        /// </summary>
+        FIFTEENMINUTES = 15,
+
+        /// <summary>
+        /// Makes calendar show intervals of 10 minutes
+        /// </summary>
+        TENMINUTES = 10,
+
+        /// <summary>
+        /// Makes calendar show intervals of 6 minutes
+        /// </summary>
+        SIXMINUTES = 6,
+
+        /// <summary>
+        /// Makes calendar show intervals of 5 minutes
+        /// </summary>
+        FIVEMINUTES = 5
+    }
+
     #region Scrollbars
     /// <summary>
     /// The scrollbar arrow button states.
