b31e13d0eb2153f6001e5781d52870708f3ac7e2
Peter Wagner <peterwagner@live.co.uk>
2020-04-05 09:17:32 +0100

    * TypefacePreviewControl

---
+++ Source/Krypton Toolkit Suite Extended/Libraries/Extended Dialogs/Controls/Typeface/TypefacePreviewControl.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Krypton.Toolkit.Extended.Dialogs
+{
+    public class TypefacePreviewControl : UserControl
+    {
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // TypefacePreviewControl
+            // 
+            this.Name = "TypefacePreviewControl";
+            this.Size = new System.Drawing.Size(401, 242);
+            this.ResumeLayout(false);
+
+        }
+    }
+}
