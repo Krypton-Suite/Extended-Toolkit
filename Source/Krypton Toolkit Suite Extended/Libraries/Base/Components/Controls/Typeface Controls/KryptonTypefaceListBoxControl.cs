814ba2d34feceafe3b871589ed1c33c74787f41d
Peter Wagner <peterwagner@live.co.uk>
2020-04-07 17:19:52 +0100

    Create KryptonTypefaceListBoxControl.cs

---
+++ Source/Krypton Toolkit Suite Extended/Libraries/Base/Components/Controls/Typeface Controls/KryptonTypefaceListBoxControl.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Krypton.Toolkit.Extended.Base
+{
+    public class KryptonTypefaceListBoxControl : UserControl
+    {
+    }
+}
