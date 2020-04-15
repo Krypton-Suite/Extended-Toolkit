0bb56f3a57bd6f16dafb1d2d73601f36a20ef63e
Peter Wagner <peterwagner@live.co.uk>
2020-04-15 16:08:35 +0100

    Update ToggleSwitch.cs

--- Source/Krypton Toolkit Suite Extended/Libraries/Base/Components/Controls/Toggle Controls/ToggleSwitch.cs
+++ Source/Krypton Toolkit Suite Extended/Libraries/Base/Components/Controls/Toggle Controls/ToggleSwitch.cs
@@ -8,6 +8,52 @@ namespace Krypton.Toolkit.Extended.Base
     [DefaultValue("Checked"), DefaultEvent("CheckedChanged"), ToolboxBitmap(typeof(CheckBox))]
     public class ToggleSwitch : Control
     {
-        
+        #region Event
+        public delegate void CheckedChangedEventHandler(object sender, EventArgs e);
+
+        [Description("Raised when the ToggleSwitch has changed state")]
+        public event CheckedChangedEventHandler CheckedChanged;
+
+        protected override void OnCheckChanged(object sender, EventArgs e) => CheckedChanged?.Invoke(sender, e);
+
+        public delegate void RendererChangedEventHandler(object sender, RendererChangedEventArgs e);
+
+        [Description("Raised when the ToggleSwitch renderer is changed")]
+        public event RendererChangedEventHandler RendererChanged;
+
+        protected override void OnRendererChanged(object sender, RendererChangedEventArgs e) => RendererChanged?.Invoke(sender, e);
+        #endregion
+
+        #region Enumerations
+        public enum ToggleSwitchStyle
+        {
+            ANDROID,
+            BRUSHEDMETAL,
+            CARBON,
+            FANCY,
+            IOS,
+            IPHONE,
+            OSX,
+            METRO,
+            MODERN,
+            PLAINANDSIMPLE,
+            KRYPTONBRUSHMETAL,
+            KRYPTONSTANDARD
+        }            
+
+        public enum ToggleSwitchAlignment
+        {
+            NEAR,
+            CENTRE,
+            FAR
+        }
+
+        public enum ToggleSwitchButtonAlignment
+        {
+            LEFT,
+            CENTRE,
+            RIGHT
+        }
+        #endregion
     }
 }
