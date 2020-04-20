5585db42ec7ddb412eea2dd93a14c081d508aa8d
Peter Wagner <peterwagner@live.co.uk>
2020-04-20 17:27:28 +0100

    Update Enumerations.cs

--- Source/Krypton Toolkit Suite Extended/Shared/Common/Enumerations/Enumerations.cs
+++ Source/Krypton Toolkit Suite Extended/Shared/Common/Enumerations/Enumerations.cs
@@ -10,9 +10,7 @@
         RACEINTEGRITYPRIMITIVESEVALUATIONMESSAGEDIGEST = 5
     }
 
-    /// <summary>
-    /// The icon type.
-    /// </summary>
+    /// <summary>The icon type.</summary>
     public enum IconType
     {
         /// <summary>
@@ -45,5 +43,20 @@
         RTM,
         CURRENT,
         EOL
+    }
+
+    /// <summary>The dialog result for use with the KryptonMemoryBox.</summary>
+    public enum KryptonMemoryBoxDialogResult
+    {
+        /// <summary>A yes dialog result</summary>
+        YES = 0,
+		/// <summary>A yes to all dialog result</summary>
+        YESTOALL = 1,
+		/// <summary>A no dialog result</summary>
+        NO = 2,
+		/// <summary>A no to all dialog result</summary>
+        NOTOALL = 3,
+		/// <summary>A cancel dialog result</summary>
+        CANCEL = 4
     }
 }
