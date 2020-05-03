ba56751da107e5ed684c7c6d3733210c21eb4e4d
Peter Wagner <peterwagner@live.co.uk>
2020-05-03 11:36:48 +0100

    Update KryptonListView.cs

--- Source/Krypton Toolkit Suite Extended/Libraries/Base/Components/List View/KryptonListView.cs
+++ Source/Krypton Toolkit Suite Extended/Libraries/Base/Components/List View/KryptonListView.cs
@@ -1,8 +1,10 @@
-﻿using System.Windows.Forms;
+﻿using System.ComponentModel;
+using System.Drawing;
+using System.Windows.Forms;
 
 namespace Krypton.Toolkit.Extended.Base
 {
-    [ToolboxBitmap(typeof(ListView))]
+    [ToolboxBitmap(typeof(ListView)), ToolboxItem(true)]
     public class KryptonListView : ListView
     {
     }
