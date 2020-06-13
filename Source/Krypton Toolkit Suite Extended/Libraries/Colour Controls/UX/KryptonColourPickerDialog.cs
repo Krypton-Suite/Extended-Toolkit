0d5ad8fa3289054f86c0c86ad5f2466626071f7d
Peter Wagner <peterwagner@live.co.uk>
2020-06-13 19:16:57 +0100

    * Files

--- Source/Krypton Toolkit Suite Extended/Libraries/Colour Controls/UX/KryptonColourPickerDialog.cs
+++ Source/Krypton Toolkit Suite Extended/Libraries/Colour Controls/UX/KryptonColourPickerDialog.cs
@@ -8,5 +8,62 @@ namespace Krypton.Toolkit.Extended.Colour.Controls
 {
     public class KryptonColourPickerDialog : KryptonForm
     {
+        private KryptonPanel kryptonPanel1;
+        private System.Windows.Forms.Panel panel1;
+        private KryptonPanel kryptonPanel2;
+
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(KryptonColourPickerDialog));
+            this.kryptonPanel1 = new Krypton.Toolkit.KryptonPanel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.kryptonPanel2 = new Krypton.Toolkit.KryptonPanel();
+            ((System.ComponentModel.ISupportInitialize)(this.kryptonPanel1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.kryptonPanel2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // kryptonPanel1
+            // 
+            this.kryptonPanel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.kryptonPanel1.Location = new System.Drawing.Point(0, 527);
+            this.kryptonPanel1.Name = "kryptonPanel1";
+            this.kryptonPanel1.Size = new System.Drawing.Size(760, 45);
+            this.kryptonPanel1.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 524);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(760, 3);
+            this.panel1.TabIndex = 1;
+            // 
+            // kryptonPanel2
+            // 
+            this.kryptonPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.kryptonPanel2.Location = new System.Drawing.Point(0, 0);
+            this.kryptonPanel2.Name = "kryptonPanel2";
+            this.kryptonPanel2.Size = new System.Drawing.Size(760, 524);
+            this.kryptonPanel2.TabIndex = 2;
+            // 
+            // KryptonColourPickerDialog
+            // 
+            this.ClientSize = new System.Drawing.Size(760, 572);
+            this.Controls.Add(this.kryptonPanel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.kryptonPanel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "KryptonColourPickerDialog";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            ((System.ComponentModel.ISupportInitialize)(this.kryptonPanel1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.kryptonPanel2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
     }
 }
