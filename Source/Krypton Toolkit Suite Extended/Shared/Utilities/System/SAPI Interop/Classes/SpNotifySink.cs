8abae99f4c52ad1894d871a201725379c363c991
Peter Wagner <peterwagner@live.co.uk>
2020-07-08 15:58:29 +0100

    * Files

--- Source/Krypton Toolkit Suite Extended/Shared/Utilities/System/SAPI Interop/Classes/SpNotifySink.cs
+++ Source/Krypton Toolkit Suite Extended/Shared/Utilities/System/SAPI Interop/Classes/SpNotifySink.cs
@@ -1,12 +1,24 @@
-﻿namespace Krypton.Toolkit.Suite.Extended.Utilities.System.SAPIInterop
+﻿using System;
+using System.Threading;
+
+namespace Krypton.Toolkit.Suite.Extended.Utilities.System.SAPIInterop
 {
     internal class SpNotifySink : ISpNotifySink
     {
-        private EventNotify eventNotify;
+        private WeakReference _eventNotifyReference;
 
         public SpNotifySink(EventNotify eventNotify)
         {
-            this.eventNotify = eventNotify;
+            _eventNotifyReference = new WeakReference(eventNotify);
+        }
+
+        void ISpNotifySink.Notify()
+        {
+            EventNotify eventNotify = (EventNotify)_eventNotifyReference.Target;
+            if (eventNotify != null)
+            {
+                ThreadPool.QueueUserWorkItem(eventNotify.SendNotification);
+            }
         }
     }
 }
--- Source/Krypton Toolkit Suite Extended/Shared/Utilities/System/SAPI Interop/Enumerations/Enumerations.cs
+++ Source/Krypton Toolkit Suite Extended/Shared/Utilities/System/SAPI Interop/Enumerations/Enumerations.cs
@@ -253,4 +253,22 @@ namespace Krypton.Toolkit.Suite.Extended.Utilities.System.SAPIInterop
         SPINTERFERENCE_TOOFAST,
         SPINTERFERENCE_TOOSLOW
     }
+
+    internal enum SPLOADOPTIONS
+    {
+        SPLO_STATIC,
+        SPLO_DYNAMIC
+    }
+
+    [Flags]
+    internal enum SPRECOEVENTFLAGS
+    {
+        SPREF_AutoPause = 0x1,
+        SPREF_Emulated = 0x2,
+        SPREF_SMLTimeout = 0x4,
+        SPREF_ExtendableParse = 0x8,
+        SPREF_ReSent = 0x10,
+        SPREF_Hypothesis = 0x20,
+        SPREF_FalseRecognition = 0x40
+    }
 }
