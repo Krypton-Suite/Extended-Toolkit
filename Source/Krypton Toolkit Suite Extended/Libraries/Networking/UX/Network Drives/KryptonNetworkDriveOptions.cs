d1b01c2afa4a590a28fee58bff9687d3cc815471
Peter Wagner <peterwagner@live.co.uk>
2020-08-11 18:52:05 +0100

    * Files

---
+++ Source/Krypton Toolkit Suite Extended/Libraries/Networking/UX/Network Drives/KryptonNetworkDriveOptions.cs
@@ -0,0 +1,17 @@
+﻿namespace Krypton.Toolkit.Suite.Extended.Networking
+{
+    public class KryptonNetworkDriveOptions : KryptonForm
+    {
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // KryptonNetworkDriveOptions
+            // 
+            this.ClientSize = new System.Drawing.Size(670, 364);
+            this.Name = "KryptonNetworkDriveOptions";
+            this.ResumeLayout(false);
+
+        }
+    }
+}
