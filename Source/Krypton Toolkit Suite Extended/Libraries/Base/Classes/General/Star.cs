b3cd5edfef0bf91eb175a48e81e415996b215e15
Peter Wagner <peterwagner@live.co.uk>
2020-08-16 18:18:51 +0100

    * Files

---
+++ Source/Krypton Toolkit Suite Extended/Libraries/Base/Classes/General/Star.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Krypton.Toolkit.Suite.Extended.Base
+{
+    [ToolboxItem(false)]
+    public class Star : PictureBox
+    {
+        private Container _components;
+        public int StarNumber { get; private set; }
+
+        private enum States { Off, On }
+
+        private readonly Dictionary<States, Image> _stars;
+
+        private readonly Action<int> _clicked;
+        private readonly Action<int> _mouseEnter;
+        private readonly Action<int> _mouseLeave;
+
+        internal Star(int starNumber, Image offImage, Image onImage, Action<int> clicked, Action<int> mouseEnter, Action<int> mouseLeave)
+        {
+            Size = new Size(offImage.Width, offImage.Height);
+            Left = (Width * (starNumber - 1));
+
+            _stars = new Dictionary<States, Image>
+                        {
+                            {States.On, onImage},
+                            {States.Off, offImage}
+                        };
+            StarNumber = starNumber;
+            Image = _stars[States.Off];
+
+            _clicked = clicked;
+            _mouseEnter = mouseEnter;
+            _mouseLeave = mouseLeave;
+
+
+            InitializeComponent();
+        }
+
+        public void ToggleStar(int starNumber)
+        {
+            Image = StarNumber > starNumber ? _stars[States.Off] : _stars[States.On];
+        }
+
+        protected override void OnMouseEnter(EventArgs e)
+        {
+            _mouseEnter(StarNumber);
+        }
+
+        protected override void OnMouseLeave(EventArgs e)
+        {
+            _mouseLeave(StarNumber);
+        }
+
+        protected override void OnClick(EventArgs e)
+        {
+            _clicked(StarNumber);
+        }
+
+        #region Component Designer generated code
+
+        private void InitializeComponent()
+        {
+            _components = new Container();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                if (_components != null)
+                {
+                    _components.Dispose();
+                }
+            }
+
+            base.Dispose(disposing);
+        }
+        #endregion
+    }
+}
