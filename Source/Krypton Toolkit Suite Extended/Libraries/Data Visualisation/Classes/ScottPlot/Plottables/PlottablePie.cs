2e4d3d8041cec420a51c2bc3df48310905b0eca4
Peter Wagner <peterwagner@live.co.uk>
2020-09-02 19:03:39 +0100

    * Files

---
+++ Source/Krypton Toolkit Suite Extended/Libraries/Data Visualisation/Classes/ScottPlot/Plottables/PlottablePie.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using ScottPlot.Config;
+
+namespace Krypton.Toolkit.Suite.Extended.Data.Visualisation
+{
+    public class PlottablePie : Plottable
+    {
+        public double[] values;
+        public string label;
+        public string[] groupNames;
+        public Color[] colors;
+        bool explodedChart;
+        bool showValues;
+        bool showPercentages;
+        bool showLabels;
+
+        private SolidBrush brush = new SolidBrush(Color.Black);
+        private Pen pen = new Pen(Color.Black);
+
+        public PlottablePie(double[] values, string[] groupNames, Color[] colors, bool explodedChart, bool showValues, bool showPercentages, bool showLabels, string label)
+        {
+            this.values = values;
+            this.label = label;
+            this.groupNames = groupNames;
+            this.colors = colors;
+            this.explodedChart = explodedChart;
+            this.showValues = showValues;
+            this.showPercentages = showPercentages;
+            this.showLabels = (groupNames is null) ? false : showLabels;
+        }
+
+        public override LegendItem[] GetLegendItems()
+        {
+            if (groupNames is null)
+                return null;
+
+            var items = new LegendItem[values.Length];
+            for (int i = 0; i < values.Length; i++)
+            {
+                items[i] = new LegendItem(groupNames[i], colors[i], lineWidth: 10);
+            }
+            return items;
+        }
+
+        public override AxisLimits2D GetLimits()
+        {
+            return new AxisLimits2D(-0.5, 0.5, -1, 1);
+        }
+
+        public override int GetPointCount()
+        {
+            return values.Length;
+        }
+
+        public override void Render(Settings settings)
+        {
+            double[] proportions = values.Select(x => x / values.Sum()).ToArray();
+
+            int outlineWidth = 1;
+            int sliceOutlineWidth = 0;
+            if (explodedChart)
+            {
+                pen.Color = settings.DataBackground.Color; // TODO: will fail if data background is transparent
+                outlineWidth = 20;
+                sliceOutlineWidth = 1;
+            }
+
+            AxisLimits2D limits = GetLimits();
+            double centreX = limits.xCenter;
+            double centreY = limits.yCenter;
+            float diameterPixels = .9f * Math.Min(settings.dataSize.Width, settings.dataSize.Height);
+            string fontName = Config.Fonts.GetSansFontName();
+            float fontSize = 12;
+
+            // record label details and draw them after slices to prevent cover-ups
+            double[] labelXs = new double[values.Length];
+            double[] labelYs = new double[values.Length];
+            string[] labelStrings = new string[values.Length];
+
+            RectangleF boundingRectangle = new RectangleF((float)settings.GetPixelX(centreX) - diameterPixels / 2, (float)settings.GetPixelY(centreY) - diameterPixels / 2, diameterPixels, diameterPixels);
+
+            double start = -90;
+            for (int i = 0; i < values.Length; i++)
+            {
+                // determine where the slice is to be drawn
+                double sweep = proportions[i] * 360;
+                double sweepOffset = explodedChart ? -1 : 0;
+                double angle = (Math.PI / 180) * ((sweep + 2 * start) / 2);
+                double xOffset = explodedChart ? 3 * Math.Cos(angle) : 0;
+                double yOffset = explodedChart ? 3 * Math.Sin(angle) : 0;
+
+                // record where and what to label the slice
+                double sliceLabelR = 0.35 * diameterPixels;
+                labelXs[i] = (boundingRectangle.X + diameterPixels / 2 + xOffset + Math.Cos(angle) * sliceLabelR);
+                labelYs[i] = (boundingRectangle.Y + diameterPixels / 2 + yOffset + Math.Sin(angle) * sliceLabelR);
+                string sliceLabelValue = (showValues) ? $"{values[i]}" : "";
+                string sliceLabelPercentage = (showPercentages) ? $"{proportions[i] * 100:f1}%" : "";
+                string sliceLabelName = (showLabels) ? groupNames[i] : "";
+                labelStrings[i] = $"{sliceLabelValue}\n{sliceLabelPercentage}\n{sliceLabelName}".Trim();
+
+                brush.Color = colors[i];
+                settings.gfxData.FillPie(brush, (int)(boundingRectangle.X + xOffset), (int)(boundingRectangle.Y + yOffset), boundingRectangle.Width, boundingRectangle.Height, (float)start, (float)(sweep + sweepOffset));
+
+                if (explodedChart)
+                {
+                    pen.Width = sliceOutlineWidth;
+                    settings.gfxData.DrawPie(pen, (int)(boundingRectangle.X + xOffset), (int)(boundingRectangle.Y + yOffset), boundingRectangle.Width, boundingRectangle.Height, (float)start, (float)(sweep + sweepOffset));
+                }
+                start += sweep;
+
+            }
+
+            brush.Color = Color.White;
+            var font = new Font(fontName, fontSize);
+            for (int i = 0; i < values.Length; i++)
+            {
+                if (!string.IsNullOrWhiteSpace(labelStrings[i]))
+                {
+                    settings.gfxData.DrawString(labelStrings[i], font, brush,
+                        (float)labelXs[i], (float)labelYs[i], settings.misc.sfCenterCenter);
+                }
+            }
+
+            pen.Width = outlineWidth;
+            settings.gfxData.DrawEllipse(pen, boundingRectangle.X, boundingRectangle.Y, boundingRectangle.Width, boundingRectangle.Height);
+        }
+
+        public override string ToString()
+        {
+            string label = string.IsNullOrWhiteSpace(this.label) ? "" : $" ({this.label})";
+            return $"PlottablePie{label} with {GetPointCount()} points";
+        }
+    }
+}
---
+++ Source/Krypton Toolkit Suite Extended/Libraries/Data Visualisation/Classes/ScottPlot/Plottables/PlottablePopulations.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ScottPlot.Config;
+using ScottPlot.Statistics;
+
+namespace Krypton.Toolkit.Suite.Extended.Data.Visualisation
+{
+    public class PlottablePopulations : Plottable
+    {
+        public PopulationMultiSeries popMultiSeries;
+        public int groupCount { get { return popMultiSeries.groupCount; } }
+        public int seriesCount { get { return popMultiSeries.seriesCount; } }
+        public string[] labels { get { return popMultiSeries.seriesLabels; } }
+
+        public enum DisplayItems { BoxOnly, BoxAndScatter, ScatterAndBox, ScatterOnly };
+        public enum BoxStyle { BarMeanStDev, BarMeanStdErr, BoxMeanStdevStderr, BoxMedianQuartileOutlier, MeanAndStdev, MeanAndStderr };
+
+        public bool displayDistributionCurve = true;
+        public LineStyle distributionCurveLineStyle = LineStyle.Solid;
+        public System.Drawing.Color distributionCurveColor = System.Drawing.Color.Black;
+        public System.Drawing.Color scatterOutlineColor = System.Drawing.Color.Black;
+        public DisplayItems displayItems = DisplayItems.BoxAndScatter;
+        public BoxStyle boxStyle = BoxStyle.BoxMedianQuartileOutlier;
+
+        public PlottablePopulations(PopulationMultiSeries groupedSeries)
+        {
+            this.popMultiSeries = groupedSeries;
+        }
+
+        public PlottablePopulations(Population[] populations, string label = null, System.Drawing.Color? color = null)
+        {
+            if (color is null)
+                color = System.Drawing.Color.LightGray;
+
+            var ps = new PopulationSeries(populations, label, color.Value);
+            popMultiSeries = new PopulationMultiSeries(new PopulationSeries[] { ps });
+        }
+
+        public PlottablePopulations(PopulationSeries populationSeries, string label = null, System.Drawing.Color? color = null)
+        {
+            if (color is null)
+                color = System.Drawing.Color.LightGray;
+
+            popMultiSeries = new PopulationMultiSeries(new PopulationSeries[] { populationSeries });
+        }
+
+        public PlottablePopulations(Population population, string label = null, System.Drawing.Color? color = null)
+        {
+            if (color is null)
+                color = System.Drawing.Color.LightGray;
+
+            var populations = new Population[] { population };
+            var ps = new PopulationSeries(populations, label, color.Value);
+            popMultiSeries = new PopulationMultiSeries(new PopulationSeries[] { ps });
+        }
+
+        public override string ToString()
+        {
+            return $"PlottableSeries with {popMultiSeries.groupCount} groups, {popMultiSeries.seriesCount} series, and {GetPointCount()} total points";
+        }
+
+        public override int GetPointCount()
+        {
+            int pointCount = 0;
+            foreach (var group in popMultiSeries.multiSeries)
+                foreach (var population in group.populations)
+                    pointCount += population.count;
+            return pointCount;
+        }
+
+        public override LegendItem[] GetLegendItems()
+        {
+            var items = new List<LegendItem>();
+            foreach (var series in popMultiSeries.multiSeries)
+                items.Add(new LegendItem(series.seriesLabel, series.color, lineWidth: 10));
+            return items.ToArray();
+        }
+
+        public override AxisLimits2D GetLimits()
+        {
+            double minValue = double.PositiveInfinity;
+            double maxValue = double.NegativeInfinity;
+
+            foreach (var series in popMultiSeries.multiSeries)
+            {
+                foreach (var population in series.populations)
+                {
+                    minValue = Math.Min(minValue, population.min);
+                    minValue = Math.Min(minValue, population.minus3stDev);
+                    maxValue = Math.Max(maxValue, population.max);
+                    maxValue = Math.Max(maxValue, population.plus3stDev);
+                }
+            }
+
+            double positionMin = 0;
+            double positionMax = popMultiSeries.groupCount - 1;
+
+            // padd slightly
+            positionMin -= .5;
+            positionMax += .5;
+
+            return new AxisLimits2D(positionMin, positionMax, minValue, maxValue);
+        }
+
+        public override void Render(Settings settings)
+        {
+            Random rand = new Random(0);
+            double groupWidth = .8;
+            var popWidth = groupWidth / seriesCount;
+
+            for (int seriesIndex = 0; seriesIndex < seriesCount; seriesIndex++)
+            {
+                for (int groupIndex = 0; groupIndex < groupCount; groupIndex++)
+                {
+                    var series = popMultiSeries.multiSeries[seriesIndex];
+                    var population = series.populations[groupIndex];
+                    var groupLeft = groupIndex - groupWidth / 2;
+                    var popLeft = groupLeft + popWidth * seriesIndex;
+
+                    RenderPopulation.Position scatterPos, boxPos;
+                    switch (displayItems)
+                    {
+                        case DisplayItems.BoxAndScatter:
+                            boxPos = RenderPopulation.Position.Left;
+                            scatterPos = RenderPopulation.Position.Right;
+                            break;
+                        case DisplayItems.BoxOnly:
+                            boxPos = RenderPopulation.Position.Center;
+                            scatterPos = RenderPopulation.Position.Hide;
+                            break;
+                        case DisplayItems.ScatterAndBox:
+                            boxPos = RenderPopulation.Position.Right;
+                            scatterPos = RenderPopulation.Position.Left;
+                            break;
+                        case DisplayItems.ScatterOnly:
+                            boxPos = RenderPopulation.Position.Hide;
+                            scatterPos = RenderPopulation.Position.Center;
+                            break;
+                        default:
+                            throw new NotImplementedException();
+                    }
+
+                    RenderPopulation.Scatter(settings, population, rand, popLeft, popWidth, series.color, scatterOutlineColor, 128, scatterPos);
+
+                    if (displayDistributionCurve)
+                        RenderPopulation.Distribution(settings, population, rand, popLeft, popWidth, distributionCurveColor, scatterPos, distributionCurveLineStyle);
+
+                    switch (boxStyle)
+                    {
+                        case BoxStyle.BarMeanStdErr:
+                            RenderPopulation.Bar(settings, population, rand, popLeft, popWidth, series.color, boxPos, useStdErr: true);
+                            break;
+                        case BoxStyle.BarMeanStDev:
+                            RenderPopulation.Bar(settings, population, rand, popLeft, popWidth, series.color, boxPos, useStdErr: false);
+                            break;
+                        case BoxStyle.BoxMeanStdevStderr:
+                            RenderPopulation.Box(settings, population, rand, popLeft, popWidth, series.color, boxPos, RenderPopulation.BoxFormat.StdevStderrMean);
+                            break;
+                        case BoxStyle.BoxMedianQuartileOutlier:
+                            RenderPopulation.Box(settings, population, rand, popLeft, popWidth, series.color, boxPos, RenderPopulation.BoxFormat.OutlierQuartileMedian);
+                            break;
+                        case BoxStyle.MeanAndStderr:
+                            RenderPopulation.MeanAndError(settings, population, rand, popLeft, popWidth, series.color, boxPos, useStdErr: true);
+                            break;
+                        case BoxStyle.MeanAndStdev:
+                            RenderPopulation.MeanAndError(settings, population, rand, popLeft, popWidth, series.color, boxPos, useStdErr: false);
+                            break;
+                        default:
+                            throw new NotImplementedException();
+                    }
+                }
+            }
+        }
+    }
+}
