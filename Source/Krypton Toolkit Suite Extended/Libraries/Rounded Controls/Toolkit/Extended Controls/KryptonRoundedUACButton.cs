1f8b07e9650e7a5263a0609113dbeec5d8cc49d5
Peter Wagner <peterwagner@live.co.uk>
2020-10-03 12:57:52 +0100

    * Files

---
+++ Source/Krypton Toolkit Suite Extended/Libraries/Rounded Controls/Toolkit/Extended Controls/KryptonRoundedUACButton.cs
@@ -0,0 +1,25 @@
+﻿namespace Krypton.Toolkit.Suite.Extended.Rounded.Controls
+{
+    public class KryptonRoundedUACButton : KryptonUACElevatedButton
+    {
+        #region Variables
+        private int _cornerRoundness;
+        #endregion
+
+        #region Properties
+        [DefaultValue(-1)]
+        public int CornerRoundness { get => _cornerRoundness; set { _cornerRoundness = value; Invalidate(); } }
+        #endregion
+
+        #region Constructor
+        public KryptonRoundedUACButton()
+        {
+            CornerRoundness = -1;
+        }
+        #endregion
+
+        #region Overrides
+        //protected override 
+        #endregion
+    }
+}
---
+++ Source/Krypton Toolkit Suite Extended/Libraries/Rounded Controls/Toolkit/Standard Controls/KryptonRoundedMaskedTextBox.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel;
+
+namespace Krypton.Toolkit.Suite.Extended.Rounded.Controls
+{
+    public class KryptonRoundedMaskedTextBox : KryptonMaskedTextBox
+    {
+        #region Variables
+        private int _cornerRoundness;
+        #endregion
+
+        #region Properties
+        [DefaultValue(-1)]
+        public int CornerRoundness { get => _cornerRoundness; set { _cornerRoundness = value; Invalidate(); } }
+        #endregion
+
+        #region Constructor
+        public KryptonRoundedMaskedTextBox()
+        {
+            CornerRoundness = -1;
+        }
+        #endregion
+
+        #region Overrides
+        //protected override
+        #endregion
+    }
+}
