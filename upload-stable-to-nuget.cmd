441a48464716a01ef98d07f4d920357124d541b9
Peter Wagner <peterwagner@live.co.uk>
2020-10-10 09:57:17 +0100

    * Scripts

--- upload-stable-to-nuget.cmd
+++ upload-stable-to-nuget.cmd
@@ -21,7 +21,7 @@ if /I "%INPUT%"=="y" goto publish
 if /I "%INPUT%"=="n" goto no
 
 :publish
-dotnet nuget push Krypton.Toolkit.Suite.Extended.Base.5.550.<#BUILD#>.nupkg -k <#API-KEY#> -s https://api.nuget.org/v3/index.json
+
 
 dotnet nuget push Krypton.Toolkit.Suite.Extended.Colour.Controls.5.550.<#BUILD#>.nupkg -k <#API-KEY#> -s https://api.nuget.org/v3/index.json
 
@@ -29,21 +29,19 @@ dotnet nuget push Krypton.Toolkit.Suite.Extended.Common.5.550.<#BUILD#>.nupkg -k
 
 dotnet nuget push Krypton.Toolkit.Suite.Extended.Core.5.550.<#BUILD#>.nupkg -k <#API-KEY#> -s https://api.nuget.org/v3/index.json
 
-dotnet nuget push Krypton.Toolkit.Suite.Extended.Data.Visualisation.5.550.<#BUILD#> -k <#API-KEY#> -s https://api.nuget.org/v3/index.json
 
-dotnet nuget push Krypton.Toolkit.Suite.Extended.Dialogs.5.550.<#BUILD#>.nupkg -k <#API-KEY#> -s https://api.nuget.org/v3/index.json
 
-dotnet nuget push Krypton.Toolkit.Suite.Extended.Floating.Toolbars.5.550.<#BUILD#>.nupkg -k <#API-KEY#> -s https://api.nuget.org/v3/index.json
+
 
 dotnet nuget push Krypton.Toolkit.Suite.Extended.Global.Utilities.5.550.<#BUILD#>.nupkg -k <#API-KEY#> -s https://api.nuget.org/v3/index.json
 
-dotnet nuget push Krypton.Toolkit.Suite.Extended.IO.5.550.<#BUILD#>.nupkg -k <#API-KEY#> -s https://api.nuget.org/v3/index.json
+
 
 dotnet nuget push Krypton.Toolkit.Suite.Extended.Language.Model.5.550.<#BUILD#>.nupkg -k <#API-KEY#> -s https://api.nuget.org/v3/index.json
 
-dotnet nuget push Krypton.Toolkit.Suite.Extended.Navi.Suite.5.550.<#BUILD#>.nupkg -k <#API-KEY#> -s https://api.nuget.org/v3/index.json
 
-dotnet nuget push Krypton.Toolkit.Suite.Extended.Outlook.Grid.5.550.<#BUILD#>.nupkg -k <#API-KEY#> -s https://api.nuget.org/v3/index.json
+
+
 
 dotnet nuget push Krypton.Toolkit.Suite.Extended.Palette.Controller.5.550.<#BUILD#>.nupkg -k <#API-KEY#> -s https://api.nuget.org/v3/index.json
 
@@ -55,19 +53,15 @@ dotnet nuget push Krypton.Toolkit.Suite.Extended.Settings.5.550.<#BUILD#>.nupkg 
 
 dotnet nuget push Krypton.Toolkit.Suite.Extended.Software.Updater.5.550.<#BUILD#>.nupkg -k <#API-KEY#> -s https://api.nuget.org/v3/index.json
 
-dotnet nuget push Krypton.Toolkit.Suite.Extended.Standard.Controls.5.550.<#BUILD#>.nupkg -k <#API-KEY#> -s https://api.nuget.org/v3/index.json
 
-dotnet nuget push Krypton.Toolkit.Suite.Extended.Tool.Strip.Items.5.550.<#BUILD#>.nupkg -k <#API-KEY#> -s https://api.nuget.org/v3/index.json
+
+
 
 dotnet nuget push Krypton.Toolkit.Suite.Extended.Toolkit.Control.Settings.5.550.<#BUILD#>.nupkg -k <#API-KEY#> -s https://api.nuget.org/v3/index.json
 
 dotnet nuget push Krypton.Toolkit.Suite.Extended.Utilities.5.550.<#BUILD#>.nupkg -k <#API-KEY#> -s https://api.nuget.org/v3/index.json
 
-dotnet nuget push Krypton.Toolkit.Suite.Extended.Wizard.5.550.<#BUILD#>.nupkg -k <#API-KEY#> -s https://api.nuget.org/v3/index.json
 
-dotnet nuget push Krypton.Toolkit.Suite.Extended.Task.Dialogs.5.550.<#BUILD#>.nupkg -k <#API-KEY#> -s https://api.nuget.org/v3/index.json
 
-echo All NuGet packages have now been published!
 
-:no
-pause
+
