b2a329d0bee0308f27d4be30bd2103c366b0d532
Peter Wagner <peterwagner@live.co.uk>
2020-10-22 18:42:46 +0100

    Update KryptonAboutDialog.cs

--- Source/Krypton Toolkit Suite Extended/Libraries/Extended Dialogs/UX/About/KryptonAboutDialog.cs
+++ Source/Krypton Toolkit Suite Extended/Libraries/Extended Dialogs/UX/About/KryptonAboutDialog.cs
@@ -11,7 +11,12 @@ namespace Krypton.Toolkit.Suite.Extended.Dialogs
             // KryptonAboutDialog
             // 
             this.ClientSize = new System.Drawing.Size(705, 367);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "KryptonAboutDialog";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
             this.Load += new System.EventHandler(this.KryptonAboutDialog_Load);
             this.ResumeLayout(false);
 
