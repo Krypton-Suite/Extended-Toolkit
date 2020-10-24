c0964d539d61a3005f8731b815524f36807a0ce4
Peter Wagner <peterwagner@live.co.uk>
2020-10-24 17:09:45 +0100

    * Files

---
+++ Source/Krypton Toolkit Suite Extended/Libraries/Extended Dialogs/UX/Exception Box/KryptonExceptionMessageBox.cs
@@ -0,0 +1,28 @@
+﻿using Krypton.Toolkit.Suite.Extended.Core;
+using System;
+
+namespace Krypton.Toolkit.Suite.Extended.Dialogs
+{
+    public class KryptonExceptionMessageBox : KryptonForm
+    {
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // KryptonExceptionMessageBox
+            // 
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Name = "KryptonExceptionMessageBox";
+            this.Load += new System.EventHandler(this.KryptonExceptionMessageBox_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        private void KryptonExceptionMessageBox_Load(object sender, EventArgs e)
+        {
+            GlobalUtilityMethods.NotImplementedYet();
+
+            Close();
+        }
+    }
+}
