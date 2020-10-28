1769b59e6e29eec3a7eb981c8923864fff6e36e1
Peter Wagner <peterwagner@live.co.uk>
2020-10-28 19:22:01 +0000

    * File changes

--- Source/Krypton Toolkit Suite Extended/Libraries/Error Reporting/UX/KryptonFullReportView.cs
+++ Source/Krypton Toolkit Suite Extended/Libraries/Error Reporting/UX/KryptonFullReportView.cs
@@ -1,8 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Krypton.Toolkit.Suite.Extended.Error.Reporting
 {
-    public class KryptonFullReportView : KryptonForm
+    public class KryptonFullReportView : KryptonForm, IExceptionReportView
     {
         #region Design Code
         private System.Windows.Forms.Panel panel1;
@@ -430,5 +431,64 @@ namespace Krypton.Toolkit.Suite.Extended.Error.Reporting
         {
 
         }
+
+        #region IExceptionReportView Implementation
+        public string ProgressMessage { set => throw new NotImplementedException(); }
+        public bool EnableEmailButton { set => throw new NotImplementedException(); }
+        public bool ShowProgressBar { set => throw new NotImplementedException(); }
+        public bool ShowFullDetail { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public string UserExplanation => throw new NotImplementedException();
+
+        public void Completed(bool success)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void MapiSendCompleted()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void PopulateAssembliesTab()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void PopulateExceptionTab(IEnumerable<Exception> exceptions)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void PopulateSysInfoTab()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetInProgressState()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetProgressCompleteState()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ShowError(string message, Exception exception)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ShowWindow()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ToggleShowFullDetail()
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
     }
 }
