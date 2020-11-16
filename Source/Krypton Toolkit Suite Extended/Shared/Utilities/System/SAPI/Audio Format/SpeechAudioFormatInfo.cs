b138b77040bb124eec197cf3b79b42871f3daf80
Peter Wagner <peterwagner@live.co.uk>
2020-11-16 18:01:21 +0000

    * Files

--- Source/Krypton Toolkit Suite Extended/Shared/Utilities/System/SAPI/Audio Format/SpeechAudioFormatInfo.cs
+++ Source/Krypton Toolkit Suite Extended/Shared/Utilities/System/SAPI/Audio Format/SpeechAudioFormatInfo.cs
@@ -1,12 +1,4 @@
-﻿#region BSD License
-/*
- * Use of this source code is governed by a BSD-style
- * license or other governing licenses that can be found in the LICENSE.md file or at
- * https://raw.githubusercontent.com/Krypton-Suite/Extended-Toolkit/master/LICENSE
- */
-#endregion
-
-using Krypton.Toolkit.Suite.Extended.Utilities.System.Synthesis;
+﻿using Krypton.Toolkit.Suite.Extended.Utilities.System.Synthesis;
 using Krypton.Toolkit.Suite.Extended.Utilities.SystemInternal.Speech;
 using System;
 using System.ComponentModel;
