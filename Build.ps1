c1fd1871d5bfcaef45e61f4008a390a89892cfef
Peter Wagner <peterwagner@live.co.uk>
2020-11-28 10:19:51 +0000

    Create Build.ps1

---
+++ Build.ps1
@@ -0,0 +1,31 @@
+function buildVS
+{
+    param
+    (
+        [parameter(Mandatory=$true)]
+        [String] $path,
+
+        [parameter(Mandatory=$false)]
+        [bool] $nuget = $true,
+        
+        [parameter(Mandatory=$false)]
+        [bool] $clean = $true
+    )
+    process
+    {
+        $msBuildExe = 'C:\Program Files (x86)\MSBuild\15.0\Bin\msbuild.exe'
+
+        if ($nuget) {
+            Write-Host "Restoring NuGet packages" -foregroundcolor green
+            nuget restore "$($path)"
+        }
+
+        if ($clean) {
+            Write-Host "Cleaning $($path)" -foregroundcolor green
+            & "$($msBuildExe)" "$($path)" /t:Clean /m
+        }
+
+        Write-Host "Building $($path)" -foregroundcolor green
+        & "$($msBuildExe)" "$($path)" /t:Build /m
+    }
+}
