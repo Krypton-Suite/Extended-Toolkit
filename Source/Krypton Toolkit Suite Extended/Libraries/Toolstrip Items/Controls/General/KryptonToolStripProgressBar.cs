d6ef7f449a56d0be8923af230d655700dd5db478
Peter Wagner <peterwagner@live.co.uk>
2020-12-18 11:22:12 +0000

    Create KryptonToolStripProgressBar.cs

---
+++ Source/Krypton Toolkit Suite Extended/Libraries/Toolstrip Items/Controls/General/KryptonToolStripProgressBar.cs
@@ -0,0 +1,35 @@
+﻿#region BSD License
+/*
+ * Use of this source code is governed by a BSD-style
+ * license or other governing licenses that can be found in the LICENSE.md file or at
+ * https://raw.githubusercontent.com/Krypton-Suite/Extended-Toolkit/master/LICENSE
+ */
+#endregion
+
+using System;
+using System.Drawing;
+using System.ComponentModel;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Krypton.Toolkit.Suite.Extended.Tool.Strip.Items
+{
+    [ToolStripItemDesignerAvailability(ToolStripItemDesignerAvailability.All)]
+    public class KryptonToolStripProgressBar : ToolStripProgressBar
+    {
+        #region Constructor
+        public KryptonToolStripProgressBar()
+        {
+
+        }
+        #endregion
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+        }
+    }
+}
