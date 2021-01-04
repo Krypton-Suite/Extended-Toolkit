773563086d7ec8b1966471272f6b9a6cf57b2428
Peter Wagner <peterwagner@live.co.uk>
2021-01-04 18:47:36 +0000

    Create FileDialogHandler.cs

---
+++ Source/Krypton Toolkit Suite Extended/Shared/Toolkit Core/Classes/IO/FileDialogHandler.cs
@@ -0,0 +1,17 @@
+﻿namespace Krypton.Toolkit.Suite.Extended.Core
+{
+    public class FileDialogHandler
+    {
+        #region Constructor
+
+        #endregion
+
+        #region Methods
+
+        #endregion
+
+        #region Setters and Getters
+
+        #endregion
+    }
+}
