e6942a97782667faa7b763c887389f1c93199d71
Peter Wagner <peterwagner@live.co.uk>
2021-01-04 19:14:25 +0000

    Update FileDialogHandler.cs

--- Source/Krypton Toolkit Suite Extended/Shared/Toolkit Core/Classes/IO/FileDialogHandler.cs
+++ Source/Krypton Toolkit Suite Extended/Shared/Toolkit Core/Classes/IO/FileDialogHandler.cs
@@ -1,7 +1,17 @@
-﻿namespace Krypton.Toolkit.Suite.Extended.Core
+﻿using Microsoft.WindowsAPICodePack.Dialogs;
+
+namespace Krypton.Toolkit.Suite.Extended.Core
 {
     public class FileDialogHandler
     {
+        #region Variables
+        private CommonFileDialog _commonFileDialog;
+
+        private CommonOpenFileDialog _openFileDialog;
+
+        private CommonSaveFileDialog _saveFileDialog;
+        #endregion
+
         #region Constructor
         public FileDialogHandler()
         {
@@ -10,11 +20,38 @@
         #endregion
 
         #region Methods
+        public static void CreateOpenFileDialog(string title, bool isFolderPicker, string initialDirectory, CommonFileDialogFilter filter)
+        {
+            CommonOpenFileDialog cofd = new CommonOpenFileDialog();
 
+            cofd.Title = title;
+
+            cofd.IsFolderPicker = isFolderPicker;
+
+            cofd.InitialDirectory = initialDirectory;
+
+            cofd.Filters.Add(filter);
+
+            FileDialogHandler fileDialogHandler = new FileDialogHandler();
+
+            fileDialogHandler.SetOpenFileDialog(cofd);
+
+            cofd.ShowDialog();
+        }
         #endregion
 
         #region Setters and Getters
+        /// <summary>
+        /// Sets the OpenFileDialog.
+        /// </summary>
+        /// <param name="value">The value.</param>
+        private void SetOpenFileDialog(CommonOpenFileDialog value) => _openFileDialog = value;
 
+        /// <summary>
+        /// Gets the OpenFileDialog.
+        /// </summary>
+        /// <returns>The value of _openFileDialog.</returns>
+        public CommonOpenFileDialog GetOpenFileDialog() => _openFileDialog;
         #endregion
     }
 }
