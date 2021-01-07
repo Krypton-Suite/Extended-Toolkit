7dfa25cd449ec415d8bc87e15990d223fce645b0
Peter Wagner <peterwagner@live.co.uk>
2021-01-07 18:43:42 +0000

    * Files

--- Source/Krypton Toolkit Suite Extended/Libraries/Base/Classes/IO/TreeItem.cs
+++ Source/Krypton Toolkit Suite Extended/Libraries/Base/Classes/IO/TreeItem.cs
@@ -6,12 +6,20 @@ namespace Krypton.Toolkit.Suite.Extended.Base
     public class TreeItem
     {
         #region Properties
+        /// <summary>Gets or sets the item data.</summary>
+        /// <value>The item data.</value>
         public string ItemData { get; set; }
 
+        /// <summary>Gets the parent item.</summary>
+        /// <value>The parent item.</value>
         public TreeItem ParentItem { get; private set; }
 
+        /// <summary>Gets the childs.</summary>
+        /// <value>The childs.</value>
         public List<TreeItem> Childs { get; }
 
+        /// <summary>Gets the count.</summary>
+        /// <value>The count.</value>
         public static int Count => _count;
         #endregion
 
