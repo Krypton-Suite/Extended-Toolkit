b3c4be3ac5de23b07eb78bc40594b70c9b67c265
Peter Wagner <peterwagner@live.co.uk>
2021-01-19 09:24:58 +0000

    * Files

--- Source/Krypton Toolkit Suite Extended/Libraries/Outlook Grid/Classes/General/OutlookGrid.cs
+++ Source/Krypton Toolkit Suite Extended/Libraries/Outlook Grid/Classes/General/OutlookGrid.cs
@@ -1,8 +1,11 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
-using System.Text;
+using System.Windows.Forms;
 using System.Xml;
 
 namespace Krypton.Toolkit.Suite.Extended.Outlook.Grid
