9ebd8521e62bb8edd072b44ded09cfc829ec7756
Peter Wagner <peterwagner@live.co.uk>
2021-01-19 19:03:11 +0000

    * Updates

--- Source/Krypton Toolkit Suite Extended/Libraries/Outlook Grid/Classes/General/OutlookGridGroupBoxColumn.cs
+++ Source/Krypton Toolkit Suite Extended/Libraries/Outlook Grid/Classes/General/OutlookGridGroupBoxColumn.cs
@@ -1,4 +1,15 @@
-﻿using System;
+﻿#region MS-PL License
+/*
+* Copyright (C) 2013 - 2018 JDH Software - <support@jdhsoftware.com>
+*
+* This program is provided to you under the terms of the Microsoft Public
+* License (Ms-PL) as published at https://kryptonoutlookgrid.codeplex.com/license
+*
+* Visit http://www.jdhsoftware.com and follow @jdhsoftware on Twitter
+*/
+#endregion
+
+using System;
 using System.Drawing;
 using System.Windows.Forms;
 
--- Source/Krypton Toolkit Suite Extended/Libraries/Outlook Grid/Enumerations/Enumerations.cs
+++ Source/Krypton Toolkit Suite Extended/Libraries/Outlook Grid/Enumerations/Enumerations.cs
@@ -1,4 +1,15 @@
-﻿namespace Krypton.Toolkit.Suite.Extended.Outlook.Grid
+﻿#region MS-PL License
+/*
+* Copyright (C) 2013 - 2018 JDH Software - <support@jdhsoftware.com>
+*
+* This program is provided to you under the terms of the Microsoft Public
+* License (Ms-PL) as published at https://kryptonoutlookgrid.codeplex.com/license
+*
+* Visit http://www.jdhsoftware.com and follow @jdhsoftware on Twitter
+*/
+#endregion
+
+namespace Krypton.Toolkit.Suite.Extended.Outlook.Grid
 {
     /// <summary>
     /// Conditional Formatting type
