5607cd3ac215eff50cc221774757d3958dcaae6d
Peter Wagner <peterwagner@live.co.uk>
2021-02-04 17:55:44 +0000

    * Files

--- Source/Krypton Toolkit Suite Extended/Libraries/Toolstrip Items/Controls/General/KryptonToolStrip.cs
+++ Source/Krypton Toolkit Suite Extended/Libraries/Toolstrip Items/Controls/General/KryptonToolStrip.cs
@@ -6,18 +6,20 @@
  */
 #endregion
 
+using System.ComponentModel;
+using System.Drawing;
+using System.Windows.Forms;
+
 namespace Krypton.Toolkit.Suite.Extended.Tool.Strip.Items
 {
-    internal sealed class NativeConstants
+    [ToolboxBitmap(typeof(ToolStrip)), Description("A standard tool strip equipped with the Krypton theme."), ToolboxItem(true)]
+    public class KryptonToolStrip : ToolStrip
     {
-        #region Constants
-        internal const uint WM_MOUSEACTIVATE = 0x21, MA_ACTIVATE = 1, MA_ACTIVATEANDEAT = 2, MA_NOACTIVATE = 3, MA_NOACTIVATEANDEAT = 4;
-        #endregion
-
         #region Constructor
-        public NativeConstants()
+        public KryptonToolStrip()
         {
-
+            // Use Krypton
+            RenderMode = ToolStripRenderMode.ManagerRenderMode;
         }
         #endregion
     }
