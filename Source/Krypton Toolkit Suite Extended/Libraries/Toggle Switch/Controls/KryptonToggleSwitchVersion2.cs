8711295f62153fe5675f86256563fafcff60c67e
Peter Wagner <peterwagner@live.co.uk>
2021-02-19 17:32:22 +0000

    * Edits

--- Source/Krypton Toolkit Suite Extended/Libraries/Toggle Switch/Controls/KryptonToggleSwitchVersion2.cs
+++ Source/Krypton Toolkit Suite Extended/Libraries/Toggle Switch/Controls/KryptonToggleSwitchVersion2.cs
@@ -1,4 +1,6 @@
 using System.ComponentModel;
+using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Windows.Forms;
 
 namespace Krypton.Toolkit.Suite.Extended.Toggle.Switch
@@ -11,29 +13,276 @@ namespace Krypton.Toolkit.Suite.Extended.Toggle.Switch
     public class KryptonToggleSwitchVersion2 : Control
     {
         #region Variables
-
+        private float _diameter, _artis;
+        private RoundedRectangleF _rect;
+        private RectangleF _circle;
+        private bool _isOn, _textEnabled, _useGradientOnKnob;
+        private Color _borderColour, _textEnabledForeColour, _textDisabledForeColour,
+                      _enabledBackColour, _disabledBackColour, _disabledControlColour,
+                      _knobColour, _penColour, _gradientStartColour,
+                      _gradientEndColour;
+        private LinearGradientMode _mode;
+        private string _onText, _offText;
         #endregion
 
         #region Properties
+        [Description("Shows or hides the slider text.")]
+        public bool TextEnabled
+        {
+            get => _textEnabled;
 
+            set
+            {
+                _textEnabled = value;
+                Invalidate();
+            }
+        }
+
+        public bool IsOn
+        {
+            get => _isOn;
+
+            set
+            {
+                _isOn = value;
+
+                SliderValueChangedEventArgs evt = new SliderValueChangedEventArgs(value);
+
+                OnSliderValueChanged(null, evt);
+            }
+        }
+
+        public bool UseGradientOnKnob
+        {
+            get => _useGradientOnKnob;
+
+            set
+            {
+                _useGradientOnKnob = value;
+
+                Invalidate();
+            }
+        }
+
+        public Color BorderColour
+        {
+            get => _borderColour;
+
+            set
+            {
+                _borderColour = value;
+
+                Invalidate();
+            }
+        }
+
+        public Color TextEnabledForeColour
+        {
+            get => _textEnabledForeColour;
+
+            set
+            {
+                _textEnabledForeColour = value;
+
+                Invalidate();
+            }
+        }
+
+        public Color TextDisabledForeColour
+        {
+            get => _textDisabledForeColour;
+
+            set
+            {
+                _textDisabledForeColour = value;
+
+                Invalidate();
+            }
+        }
+
+        public Color EnabledBackColour
+        {
+            get => _enabledBackColour;
+
+            set
+            {
+                _enabledBackColour = value;
+
+                Invalidate();
+            }
+        }
+
+        public Color DisabledBackColour
+        {
+            get => _disabledBackColour;
+
+            set
+            {
+                _disabledBackColour = value;
+
+                Invalidate();
+            }
+        }
+
+        public Color DisabledControlColour
+        {
+            get => _disabledControlColour;
+
+            set
+            {
+                _disabledControlColour = value;
+
+                Invalidate();
+            }
+        }
+
+        public Color KnobColour
+        {
+            get => _knobColour;
+
+            set
+            {
+                _knobColour = value;
+
+                Invalidate();
+            }
+        }
+
+        public Color PenColour
+        {
+            get => _penColour;
+
+            set
+            {
+                _penColour = value;
+
+                Invalidate();
+            }
+        }
+
+        public Color GradientStartColour
+        {
+            get => _gradientStartColour;
+
+            set
+            {
+                _gradientStartColour = value;
+
+                Invalidate();
+            }
+        }
+
+        public Color GradientEndColour
+        {
+            get => _gradientEndColour;
+
+            set
+            {
+                _gradientEndColour = value;
+
+                Invalidate();
+            }
+        }
+
+        public LinearGradientMode GradientMode
+        {
+            get => _mode;
+
+            set
+            {
+                _mode = value;
+
+                Invalidate();
+            }
+        }
+
+        public string OnText
+        {
+            get => _onText;
+
+            set
+            {
+                _onText = value;
+
+                Invalidate();
+            }
+        }
+
+        public string OffText
+        {
+            get => _offText;
+
+            set
+            {
+                _offText = value;
+
+                Invalidate();
+            }
+        }
         #endregion
 
         #region Custom Event
+        public delegate void SliderValueChangedEventHandler(object sender, SliderValueChangedEventArgs e);
 
+        public event SliderValueChangedEventHandler SliderValueChanged;
         #endregion
 
         #region Constructor
         public KryptonToggleSwitchVersion2()
         {
+            SetStyle(ControlStyles.SupportsTransparentBackColor, true);
 
+            Cursor = Cursors.Hand;
+            DoubleBuffered = true;
+
+            BackColor = Color.Transparent;
+
+            Font = new Font("Segoe UI", 9f);
+
+            _artis = 4; //increment for sliding animation
+
+            _diameter = 30;
+
+            TextEnabled = true;
+
+            _rect = new RoundedRectangleF(2 * _diameter, _diameter + 2, _diameter / 2, 1, 1);
+
+            _circle = new RectangleF(1, 1, _diameter, _diameter);
+
+            IsOn = true;
+
+            BorderColour = Color.LightGray;
+
+            UseGradientOnKnob = true;
+
+            TextEnabledForeColour = Color.Gray;
+
+            TextDisabledForeColour = Color.White;
+
+            EnabledBackColour = Color.LightGreen;
+
+            DisabledBackColour = Color.Red;
+
+            PenColour = Color.LightGray;
+
+            DisabledControlColour = Color.LightGray;
+
+            KnobColour = Color.White;
+
+            GradientStartColour = Color.FromArgb(187, 206, 230);
+
+            GradientEndColour = Color.FromArgb(132, 157, 189);
+
+            GradientMode = LinearGradientMode.ForwardDiagonal;
+
+            OnText = "On";
+
+            OffText = "Off";
         }
         #endregion
 
         #region Protected
-        protected void OnSliderValueChanged(object sender, SliderValueChangedEventArgs e)
-        {
-
-        }
+        protected void OnSliderValueChanged(object sender, SliderValueChangedEventArgs e) => SliderValueChanged?.Invoke(sender, e);
         #endregion
 
         #region Events
@@ -41,9 +290,48 @@ namespace Krypton.Toolkit.Suite.Extended.Toggle.Switch
         #endregion
 
         #region Methods
-        private void Animmate(bool toggleValue)          
+        private void Animate(bool toggleValue)          
         {
+            float x = _circle.X;
 
+            if (toggleValue)
+            {
+                if (x + _artis <= Width - _diameter - 1)
+                {
+                    x += _artis;
+
+                    _circle = new RectangleF(x, 1, _diameter, _diameter);
+
+                    Invalidate();
+                }
+                else
+                {
+                    x = Width - _diameter - 1;
+                    
+                    _circle = new RectangleF(x, 1, _diameter, _diameter);
+
+                    Invalidate();
+                }
+            }
+            else
+            {
+                if (x - _artis >= 1)
+                {
+                    x -= _artis;
+
+                    _circle = new RectangleF(x, 1, _diameter, _diameter);
+
+                    Invalidate();
+                }
+                else
+                {
+                    x = 1;
+
+                    _circle = new RectangleF(x, 1, _diameter, _diameter);
+
+                    Invalidate();
+                }
+            }
         }
         #endregion
 
@@ -54,8 +342,97 @@ namespace Krypton.Toolkit.Suite.Extended.Toggle.Switch
         #region Overrides
         protected override void OnPaint(PaintEventArgs e)
         {
+            e.Graphics.SmoothingMode = SmoothingMode.HighQuality;
+
+            if (Enabled)
+            {
+                using (SolidBrush brush = new SolidBrush(_isOn ? _enabledBackColour : _disabledBackColour))
+                {
+                    e.Graphics.FillPath(brush, _rect.Path);
+                }
+
+                using (Pen pen = new Pen(_borderColour, 2f))
+                {
+                    e.Graphics.DrawPath(pen, _rect.Path);
+                }
+
+                string onText = _onText, offText = _offText;
+
+                SolidBrush onBrushColour = new SolidBrush(_textEnabledForeColour), offBrushColour = new SolidBrush(_textDisabledForeColour);
+
+                if (_textEnabled)
+                {
+                    using (Font font = new Font(Font.FontFamily, Font.Size * _diameter / 30, Font.Style))
+                    {
+                        int height = TextRenderer.MeasureText(onText, font).Height;
+
+                        float y = (_diameter - height) / 2f;
+
+                        e.Graphics.DrawString(onText, font, onBrushColour, 5, y + 1);
+
+                        height = TextRenderer.MeasureText(offText, font).Height;
+
+                        y = (_diameter - height) / 2f;
+
+                        e.Graphics.DrawString(offText, font, offBrushColour, _diameter + 2, y + 1);
+                    }
+
+                    if (_useGradientOnKnob)
+                    {
+                        using (LinearGradientBrush lgb = new LinearGradientBrush(_circle, _gradientStartColour, _gradientEndColour, _mode))
+                        {
+                            e.Graphics.FillEllipse(lgb, _circle);
+                        }
+                    }
+                    else
+                    {
+                        using (SolidBrush circleBrush = new SolidBrush(_knobColour))
+                        {
+                            e.Graphics.FillEllipse(circleBrush, _circle);
+                        }
+                    }
+
+                    using (Pen pen = new Pen(_penColour, 1.2f))
+                    {
+                        e.Graphics.DrawEllipse(pen, _circle);
+                    }
+                }
+            }
+            else
+            {
+                using (SolidBrush disabledBrush = new SolidBrush(_disabledControlColour))
+                {
+                    using (SolidBrush ellBrush = new SolidBrush(_disabledBackColour))
+                    {
+                        e.Graphics.FillPath(disabledBrush, _rect.Path);
+
+                        e.Graphics.FillEllipse(ellBrush, _circle);
+
+                        e.Graphics.DrawEllipse(Pens.DarkGray, _circle);
+                    }
+                }
+            }
+
+            Animate(_isOn);
+
             base.OnPaint(e);
         }
+
+        protected override void OnMouseDown(MouseEventArgs e)
+        {
+            if (e.Button != MouseButtons.Left)
+            {
+                return;
+            }
+
+            _isOn = !_isOn;
+
+            IsOn = _isOn;
+
+            base.OnMouseDown(e);
+        }
+
+        protected override Size DefaultSize => new Size(60, 15);
         #endregion
     }
 }
