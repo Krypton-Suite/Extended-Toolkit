bb6b24a95d0bc84c4bd413d08c6402e0bf3abcdf
Peter Wagner <peterwagner@live.co.uk>
2021-02-23 18:05:49 +0000

    * Files

--- Source/Krypton Toolkit Suite Extended/Libraries/Task Dialogs/Enumerations/Enumerations.cs
+++ Source/Krypton Toolkit Suite Extended/Libraries/Task Dialogs/Enumerations/Enumerations.cs
@@ -78,4 +78,70 @@ namespace Krypton.Toolkit.Suite.Extended.Task.Dialogs
         /// </remarks>
         Help = 1 << 20
     }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public enum TaskDialogCustomButtonStyle
+    {
+        /// <summary>
+        /// Custom buttons should be displayed as normal buttons.
+        /// </summary>
+        Default = 0,
+
+        /// <summary>
+        /// Custom buttons should be displayed as command links.
+        /// </summary>
+        CommandLinks = 1,
+
+        /// <summary>
+        /// Custom buttons should be displayed as command links, but without an icon.
+        /// </summary>
+        CommandLinksNoIcon = 2
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public enum TaskDialogProgressBarState : int
+    {
+        /// <summary>
+        /// Shows a regular progress bar.
+        /// </summary>
+        Normal = 0,
+
+        /// <summary>
+        /// Shows a paused (yellow) progress bar.
+        /// </summary>
+        Paused,
+
+        /// <summary>
+        /// Shows an error (red) progress bar.
+        /// </summary>
+        Error,
+
+        /// <summary>
+        /// Shows a marquee progress bar.
+        /// </summary>
+        Marquee,
+
+        /// <summary>
+        /// Shows a marquee progress bar where the marquee animation is paused.
+        /// </summary>
+        /// <remarks>
+        /// For example, if you switch from <see cref="Marquee"/> to 
+        /// <see cref="MarqueePaused"/> while the dialog is shown, the 
+        /// marquee animation will stop.
+        /// </remarks>
+        MarqueePaused,
+
+        /// <summary>
+        /// The progress bar will not be displayed.
+        /// </summary>
+        /// <remarks>
+        /// Note that while the dialog is showing, you cannot switch from
+        /// <see cref="None"/> to any other state, and vice versa.
+        /// </remarks>
+        None
+    }
 }
