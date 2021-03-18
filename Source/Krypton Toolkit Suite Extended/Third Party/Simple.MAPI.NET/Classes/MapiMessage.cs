cf2f2ee03e482df63eb5b7590810771048439cf0
Peter Wagner <peterwagner@live.co.uk>
2021-03-18 15:39:35 +0000

    * Files

---
+++ Source/Krypton Toolkit Suite Extended/Third Party/Simple.MAPI.NET/Classes/MapiMessage.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Simple.MAPI.NET
+{
+    [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Ansi)]
+    public class MapiMessage
+    {
+        public int reserved;
+        public string subject;
+        public string noteText;
+        public string messageType;
+        public string dateReceived;
+        public string conversationID;
+        public int flags;
+        public IntPtr originator; // MapiRecipDesc* [1]
+        public int recipCount;
+        public IntPtr recips; // MapiRecipDesc* [n]
+        public int fileCount;
+        public IntPtr files; // MapiFileDesc*  [n]
+    }
+}
