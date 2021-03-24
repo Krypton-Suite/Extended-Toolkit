f27efbc524a58ecfc29c193574b9c2c0c58bc13e
Peter Wagner <peterwagner@live.co.uk>
2021-03-24 15:53:40 +0000

    Update KryptonEditorNavigator.cs

--- Source/Krypton Toolkit Suite Extended/Libraries/Navigator/Controls/KryptonEditorNavigator.cs
+++ Source/Krypton Toolkit Suite Extended/Libraries/Navigator/Controls/KryptonEditorNavigator.cs
@@ -4,15 +4,23 @@ using System.Windows.Forms;
 
 namespace Krypton.Toolkit.Suite.Extended.Navigator
 {
+    /// <summary>
+    ///   <br />
+    /// </summary>
     public class KryptonEditorNavigator : KryptonNavigator
     {
         #region Properties
+        /// <summary>Gets a value indicating whether this instance is rich text box multiline.</summary>
+        /// <value><c>true</c> if this instance is rich text box multiline; otherwise, <c>false</c>.</value>
         public bool IsRichTextBoxMultiline => ((KryptonRichTextBox)base.SelectedPage.Controls[0]).Multiline;
 
+        /// <summary>Gets the selected text.</summary>
+        /// <value>The selected text.</value>
         public string SelectedText => base.SelectedPage.Controls[0].Text;
         #endregion
 
         #region Constructor
+        /// <summary>Initializes a new instance of the <see cref="KryptonEditorNavigator" /> class.</summary>
         public KryptonEditorNavigator()
         {
 
@@ -20,8 +28,17 @@ namespace Krypton.Toolkit.Suite.Extended.Navigator
         #endregion
 
         #region Methods
+        /// <summary>Creates the krypton rich text box.</summary>
+        /// <returns>
+        ///   <br />
+        /// </returns>
         public KryptonRichTextBox CreateKryptonRichTextBox() => new KryptonRichTextBox() { Multiline = true, Dock = DockStyle.Fill };
 
+        /// <summary>Gets the krypton rich text box.</summary>
+        /// <param name="page">The page.</param>
+        /// <returns>
+        ///   <br />
+        /// </returns>
         public KryptonRichTextBox GetKryptonRichTextBox(KryptonPage page)
         {
             KryptonRichTextBox richTextBox = new KryptonRichTextBox();
@@ -40,6 +57,8 @@ namespace Krypton.Toolkit.Suite.Extended.Navigator
         #endregion
 
         #region Overrides
+        /// <summary>Raises the <see cref="E:System.Windows.Forms.Control.ControlAdded">ControlAdded</see> event.</summary>
+        /// <param name="e">A <see cref="T:System.Windows.Forms.ControlEventArgs">ControlEventArgs</see> that contains the event data.</param>
         protected override void OnControlAdded(ControlEventArgs e)
         {
             base.OnControlAdded(e);
@@ -47,6 +66,8 @@ namespace Krypton.Toolkit.Suite.Extended.Navigator
             e.Control.Controls.Add(this.CreateKryptonRichTextBox());
         }
 
+        /// <summary>Raises the <see cref="E:System.Windows.Forms.Control.TextChanged">TextChanged</see> event.</summary>
+        /// <param name="e">An <see cref="T:System.EventArgs">EventArgs</see> that contains the event data.</param>
         protected override void OnTextChanged(EventArgs e)
         {
             base.OnTextChanged(e);
