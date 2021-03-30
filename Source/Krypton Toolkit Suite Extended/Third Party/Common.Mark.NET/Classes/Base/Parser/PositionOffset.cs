70c0ec3fa7bff3d4eb6892993660f49190430011
Peter Wagner <peterwagner@live.co.uk>
2021-03-30 16:26:28 +0100

    * Projects

---
+++ Source/Krypton Toolkit Suite Extended/Third Party/Common.Mark.NET/Classes/Base/Parser/PositionOffset.cs
@@ -0,0 +1,24 @@
+﻿namespace Common.Mark.NET
+{
+    internal struct PositionOffset
+    {
+        public PositionOffset(int position, int offset)
+        {
+            this.Position = position;
+            this.Offset = offset;
+        }
+
+        public int Position;
+        public int Offset;
+
+        public override string ToString()
+        {
+            if (this.Offset == 0)
+                return "empty";
+
+            return (this.Offset < 0 ? string.Empty : "+")
+                + this.Offset.ToString(System.Globalization.CultureInfo.InvariantCulture)
+                + " chars @ " + this.Position.ToString(System.Globalization.CultureInfo.InvariantCulture);
+        }
+    }
+}
