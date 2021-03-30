93c3ae3b1bfe184ffe17d64a5777cccc4267dfba
Peter Wagner <peterwagner@live.co.uk>
2021-03-30 14:57:54 +0100

    * Files

---
+++ Source/Krypton Toolkit Suite Extended/Libraries/Messagebox/Classes/HelpInformation.cs
@@ -0,0 +1,82 @@
+﻿using System.Windows.Forms;
+
+namespace Krypton.Toolkit.Suite.Extended.Messagebox
+{
+    internal class HelpInformation
+    {
+        #region Properties
+        /// <summary>
+        /// Gets the HelpFilePath property.
+        /// </summary>
+        public string HelpFilePath { get; }
+
+        /// <summary>
+        /// Gets the Keyword property.
+        /// </summary>
+        public string Keyword { get; }
+
+        /// <summary>
+        /// Gets the Navigator property.
+        /// </summary>
+        public HelpNavigator Navigator { get; }
+
+        /// <summary>
+        /// Gets the Param property.
+        /// </summary>
+        public object Param { get; }
+        #endregion
+
+        #region Methods
+        /// <summary>
+        /// Initialize a new instance of the HelpInformation class.
+        /// </summary>
+        public HelpInformation()
+        {
+        }
+
+        /// <summary>
+        /// Initialize a new instance of the HelpInformation class.
+        /// </summary>
+        /// <param name="helpFilePath">Value for HelpFilePath.</param>
+        public HelpInformation(string helpFilePath)
+        {
+            HelpFilePath = helpFilePath;
+        }
+
+        /// <summary>
+        /// Initialize a new instance of the HelpInformation class.
+        /// </summary>
+        /// <param name="helpFilePath">Value for HelpFilePath.</param>
+        /// <param name="keyword">Value for Keyword</param>
+        public HelpInformation(string helpFilePath, string keyword)
+        {
+            HelpFilePath = helpFilePath;
+            Keyword = keyword;
+        }
+
+        /// <summary>
+        /// Initialize a new instance of the HelpInformation class.
+        /// </summary>
+        /// <param name="helpFilePath">Value for HelpFilePath.</param>
+        /// <param name="navigator">Value for Navigator</param>
+        public HelpInformation(string helpFilePath, HelpNavigator navigator)
+        {
+            HelpFilePath = helpFilePath;
+            Navigator = navigator;
+        }
+
+        /// <summary>
+        /// Initialize a new instance of the HelpInformation class.
+        /// </summary>
+        /// <param name="helpFilePath">Value for HelpFilePath.</param>
+        /// <param name="navigator">Value for Navigator</param>
+        /// <param name="param">Value for Param</param>
+        public HelpInformation(string helpFilePath, HelpNavigator navigator, object param)
+        {
+            HelpFilePath = helpFilePath;
+            Navigator = navigator;
+            Param = param;
+        }
+        #endregion
+    }
+}
--- Source/Krypton Toolkit Suite Extended/Libraries/Messagebox/Components/ExtendedKryptonMessageBox.cs
+++ Source/Krypton Toolkit Suite Extended/Libraries/Messagebox/Components/ExtendedKryptonMessageBox.cs
@@ -1,12 +1,24 @@
-﻿using System.ComponentModel;
+﻿using System;
+using System.ComponentModel;
 using System.Drawing;
+using System.Windows.Forms;
 
 namespace Krypton.Toolkit.Suite.Extended.Messagebox
 {
     [ToolboxItem(true), ToolboxBitmap(typeof(ExtendedKryptonMessageBox), "ToolboxBitmaps.KryptonMessageBox.bmp"),
-     DefaultEvent("Show"), DesignerCategory("code"), Designer(ExtendedKryptonMessageBoxDesigner),
+     DefaultEvent("Show"), DesignerCategory("code"), Designer(typeof(ExtendedKryptonMessageBoxDesigner)),
      Description("")]
     public class ExtendedKryptonMessageBox : Component
     {
+        #region Variables
+        private bool _showCtrlCopy;
+        private Font _messageboxTypeface;
+        private IWin32Window _owner;
+        private HelpInformation _helpInformation;
+        #endregion
+
+        #region Static Events
+        public static event EventHandler Show;
+        #endregion
     }
 }
