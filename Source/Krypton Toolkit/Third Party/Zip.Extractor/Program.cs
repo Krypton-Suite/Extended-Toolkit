c5c42dc72c8f2e40f57e326aef099b74ce485613
Peter Wagner <peterwagner@live.co.uk>
2021-03-31 12:02:25 +0100

    * Code

--- Source/Krypton Toolkit/Third Party/Zip.Extractor/Program.cs
+++ Source/Krypton Toolkit/Third Party/Zip.Extractor/Program.cs
@@ -17,7 +17,7 @@ namespace Zip.Extractor
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainWindow());
         }
     }
 }
