41e19018d079f3577373c01d8b79055e718c2c52
Peter Wagner <peterwagner@live.co.uk>
2021-04-01 16:09:40 +0100

    * Code

---
+++ Source/Krypton Toolkit/Main/Krypton.Toolkit.Suite.Extended.Messagebox/Controls/MessageButton.cs
@@ -0,0 +1,47 @@
+﻿using System.ComponentModel;
+using System.Windows.Forms;
+
+namespace Krypton.Toolkit.Suite.Extended.Messagebox
+{
+    [ToolboxItem(false)]
+    internal class MessageButton : KryptonButton
+    {
+        #region Identity
+        /// <summary>
+        /// Gets and sets the ignoring of Alt+F4
+        /// </summary>
+        public bool IgnoreAltF4 { get; set; }
+
+        #endregion
+
+        #region Protected
+        /// <summary>
+        /// Processes Windows messages.
+        /// </summary>
+        /// <param name="m">The Windows Message to process. </param>
+        protected override void WndProc(ref Message m)
+        {
+            switch (m.Msg)
+            {
+                case PlatformInvoke.WM_KEYDOWN:
+                case PlatformInvoke.WM_SYSKEYDOWN:
+                    if (IgnoreAltF4)
+                    {
+                        // Extract the keys being pressed
+                        Keys keys = ((Keys)((int)m.WParam.ToInt64()));
+
+                        // If the user standard combination ALT + F4
+                        if ((keys == Keys.F4) && ((ModifierKeys & Keys.Alt) == Keys.Alt))
+                        {
+                            // Eat the message, so standard window proc does not close the window
+                            return;
+                        }
+                    }
+                    break;
+            }
+
+            base.WndProc(ref m);
+        }
+        #endregion
+    }
+}
