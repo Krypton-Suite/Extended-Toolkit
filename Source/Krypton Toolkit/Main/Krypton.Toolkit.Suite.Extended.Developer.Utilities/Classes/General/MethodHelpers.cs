26503cbc1583e16a75afc566445879f876479799
Peter Wagner <peterwagner@live.co.uk>
2021-04-04 12:30:22 +0100

    * Changes

---
+++ Source/Krypton Toolkit/Main/Krypton.Toolkit.Suite.Extended.Developer.Utilities/Classes/General/MethodHelpers.cs
@@ -0,0 +1,18 @@
+﻿using System.Diagnostics;
+using System.Runtime.CompilerServices;
+
+namespace Krypton.Toolkit.Suite.Extended.Developer.Utilities
+{
+    public class MethodHelpers
+    {
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        public static string GetCurrentMethod()
+        {
+            var stackTrace = new StackTrace();
+
+            var stackFrame = stackTrace.GetFrame(1);
+
+            return stackFrame.GetMethod().ToString();
+        }
+    }
+}
