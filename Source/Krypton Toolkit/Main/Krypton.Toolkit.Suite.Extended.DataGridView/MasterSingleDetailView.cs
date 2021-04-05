ad7e0d4b5f8fed21dfe8b0c3d7e1c95b999d51c7
Smurf-iv <Smurf.iv@gmail.com>
2021-04-05 19:01:28 +0100

    - Drop Arrow indicating active Details view - RowHeader indicating which is active (In case child view has similar header to master) - Scroll bar for the Details view - MultiView showing Tabs for each Child - Krypton theming
    ## Single Detail View
    This allows a Master Detail view to be seen with very little code from the client side
    
    ## Multi Detail View
    Places Child Detail views into tabs under each Master row.
    
    # ToDO
    - Add in Krypton Ext Column Types
    - Deal with no Details for Master row
    - Make the Details view height dynamic
    - Add Max Height Design control for MaxHeight
    - More Documentation
    - Extend demo App to use a progmatic list / dictionary

---
+++ Source/Krypton Toolkit/Main/Krypton.Toolkit.Suite.Extended.DataGridView/MasterSingleDetailView.cs
@@ -0,0 +1,48 @@
+﻿using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+
+using Krypton.Toolkit.Suite.Extended.DataGridView.Designers;
+using Krypton.Toolkit.Suite.Extended.DataGridView.Implementation;
+
+namespace Krypton.Toolkit.Suite.Extended.DataGridView
+{
+    /// <summary>
+    /// This is a MasterDetail DataGridView that allows a single detail block to be displayed under each master row
+    /// </summary>
+    [ToolboxItem(true)]
+    [ToolboxBitmap(typeof(MasterSingleDetailView), "Designers.MasterSingleDetailView.bmp   ")]
+    [DesignerCategory("code")]
+    [Designer(typeof(MasterSingleDetailViewDesigner))]
+    [Description("This is a MasterDetail DataGridView that allows a single detail block to be displayed under each master row.")]
+    public class MasterSingleDetailView : MasterDetailGridView<KryptonDataGridView>
+    {
+        private readonly SingleDetailView childView;
+
+        internal override IDetailView<KryptonDataGridView> ChildView => childView;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public MasterSingleDetailView()
+        {
+            childView = new SingleDetailView();
+            Controls.Add(childView);
+            SetStyles(childView);
+        }
+
+        /// <summary>
+        /// Add the tableName to be used for the Details from within the DataSource
+        /// </summary>
+        /// <param name="tableName"></param>
+        /// <param name="targetColumn"></param>
+        public void AddSingleDetail(string tableName, string targetColumn)
+        {
+            childView.ChildGrids[childView] = targetColumn;
+            childView.DataSource = new DataView(DataSet.Tables[tableName]);
+            ConvertToExtKryptonColumns(this);
+
+        }
+
+    }
+}
---
+++ Source/Krypton Toolkit/Main/Krypton.Toolkit.Suite.Extended.DataGridView/Readme.md
@@ -0,0 +1,22 @@
+﻿# DataGridView
+![](SingleAndMultiDetailViews.png)
+## Things of note in the picture
+- Drop Arrow indicating active Details view
+- RowHeader indicating which is active (In case child view has similar header to master)
+- Scroll bar for the Details view
+- MultiView showing Tabs for each Child
+- Krypton theming
+
+## Single Detail View
+This allows a Master Detail view to be seen with very little code from the client side
+
+## Multi Detail View
+Places Child Detail views into tabs under each Master row.
+
+# ToDO
+- Add in Krypton Ext Column Types
+- Deal with no Details for Master row
+- Make the Details view height dynamic
+- Add Max Height Design control for MaxHeight
+- More Documentation
+- Extend demo App to use a progmatic list / dictionary
