63574424e9f73b4693d2a8c271f247b3dc6cda49
Peter Wagner <peterwagner@live.co.uk>
2021-04-05 17:31:52 +0100

    * Files

---
+++ Source/Krypton Toolkit/Third Party/Microsoft.Windows.API.Code.Pack.Shell/Common/ShellSavedSearchCollection.cs
@@ -0,0 +1,14 @@
+﻿namespace Microsoft.Windows.API.Code.Pack.Shell
+{
+    /// <summary>
+    /// Represents a saved search
+    /// </summary>
+    public class ShellSavedSearchCollection : ShellSearchCollection
+    {
+        internal ShellSavedSearchCollection(IShellItem2 shellItem)
+            : base(shellItem)
+        {
+            CoreHelpers.ThrowIfNotVista();
+        }
+    }
+}
