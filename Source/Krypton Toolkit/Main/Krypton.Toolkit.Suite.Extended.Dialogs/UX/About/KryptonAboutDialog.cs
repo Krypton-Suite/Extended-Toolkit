eb19325c02d9bd1a233fdec19625d52d3b38b12d
Peter Wagner <peterwagner@live.co.uk>
2021-04-06 09:04:01 +0100

    * Files

---
+++ Source/Krypton Toolkit/Main/Krypton.Toolkit.Suite.Extended.Dialogs/UX/About/KryptonAboutDialog.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Krypton.Toolkit.Suite.Extended.Dialogs
+{
+    public class KryptonAboutDialog : KryptonForm
+    {
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // KryptonAboutDialog
+            // 
+            this.ClientSize = new System.Drawing.Size(622, 373);
+            this.Name = "KryptonAboutDialog";
+            this.Load += new System.EventHandler(this.KryptonAboutDialog_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        private void KryptonAboutDialog_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
