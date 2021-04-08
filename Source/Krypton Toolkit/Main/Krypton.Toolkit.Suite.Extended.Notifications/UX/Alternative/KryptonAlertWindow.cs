431d52f5597e84650465d32838099ee682c50aa8
Peter Wagner <peterwagner@live.co.uk>
2021-04-08 16:30:15 +0100

    * More on #58

--- Source/Krypton Toolkit/Main/Krypton.Toolkit.Suite.Extended.Notifications/UX/Alternative/KryptonAlertWindow.cs
+++ Source/Krypton Toolkit/Main/Krypton.Toolkit.Suite.Extended.Notifications/UX/Alternative/KryptonAlertWindow.cs
@@ -13,20 +13,20 @@ namespace Krypton.Toolkit.Suite.Extended.Notifications
         private KryptonLabel klblContent;
         private KryptonLabel klblHeader;
         private KryptonWrapLabel kwlContent;
-        private System.Windows.Forms.PictureBox ptbLogo;
+        private System.Windows.Forms.PictureBox pbxLogo;
 
         private void InitializeComponent()
         {
             this.kpnlBackground = new Krypton.Toolkit.KryptonPanel();
-            this.ptbLogo = new System.Windows.Forms.PictureBox();
-            this.klblHeader = new Krypton.Toolkit.KryptonLabel();
-            this.klblContent = new Krypton.Toolkit.KryptonLabel();
-            this.ptbClose = new System.Windows.Forms.PictureBox();
             this.kwlContent = new Krypton.Toolkit.KryptonWrapLabel();
+            this.ptbClose = new System.Windows.Forms.PictureBox();
+            this.klblContent = new Krypton.Toolkit.KryptonLabel();
+            this.klblHeader = new Krypton.Toolkit.KryptonLabel();
+            this.pbxLogo = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.kpnlBackground)).BeginInit();
             this.kpnlBackground.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.ptbLogo)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.ptbClose)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxLogo)).BeginInit();
             this.SuspendLayout();
             // 
             // kpnlBackground
@@ -35,43 +35,24 @@ namespace Krypton.Toolkit.Suite.Extended.Notifications
             this.kpnlBackground.Controls.Add(this.ptbClose);
             this.kpnlBackground.Controls.Add(this.klblContent);
             this.kpnlBackground.Controls.Add(this.klblHeader);
-            this.kpnlBackground.Controls.Add(this.ptbLogo);
+            this.kpnlBackground.Controls.Add(this.pbxLogo);
             this.kpnlBackground.Dock = System.Windows.Forms.DockStyle.Fill;
             this.kpnlBackground.Location = new System.Drawing.Point(0, 0);
             this.kpnlBackground.Name = "kpnlBackground";
             this.kpnlBackground.Size = new System.Drawing.Size(417, 83);
             this.kpnlBackground.TabIndex = 0;
             // 
-            // ptbLogo
+            // kwlContent
             // 
-            this.ptbLogo.BackColor = System.Drawing.Color.Transparent;
-            this.ptbLogo.Image = global::Krypton.Toolkit.Suite.Extended.Notifications.Properties.Resources.sucess48px;
-            this.ptbLogo.Location = new System.Drawing.Point(12, 12);
-            this.ptbLogo.Name = "ptbLogo";
-            this.ptbLogo.Size = new System.Drawing.Size(63, 57);
-            this.ptbLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.ptbLogo.TabIndex = 2;
-            this.ptbLogo.TabStop = false;
-            // 
-            // klblHeader
-            // 
-            this.klblHeader.LabelStyle = Krypton.Toolkit.LabelStyle.BoldControl;
-            this.klblHeader.Location = new System.Drawing.Point(82, 13);
-            this.klblHeader.Name = "klblHeader";
-            this.klblHeader.Size = new System.Drawing.Size(98, 19);
-            this.klblHeader.StateCommon.ShortText.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.klblHeader.StateCommon.ShortText.ImageStyle = Krypton.Toolkit.PaletteImageStyle.Inherit;
-            this.klblHeader.StateCommon.ShortText.Trim = Krypton.Toolkit.PaletteTextTrim.Inherit;
-            this.klblHeader.TabIndex = 3;
-            this.klblHeader.Values.Text = "kryptonLabel1";
-            // 
-            // klblContent
-            // 
-            this.klblContent.Location = new System.Drawing.Point(82, 38);
-            this.klblContent.Name = "klblContent";
-            this.klblContent.Size = new System.Drawing.Size(88, 20);
-            this.klblContent.TabIndex = 4;
-            this.klblContent.Values.Text = "kryptonLabel1";
+            this.kwlContent.AutoSize = false;
+            this.kwlContent.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.kwlContent.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(57)))), ((int)(((byte)(91)))));
+            this.kwlContent.Location = new System.Drawing.Point(82, 12);
+            this.kwlContent.Name = "kwlContent";
+            this.kwlContent.Size = new System.Drawing.Size(291, 57);
+            this.kwlContent.Text = "kryptonWrapLabel1";
+            this.kwlContent.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.kwlContent.Visible = false;
             // 
             // ptbClose
             // 
@@ -87,17 +68,34 @@ namespace Krypton.Toolkit.Suite.Extended.Notifications
             this.ptbClose.TabStop = false;
             this.ptbClose.Click += new System.EventHandler(this.ptbClose_Click);
             // 
-            // kwlContent
+            // klblContent
             // 
-            this.kwlContent.AutoSize = false;
-            this.kwlContent.Font = new System.Drawing.Font("Segoe UI", 9F);
-            this.kwlContent.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(57)))), ((int)(((byte)(91)))));
-            this.kwlContent.Location = new System.Drawing.Point(82, 12);
-            this.kwlContent.Name = "kwlContent";
-            this.kwlContent.Size = new System.Drawing.Size(291, 57);
-            this.kwlContent.Text = "kryptonWrapLabel1";
-            this.kwlContent.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.kwlContent.Visible = false;
+            this.klblContent.Location = new System.Drawing.Point(82, 38);
+            this.klblContent.Name = "klblContent";
+            this.klblContent.Size = new System.Drawing.Size(88, 20);
+            this.klblContent.TabIndex = 4;
+            this.klblContent.Values.Text = "kryptonLabel1";
+            // 
+            // klblHeader
+            // 
+            this.klblHeader.LabelStyle = Krypton.Toolkit.LabelStyle.BoldControl;
+            this.klblHeader.Location = new System.Drawing.Point(82, 13);
+            this.klblHeader.Name = "klblHeader";
+            this.klblHeader.Size = new System.Drawing.Size(98, 19);
+            this.klblHeader.StateCommon.ShortText.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.klblHeader.TabIndex = 3;
+            this.klblHeader.Values.Text = "kryptonLabel1";
+            // 
+            // pbxLogo
+            // 
+            this.pbxLogo.BackColor = System.Drawing.Color.Transparent;
+            this.pbxLogo.Image = global::Krypton.Toolkit.Suite.Extended.Notifications.Properties.Resources.sucess48px;
+            this.pbxLogo.Location = new System.Drawing.Point(12, 12);
+            this.pbxLogo.Name = "pbxLogo";
+            this.pbxLogo.Size = new System.Drawing.Size(63, 57);
+            this.pbxLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbxLogo.TabIndex = 2;
+            this.pbxLogo.TabStop = false;
             // 
             // KryptonAlertWindow
             // 
@@ -111,8 +109,8 @@ namespace Krypton.Toolkit.Suite.Extended.Notifications
             ((System.ComponentModel.ISupportInitialize)(this.kpnlBackground)).EndInit();
             this.kpnlBackground.ResumeLayout(false);
             this.kpnlBackground.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.ptbLogo)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.ptbClose)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxLogo)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -178,27 +176,27 @@ namespace Krypton.Toolkit.Suite.Extended.Notifications
             switch (alertType)
             {
                 case AlertType.SUCESS:
-                    ptbLogo.Image = Resources.sucess48px;
+                    pbxLogo.Image = Resources.sucess48px;
 
                     ChangeColour(Color.SeaGreen, Color.White);
                     break;
                 case AlertType.INFORMATION:
-                    ptbLogo.Image = Resources.information48px;
+                    pbxLogo.Image = Resources.information48px;
 
                     ChangeColour(Color.RoyalBlue, Color.White);
                     break;
                 case AlertType.WARNING:
-                    ptbLogo.Image = Resources.warning48px;
+                    pbxLogo.Image = Resources.warning48px;
 
                     ChangeColour(Color.FromArgb(230, 126, 34), Color.White);
                     break;
                 case AlertType.ERROR:
-                    ptbLogo.Image = Resources.error48px;
+                    pbxLogo.Image = Resources.error48px;
 
                     ChangeColour(Color.FromArgb(231, 76, 60), Color.White);
                     break;
                 case AlertType.CUSTOM:
-                    ptbLogo.Image = image ?? Resources.information48px;
+                    pbxLogo.Image = image ?? Resources.information48px;
 
                     ChangeColour(backColour, textColour);
                     break;
