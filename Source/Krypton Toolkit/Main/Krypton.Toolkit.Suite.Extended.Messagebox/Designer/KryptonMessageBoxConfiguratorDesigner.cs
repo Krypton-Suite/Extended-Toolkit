6b7ad37e57dc8627de60715a7b77fdf293d27e8e
Peter Wagner <peterwagner@live.co.uk>
2021-04-09 11:51:55 +0100

    Create KryptonMessageBoxConfiguratorDesigner.cs

---
+++ Source/Krypton Toolkit/Main/Krypton.Toolkit.Suite.Extended.Messagebox/Designer/KryptonMessageBoxConfiguratorDesigner.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms.Design;
+
+namespace Krypton.Toolkit.Suite.Extended.Messagebox
+{
+    public class KryptonMessageBoxConfiguratorDesigner : ControlDesigner
+    {
+    }
+}
