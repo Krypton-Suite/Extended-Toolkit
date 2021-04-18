692e7980ee3c64542416f7bbe25941c424d1f7b7
Peter Wagner <peterwagner@live.co.uk>
2021-04-18 18:39:45 +0100

    * Edits

--- Source/Krypton Toolkit/Examples/Controls/Form1.Designer.cs
+++ Source/Krypton Toolkit/Examples/Controls/Form1.Designer.cs
@@ -29,10 +29,17 @@ namespace Controls
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Name = "Form1";
+            this.Text = "Controls Showcase";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
