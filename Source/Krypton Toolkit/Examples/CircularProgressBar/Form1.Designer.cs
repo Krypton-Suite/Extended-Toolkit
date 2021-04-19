51a31fa5d2b00543f518b5bbcc192a2a6337097f
Peter Wagner <peterwagner@live.co.uk>
2021-04-19 12:19:19 +0100

    * Files

---
+++ Source/Krypton Toolkit/Examples/CircularProgressBar/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿
+namespace CircularProgressBar
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.kryptonPanel1 = new Krypton.Toolkit.KryptonPanel();
+            ((System.ComponentModel.ISupportInitialize)(this.kryptonPanel1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // kryptonPanel1
+            // 
+            this.kryptonPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.kryptonPanel1.Location = new System.Drawing.Point(0, 0);
+            this.kryptonPanel1.Name = "kryptonPanel1";
+            this.kryptonPanel1.Size = new System.Drawing.Size(367, 341);
+            this.kryptonPanel1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(367, 341);
+            this.Controls.Add(this.kryptonPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.kryptonPanel1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Krypton.Toolkit.KryptonPanel kryptonPanel1;
+    }
+}
+
---
+++ Source/Krypton Toolkit/Third Party/Windows.Forms.Animation.Library/Classes/Path.cs
@@ -0,0 +1,151 @@
+﻿namespace Windows.Forms.Animation.Library
+{
+    /// <summary>
+    ///     The Path class is a representation of a line in a 1D plane and the
+    ///     speed in which the animator plays it
+    /// </summary>
+    public class Path
+    {
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="Path" /> class.
+        /// </summary>
+        public Path() : this(default(float), default(float), default(ulong), 0, null)
+        {
+        }
+
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="Path" /> class.
+        /// </summary>
+        /// <param name="start">
+        ///     The starting value
+        /// </param>
+        /// <param name="end">
+        ///     The ending value
+        /// </param>
+        /// <param name="duration">
+        ///     The time in miliseconds that the animator must play this path
+        /// </param>
+        /// <exception cref="ArgumentOutOfRangeException">
+        ///     Duration is less than zero
+        /// </exception>
+        public Path(float start, float end, ulong duration) : this(start, end, duration, 0, null)
+        {
+        }
+
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="Path" /> class.
+        /// </summary>
+        /// <param name="start">
+        ///     The starting value
+        /// </param>
+        /// <param name="end">
+        ///     The ending value
+        /// </param>
+        /// <param name="duration">
+        ///     The time in miliseconds that the animator must play this path
+        /// </param>
+        /// <param name="function">
+        ///     The animation function
+        /// </param>
+        /// <exception cref="ArgumentOutOfRangeException">
+        ///     Duration is less than zero
+        /// </exception>
+        public Path(float start, float end, ulong duration, AnimationFunctions.Function function)
+            : this(start, end, duration, 0, function)
+        {
+        }
+
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="Path" /> class.
+        /// </summary>
+        /// <param name="start">
+        ///     The starting value
+        /// </param>
+        /// <param name="end">
+        ///     The ending value
+        /// </param>
+        /// <param name="duration">
+        ///     The time in miliseconds that the animator must play this path
+        /// </param>
+        /// <param name="delay">
+        ///     The time in miliseconds that the animator must wait before playing this path
+        /// </param>
+        /// <exception cref="ArgumentOutOfRangeException">
+        ///     Duration is less than zero
+        /// </exception>
+        public Path(float start, float end, ulong duration, ulong delay) : this(start, end, duration, delay, null)
+        {
+        }
+
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="Path" /> class.
+        /// </summary>
+        /// <param name="start">
+        ///     The starting value
+        /// </param>
+        /// <param name="end">
+        ///     The ending value
+        /// </param>
+        /// <param name="duration">
+        ///     The time in miliseconds that the animator must play this path
+        /// </param>
+        /// <param name="delay">
+        ///     The time in miliseconds that the animator must wait before playing this path
+        /// </param>
+        /// <param name="function">
+        ///     The animation function
+        /// </param>
+        /// <exception cref="ArgumentOutOfRangeException">
+        ///     Duration is less than zero
+        /// </exception>
+        public Path(float start, float end, ulong duration, ulong delay, AnimationFunctions.Function function)
+        {
+            Start = start;
+            End = end;
+            Function = function ?? AnimationFunctions.Linear;
+            Duration = duration;
+            Delay = delay;
+        }
+
+        /// <summary>
+        ///     Gets the difference of starting and ending values
+        /// </summary>
+        public float Change => End - Start;
+
+        /// <summary>
+        ///     Gets or sets the starting delay
+        /// </summary>
+        public ulong Delay { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the duration in milliseconds
+        /// </summary>
+        public ulong Duration { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the ending value
+        /// </summary>
+        public float End { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the animation function
+        /// </summary>
+        public AnimationFunctions.Function Function { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the starting value
+        /// </summary>
+        public float Start { get; set; }
+
+        /// <summary>
+        ///     Creates and returns a new <see cref="Path" /> based on the current path but in reverse order
+        /// </summary>
+        /// <returns>
+        ///     A new <see cref="Path" /> which is the reverse of the current <see cref="Path" />
+        /// </returns>
+        public Path Reverse()
+        {
+            return new Path(End, Start, Duration, Delay, Function);
+        }
+    }
+}
