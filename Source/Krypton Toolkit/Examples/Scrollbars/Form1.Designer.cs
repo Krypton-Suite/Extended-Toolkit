6be7bae7ab0050cf2ef5839f74e93476b49b4bb6
Peter Wagner <peterwagner@live.co.uk>
2021-04-21 17:28:26 +0100

    * Demo

--- Source/Krypton Toolkit/Examples/Scrollbars/Form1.Designer.cs
+++ Source/Krypton Toolkit/Examples/Scrollbars/Form1.Designer.cs
@@ -30,12 +30,45 @@ namespace Scrollbars
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.kryptonScrollBar1 = new Krypton.Toolkit.Suite.Extended.Scroll.Bars.KryptonScrollBar();
+            this.kryptonScrollBar2 = new Krypton.Toolkit.Suite.Extended.Scroll.Bars.KryptonScrollBar();
+            this.SuspendLayout();
+            // 
+            // kryptonScrollBar1
+            // 
+            this.kryptonScrollBar1.Location = new System.Drawing.Point(12, 36);
+            this.kryptonScrollBar1.Name = "kryptonScrollBar1";
+            this.kryptonScrollBar1.Size = new System.Drawing.Size(19, 200);
+            this.kryptonScrollBar1.TabIndex = 0;
+            // 
+            // kryptonScrollBar2
+            // 
+            this.kryptonScrollBar2.Location = new System.Drawing.Point(12, 12);
+            this.kryptonScrollBar2.Name = "kryptonScrollBar2";
+            this.kryptonScrollBar2.Orientation = Krypton.Toolkit.Suite.Extended.Scroll.Bars.ScrollBarOrientation.HORIZONTAL;
+            this.kryptonScrollBar2.Size = new System.Drawing.Size(200, 19);
+            this.kryptonScrollBar2.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(266, 248);
+            this.Controls.Add(this.kryptonScrollBar2);
+            this.Controls.Add(this.kryptonScrollBar1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Scrollbars";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private Krypton.Toolkit.Suite.Extended.Scroll.Bars.KryptonScrollBar kryptonScrollBar1;
+        private Krypton.Toolkit.Suite.Extended.Scroll.Bars.KryptonScrollBar kryptonScrollBar2;
     }
 }
 
