d25d964b810a3a045a1bb001d5ca5ff29b752626
Peter Wagner <peterwagner@live.co.uk>
2021-04-21 18:53:39 +0100

    * Demo

--- Source/Krypton Toolkit/Examples/CircularProgressBar/Form1.cs
+++ Source/Krypton Toolkit/Examples/CircularProgressBar/Form1.cs
@@ -1,5 +1,13 @@
 ﻿using Krypton.Toolkit;
 using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace CircularProgressBar
 {
@@ -10,5 +18,45 @@ namespace CircularProgressBar
             InitializeComponent();
         }
 
+        private void kcbColourTrio_CheckedChanged(object sender, EventArgs e)
+        {
+            ccbTest.UseColourTrio = kcbColourTrio.Checked;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            tmrIncrement.Enabled = false;
+
+            ccbTest.Text = "0%";
+        }
+
+        private void tmrIncrement_Tick(object sender, EventArgs e)
+        {
+            ccbTest.Increment(1);
+
+            ccbTest.Text = $"{ ccbTest.Value }%";
+
+            if (ccbTest.Value >= 100)
+            {
+                tmrIncrement.Stop();
+            }
+        }
+
+        private void kbtnStart_Click(object sender, EventArgs e)
+        {
+            tmrIncrement.Start();
+        }
+
+        private void kbtnStop_Click(object sender, EventArgs e)
+        {
+            tmrIncrement.Stop();
+        }
+
+        private void kbtnReset_Click(object sender, EventArgs e)
+        {
+            ccbTest.Value = 0;
+
+            ccbTest.Text = "0%";
+        }
     }
 }
