907f31633c400d73566398283cbd91f0308de315
Peter Wagner <peterwagner@live.co.uk>
2021-04-28 17:19:21 +0100

    * Sample

--- Source/Krypton Toolkit/Examples/Buttons/Form1.Designer.cs
+++ Source/Krypton Toolkit/Examples/Buttons/Form1.Designer.cs
@@ -31,64 +31,141 @@ namespace Buttons
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.kryptonPanel1 = new Krypton.Toolkit.KryptonPanel();
+            this.kbtnBrowse = new Krypton.Toolkit.KryptonButton();
+            this.kryptonLabel1 = new Krypton.Toolkit.KryptonLabel();
+            this.kryptonCommandLinkButton1 = new Krypton.Toolkit.Suite.Extended.Buttons.KryptonCommandLinkButton();
+            this.kryptonCommandLinkButton2 = new Krypton.Toolkit.Suite.Extended.Buttons.KryptonCommandLinkButton();
+            this.kryptonCommandLinkVersion21 = new Krypton.Toolkit.Suite.Extended.Buttons.KryptonCommandLinkVersion2();
+            this.kryptonSplitButton1 = new Krypton.Toolkit.Suite.Extended.Buttons.KryptonSplitButton();
+            this.kryptonUACButtonVersion11 = new Krypton.Toolkit.Suite.Extended.Buttons.KryptonUACButtonVersion1();
             this.kryptonUACButtonVersion21 = new Krypton.Toolkit.Suite.Extended.Buttons.KryptonUACButtonVersion2();
-            this.kuacbtnV1 = new Krypton.Toolkit.Suite.Extended.Buttons.KryptonUACButtonVersion1();
             ((System.ComponentModel.ISupportInitialize)(this.kryptonPanel1)).BeginInit();
+            this.kryptonPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // kryptonPanel1
             // 
+            this.kryptonPanel1.Controls.Add(this.kryptonUACButtonVersion21);
+            this.kryptonPanel1.Controls.Add(this.kryptonUACButtonVersion11);
+            this.kryptonPanel1.Controls.Add(this.kryptonSplitButton1);
+            this.kryptonPanel1.Controls.Add(this.kryptonCommandLinkVersion21);
+            this.kryptonPanel1.Controls.Add(this.kryptonCommandLinkButton2);
+            this.kryptonPanel1.Controls.Add(this.kryptonCommandLinkButton1);
+            this.kryptonPanel1.Controls.Add(this.kbtnBrowse);
+            this.kryptonPanel1.Controls.Add(this.kryptonLabel1);
             this.kryptonPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.kryptonPanel1.Location = new System.Drawing.Point(0, 0);
             this.kryptonPanel1.Name = "kryptonPanel1";
             this.kryptonPanel1.Size = new System.Drawing.Size(800, 450);
             this.kryptonPanel1.TabIndex = 2;
             // 
+            // kbtnBrowse
+            // 
+            this.kbtnBrowse.Location = new System.Drawing.Point(759, 422);
+            this.kbtnBrowse.Name = "kbtnBrowse";
+            this.kbtnBrowse.Size = new System.Drawing.Size(29, 22);
+            this.kbtnBrowse.TabIndex = 2;
+            this.kbtnBrowse.Values.Text = "...";
+            // 
+            // kryptonLabel1
+            // 
+            this.kryptonLabel1.AutoSize = false;
+            this.kryptonLabel1.Location = new System.Drawing.Point(393, 422);
+            this.kryptonLabel1.Name = "kryptonLabel1";
+            this.kryptonLabel1.Size = new System.Drawing.Size(361, 17);
+            this.kryptonLabel1.TabIndex = 0;
+            this.kryptonLabel1.Values.Text = "kryptonLabel1";
+            // 
+            // kryptonCommandLinkButton1
+            // 
+            this.kryptonCommandLinkButton1.Location = new System.Drawing.Point(12, 12);
+            this.kryptonCommandLinkButton1.Name = "kryptonCommandLinkButton1";
+            this.kryptonCommandLinkButton1.ProcessToElevate = null;
+            this.kryptonCommandLinkButton1.Size = new System.Drawing.Size(250, 55);
+            this.kryptonCommandLinkButton1.StateCommon.Content.ShortText.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.kryptonCommandLinkButton1.TabIndex = 3;
+            // 
+            // kryptonCommandLinkButton2
+            // 
+            this.kryptonCommandLinkButton2.Location = new System.Drawing.Point(12, 73);
+            this.kryptonCommandLinkButton2.Name = "kryptonCommandLinkButton2";
+            this.kryptonCommandLinkButton2.ProcessToElevate = null;
+            this.kryptonCommandLinkButton2.Size = new System.Drawing.Size(250, 55);
+            this.kryptonCommandLinkButton2.StateCommon.Content.ShortText.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.kryptonCommandLinkButton2.TabIndex = 4;
+            // 
+            // kryptonCommandLinkVersion21
+            // 
+            this.kryptonCommandLinkVersion21.Location = new System.Drawing.Point(12, 135);
+            this.kryptonCommandLinkVersion21.Name = "kryptonCommandLinkVersion21";
+            this.kryptonCommandLinkVersion21.ProcessToElevate = null;
+            this.kryptonCommandLinkVersion21.Size = new System.Drawing.Size(250, 60);
+            this.kryptonCommandLinkVersion21.TabIndex = 5;
+            this.kryptonCommandLinkVersion21.UACShieldSize = new System.Drawing.Size(0, 0);
+            this.kryptonCommandLinkVersion21.Values.Image = global::Buttons.Properties.Resources.Question_32_x_32;
+            this.kryptonCommandLinkVersion21.Values.Text = "kryptonCommandLinkVersion21";
+            // 
+            // kryptonSplitButton1
+            // 
+            this.kryptonSplitButton1.AutoSize = true;
+            this.kryptonSplitButton1.Location = new System.Drawing.Point(440, 255);
+            this.kryptonSplitButton1.Name = "kryptonSplitButton1";
+            this.kryptonSplitButton1.ProcessPath = null;
+            this.kryptonSplitButton1.ShowSplitOption = true;
+            this.kryptonSplitButton1.Size = new System.Drawing.Size(173, 24);
+            this.kryptonSplitButton1.TabIndex = 6;
+            this.kryptonSplitButton1.UseUACElevation = false;
+            this.kryptonSplitButton1.Values.Text = "kryptonSplitButton1";
+            // 
+            // kryptonUACButtonVersion11
+            // 
+            this.kryptonUACButtonVersion11.AssemblyToElevate = null;
+            this.kryptonUACButtonVersion11.Location = new System.Drawing.Point(440, 286);
+            this.kryptonUACButtonVersion11.Name = "kryptonUACButtonVersion11";
+            this.kryptonUACButtonVersion11.Size = new System.Drawing.Size(135, 26);
+            this.kryptonUACButtonVersion11.TabIndex = 7;
+            this.kryptonUACButtonVersion11.Values.Image = ((System.Drawing.Image)(resources.GetObject("kryptonUACButtonVersion11.Values.Image")));
+            this.kryptonUACButtonVersion11.Values.Text = "kryptonUACButtonVersion11";
+            // 
             // kryptonUACButtonVersion21
             // 
-            this.kryptonUACButtonVersion21.CustomShieldSize = new System.Drawing.Size(48, 48);
+            this.kryptonUACButtonVersion21.CustomShieldSize = new System.Drawing.Size(0, 0);
             this.kryptonUACButtonVersion21.ExtraArguments = null;
-            this.kryptonUACButtonVersion21.Location = new System.Drawing.Point(393, 210);
+            this.kryptonUACButtonVersion21.Location = new System.Drawing.Point(440, 319);
             this.kryptonUACButtonVersion21.Name = "kryptonUACButtonVersion21";
             this.kryptonUACButtonVersion21.PathToElevatedObject = null;
-            this.kryptonUACButtonVersion21.Size = new System.Drawing.Size(322, 130);
-            this.kryptonUACButtonVersion21.TabIndex = 1;
-            this.kryptonUACButtonVersion21.UACShieldSize = Krypton.Toolkit.Suite.Extended.Buttons.UACShieldSize.CUSTOM;
+            this.kryptonUACButtonVersion21.Size = new System.Drawing.Size(348, 87);
+            this.kryptonUACButtonVersion21.TabIndex = 8;
+            this.kryptonUACButtonVersion21.UACShieldSize = Krypton.Toolkit.Suite.Extended.Buttons.UACShieldSize.MEDIUM;
             this.kryptonUACButtonVersion21.UseAsUACElevatedButton = true;
             this.kryptonUACButtonVersion21.Values.Image = ((System.Drawing.Image)(resources.GetObject("kryptonUACButtonVersion21.Values.Image")));
             this.kryptonUACButtonVersion21.Values.Text = "kryptonUACButtonVersion21";
-            // 
-            // kuacbtnV1
-            // 
-            this.kuacbtnV1.AssemblyToElevate = null;
-            this.kuacbtnV1.Location = new System.Drawing.Point(393, 129);
-            this.kuacbtnV1.Name = "kuacbtnV1";
-            this.kuacbtnV1.ProcessName = "C:\\Windows\\notepad.exe";
-            this.kuacbtnV1.Size = new System.Drawing.Size(243, 51);
-            this.kuacbtnV1.TabIndex = 0;
-            this.kuacbtnV1.Values.Image = ((System.Drawing.Image)(resources.GetObject("kuacbtnV1.Values.Image")));
-            this.kuacbtnV1.Values.Text = "Open &Notepad";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.kryptonUACButtonVersion21);
-            this.Controls.Add(this.kuacbtnV1);
             this.Controls.Add(this.kryptonPanel1);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.kryptonPanel1)).EndInit();
+            this.kryptonPanel1.ResumeLayout(false);
+            this.kryptonPanel1.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private Krypton.Toolkit.Suite.Extended.Buttons.KryptonUACButtonVersion1 kuacbtnV1;
-        private Krypton.Toolkit.Suite.Extended.Buttons.KryptonUACButtonVersion2 kryptonUACButtonVersion21;
         private Krypton.Toolkit.KryptonPanel kryptonPanel1;
+        private Krypton.Toolkit.KryptonLabel kryptonLabel1;
+        private Krypton.Toolkit.KryptonButton kbtnBrowse;
+        private Krypton.Toolkit.Suite.Extended.Buttons.KryptonCommandLinkVersion2 kryptonCommandLinkVersion21;
+        private Krypton.Toolkit.Suite.Extended.Buttons.KryptonCommandLinkButton kryptonCommandLinkButton2;
+        private Krypton.Toolkit.Suite.Extended.Buttons.KryptonCommandLinkButton kryptonCommandLinkButton1;
+        private Krypton.Toolkit.Suite.Extended.Buttons.KryptonUACButtonVersion2 kryptonUACButtonVersion21;
+        private Krypton.Toolkit.Suite.Extended.Buttons.KryptonUACButtonVersion1 kryptonUACButtonVersion11;
+        private Krypton.Toolkit.Suite.Extended.Buttons.KryptonSplitButton kryptonSplitButton1;
     }
 }
 
---
+++ Source/Krypton Toolkit/Main/Krypton.Toolkit.Suite.Extended.Buttons/Readme.md
@@ -0,0 +1,20 @@
+﻿<!-- Start Document Outline -->
+
+* [Extended Buttons](#Extended Buttons)
+	* [Things of note in the pictures](#things-of-note-in-the-pictures)
+	* [Usage](#usage)
+* [ToDo](#todo)
+
+<!-- End Document Outline -->
+
+# `Extended Buttons`
+
+
+## Things of note in the picture
+- 
+
+## Usage
+- Drag and drop control on a `KryptonForm`
+- With the UAC enabled buttons, you don't need to invoke the `Click()` event, as all elevation code is handled within the button itself
+
+# ToDo
