b4c8ee267765f1dc3a128bb70d4dc2b15dd1720d
Peter Wagner <peterwagner@live.co.uk>
2021-05-09 14:26:57 +0100

    * Edits

--- Source/Krypton Toolkit/Main/Krypton.Toolkit.Suite.Extended.Messagebox/Designer/KryptonMessageBoxConfiguratorActionList.cs
+++ Source/Krypton Toolkit/Main/Krypton.Toolkit.Suite.Extended.Messagebox/Designer/KryptonMessageBoxConfiguratorActionList.cs
@@ -6,7 +6,7 @@ namespace Krypton.Toolkit.Suite.Extended.Messagebox
     internal class KryptonMessageBoxConfiguratorActionList : DesignerActionList
     {
         #region Instance Fields
-        private readonly KryptonMessageBoxManager _messageBoxConfigurator;
+        private readonly KryptonMessageBoxExtendedManager _messageBoxConfigurator;
 
         private readonly IComponentChangeService _service;
         #endregion
@@ -14,7 +14,7 @@ namespace Krypton.Toolkit.Suite.Extended.Messagebox
         #region Identity
         public KryptonMessageBoxConfiguratorActionList(KryptonMessageBoxConfiguratorDesigner owner) : base(owner.Component)
         {
-            _messageBoxConfigurator = owner.Component as KryptonMessageBoxManager;
+            _messageBoxConfigurator = owner.Component as KryptonMessageBoxExtendedManager;
 
             _service = (IComponentChangeService)GetService(typeof(IComponentChangeService));
         }
