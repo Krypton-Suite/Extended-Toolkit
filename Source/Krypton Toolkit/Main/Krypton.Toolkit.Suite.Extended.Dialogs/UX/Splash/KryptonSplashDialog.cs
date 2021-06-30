08fd6ccee4eb41407f763fd22ee42e74d26e653c
Peter Wagner <peterwagner@live.co.uk>
2021-06-30 19:08:55 +0100

    * Splash screen

--- Source/Krypton Toolkit/Main/Krypton.Toolkit.Suite.Extended.Dialogs/UX/Splash/KryptonSplashDialog.cs
+++ Source/Krypton Toolkit/Main/Krypton.Toolkit.Suite.Extended.Dialogs/UX/Splash/KryptonSplashDialog.cs
@@ -8,5 +8,78 @@ namespace Krypton.Toolkit.Suite.Extended.Dialogs
 {
     public class KryptonSplashDialog : InternalExtendedKryptonForm
     {
+        private System.Windows.Forms.ProgressBar pbLoading;
+        private System.Windows.Forms.PictureBox pbAppIcon;
+        private KryptonWrapLabel kryptonWrapLabel1;
+        private KryptonPanel kryptonPanel1;
+
+        private void InitializeComponent()
+        {
+            this.pbLoading = new System.Windows.Forms.ProgressBar();
+            this.kryptonPanel1 = new Krypton.Toolkit.KryptonPanel();
+            this.kryptonWrapLabel1 = new Krypton.Toolkit.KryptonWrapLabel();
+            this.pbAppIcon = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.kryptonPanel1)).BeginInit();
+            this.kryptonPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbAppIcon)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pbLoading
+            // 
+            this.pbLoading.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.pbLoading.Location = new System.Drawing.Point(0, 425);
+            this.pbLoading.Name = "pbLoading";
+            this.pbLoading.Size = new System.Drawing.Size(805, 5);
+            this.pbLoading.TabIndex = 0;
+            // 
+            // kryptonPanel1
+            // 
+            this.kryptonPanel1.Controls.Add(this.pbAppIcon);
+            this.kryptonPanel1.Controls.Add(this.kryptonWrapLabel1);
+            this.kryptonPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.kryptonPanel1.Location = new System.Drawing.Point(0, 0);
+            this.kryptonPanel1.Name = "kryptonPanel1";
+            this.kryptonPanel1.Size = new System.Drawing.Size(805, 425);
+            this.kryptonPanel1.TabIndex = 1;
+            // 
+            // kryptonWrapLabel1
+            // 
+            this.kryptonWrapLabel1.AutoSize = false;
+            this.kryptonWrapLabel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.kryptonWrapLabel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.kryptonWrapLabel1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(57)))), ((int)(((byte)(91)))));
+            this.kryptonWrapLabel1.Location = new System.Drawing.Point(0, 310);
+            this.kryptonWrapLabel1.Name = "kryptonWrapLabel1";
+            this.kryptonWrapLabel1.Size = new System.Drawing.Size(805, 115);
+            this.kryptonWrapLabel1.StateCommon.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.kryptonWrapLabel1.Text = "kryptonWrapLabel1";
+            this.kryptonWrapLabel1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pbAppIcon
+            // 
+            this.pbAppIcon.BackColor = System.Drawing.Color.Transparent;
+            this.pbAppIcon.Location = new System.Drawing.Point(274, 32);
+            this.pbAppIcon.Name = "pbAppIcon";
+            this.pbAppIcon.Size = new System.Drawing.Size(256, 256);
+            this.pbAppIcon.TabIndex = 1;
+            this.pbAppIcon.TabStop = false;
+            // 
+            // KryptonSplashDialog
+            // 
+            this.ClientSize = new System.Drawing.Size(805, 430);
+            this.ControlBox = false;
+            this.Controls.Add(this.kryptonPanel1);
+            this.Controls.Add(this.pbLoading);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "KryptonSplashDialog";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            ((System.ComponentModel.ISupportInitialize)(this.kryptonPanel1)).EndInit();
+            this.kryptonPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pbAppIcon)).EndInit();
+            this.ResumeLayout(false);
+
+        }
     }
 }
