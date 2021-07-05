c3c4fb7660d03655868c62e2aa344ffd3d0b911c
Peter Wagner <peterwagner@live.co.uk>
2021-07-05 18:20:35 +0100

    * Files

---
+++ Source/Krypton Toolkit/Krypton.Toolkit.Suite.Extended.File.Copier 2019/Globals/GlobalDeclarations.cs
@@ -0,0 +1,9 @@
+﻿global using System;
+global using System.Collections.Generic;
+global using System.ComponentModel;
+global using System.Diagnostics;
+global using System.IO;
+global using System.Linq;
+global using System.Windows.Forms;
+
+global using Microsoft.WindowsAPICodePack.Dialogs;
