dfd866441d1a577420fafae7952cf34c710e1b39
Peter Wagner <peterwagner@live.co.uk>
2021-07-05 18:03:43 +0100

    * Edits

---
+++ Source/Krypton Toolkit/Krypton.Toolkit.Suite.Extended.File.Copier 2019/UX/KryptonFileCopier.cs
@@ -0,0 +1,17 @@
+﻿namespace Krypton.Toolkit.Suite.Extended.File.Copier
+{
+    public class KryptonFileCopier : KryptonForm
+    {
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // KryptonFileCopier
+            // 
+            this.ClientSize = new System.Drawing.Size(832, 430);
+            this.Name = "KryptonFileCopier";
+            this.ResumeLayout(false);
+
+        }
+    }
+}
