e004d350a920d21cadd9afb27cef183e698cd02b
Peter Wagner <peterwagner@live.co.uk>
2021-07-12 19:05:28 +0100

    Update KryptonTextToSpeechDialog.cs

--- Source/Krypton Toolkit/Krypton.Toolkit.Suite.Extended.Dialogs/UX/Text to Speech/KryptonTextToSpeechDialog.cs
+++ Source/Krypton Toolkit/Krypton.Toolkit.Suite.Extended.Dialogs/UX/Text to Speech/KryptonTextToSpeechDialog.cs
@@ -7,38 +7,38 @@
         private KryptonBorderEdge kryptonBorderEdge1;
         private KryptonPanel kryptonPanel2;
         private KryptonGroupBox kryptonGroupBox2;
-        private KryptonTrackBar kryptonTrackBar1;
+        private KryptonTrackBar ktrkRate;
         private KryptonLabel kryptonLabel1;
         private KryptonGroupBox kryptonGroupBox1;
-        private KryptonComboBox kryptonComboBox1;
-        private KryptonWrapLabel kryptonWrapLabel2;
-        private KryptonTrackBar kryptonTrackBar2;
+        private KryptonComboBox kcmbInstalledVoices;
+        private KryptonWrapLabel kwlVolume;
+        private KryptonTrackBar ktrkVolume;
         private KryptonLabel kryptonLabel2;
-        private KryptonWrapLabel kryptonWrapLabel1;
-        private KryptonButton kryptonButton1;
+        private KryptonWrapLabel kwlRate;
+        private KryptonButton kbtnPreview;
         private KryptonGroupBox kryptonGroupBox3;
-        private KryptonRichTextBox kryptonRichTextBox1;
+        private KryptonRichTextBox krtbInput;
         private KryptonPanel kryptonPanel1;
 
         private void InitializeComponent()
         {
             this.kryptonPanel1 = new Krypton.Toolkit.KryptonPanel();
-            this.kryptonButton1 = new Krypton.Toolkit.KryptonButton();
+            this.kbtnPreview = new Krypton.Toolkit.KryptonButton();
             this.kbtnSpeak = new Krypton.Toolkit.KryptonButton();
             this.kbtnCancel = new Krypton.Toolkit.KryptonButton();
             this.kryptonBorderEdge1 = new Krypton.Toolkit.KryptonBorderEdge();
             this.kryptonPanel2 = new Krypton.Toolkit.KryptonPanel();
             this.kryptonGroupBox3 = new Krypton.Toolkit.KryptonGroupBox();
-            this.kryptonRichTextBox1 = new Krypton.Toolkit.KryptonRichTextBox();
+            this.krtbInput = new Krypton.Toolkit.KryptonRichTextBox();
             this.kryptonGroupBox2 = new Krypton.Toolkit.KryptonGroupBox();
-            this.kryptonWrapLabel2 = new Krypton.Toolkit.KryptonWrapLabel();
-            this.kryptonTrackBar2 = new Krypton.Toolkit.KryptonTrackBar();
+            this.kwlVolume = new Krypton.Toolkit.KryptonWrapLabel();
+            this.ktrkVolume = new Krypton.Toolkit.KryptonTrackBar();
             this.kryptonLabel2 = new Krypton.Toolkit.KryptonLabel();
-            this.kryptonWrapLabel1 = new Krypton.Toolkit.KryptonWrapLabel();
-            this.kryptonTrackBar1 = new Krypton.Toolkit.KryptonTrackBar();
+            this.kwlRate = new Krypton.Toolkit.KryptonWrapLabel();
+            this.ktrkRate = new Krypton.Toolkit.KryptonTrackBar();
             this.kryptonLabel1 = new Krypton.Toolkit.KryptonLabel();
             this.kryptonGroupBox1 = new Krypton.Toolkit.KryptonGroupBox();
-            this.kryptonComboBox1 = new Krypton.Toolkit.KryptonComboBox();
+            this.kcmbInstalledVoices = new Krypton.Toolkit.KryptonComboBox();
             ((System.ComponentModel.ISupportInitialize)(this.kryptonPanel1)).BeginInit();
             this.kryptonPanel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.kryptonPanel2)).BeginInit();
@@ -55,12 +55,12 @@
             ((System.ComponentModel.ISupportInitialize)(this.kryptonGroupBox1.Panel)).BeginInit();
             this.kryptonGroupBox1.Panel.SuspendLayout();
             this.kryptonGroupBox1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.kryptonComboBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.kcmbInstalledVoices)).BeginInit();
             this.SuspendLayout();
             // 
             // kryptonPanel1
             // 
-            this.kryptonPanel1.Controls.Add(this.kryptonButton1);
+            this.kryptonPanel1.Controls.Add(this.kbtnPreview);
             this.kryptonPanel1.Controls.Add(this.kbtnSpeak);
             this.kryptonPanel1.Controls.Add(this.kbtnCancel);
             this.kryptonPanel1.Controls.Add(this.kryptonBorderEdge1);
@@ -71,13 +71,14 @@
             this.kryptonPanel1.Size = new System.Drawing.Size(458, 50);
             this.kryptonPanel1.TabIndex = 0;
             // 
-            // kryptonButton1
+            // kbtnPreview
             // 
-            this.kryptonButton1.Location = new System.Drawing.Point(12, 13);
-            this.kryptonButton1.Name = "kryptonButton1";
-            this.kryptonButton1.Size = new System.Drawing.Size(90, 25);
-            this.kryptonButton1.TabIndex = 2;
-            this.kryptonButton1.Values.Text = "Pr&eview";
+            this.kbtnPreview.Enabled = false;
+            this.kbtnPreview.Location = new System.Drawing.Point(12, 13);
+            this.kbtnPreview.Name = "kbtnPreview";
+            this.kbtnPreview.Size = new System.Drawing.Size(90, 25);
+            this.kbtnPreview.TabIndex = 2;
+            this.kbtnPreview.Values.Text = "Pr&eview";
             // 
             // kbtnSpeak
             // 
@@ -126,17 +127,19 @@
             // 
             // kryptonGroupBox3.Panel
             // 
-            this.kryptonGroupBox3.Panel.Controls.Add(this.kryptonRichTextBox1);
+            this.kryptonGroupBox3.Panel.Controls.Add(this.krtbInput);
             this.kryptonGroupBox3.Size = new System.Drawing.Size(433, 150);
             this.kryptonGroupBox3.TabIndex = 2;
+            this.kryptonGroupBox3.Values.Heading = "Input";
             // 
-            // kryptonRichTextBox1
+            // krtbInput
             // 
-            this.kryptonRichTextBox1.Location = new System.Drawing.Point(12, 15);
-            this.kryptonRichTextBox1.Name = "kryptonRichTextBox1";
-            this.kryptonRichTextBox1.Size = new System.Drawing.Size(405, 96);
-            this.kryptonRichTextBox1.TabIndex = 0;
-            this.kryptonRichTextBox1.Text = "kryptonRichTextBox1";
+            this.krtbInput.Location = new System.Drawing.Point(12, 15);
+            this.krtbInput.Name = "krtbInput";
+            this.krtbInput.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.ForcedVertical;
+            this.krtbInput.Size = new System.Drawing.Size(405, 96);
+            this.krtbInput.TabIndex = 0;
+            this.krtbInput.Text = "";
             // 
             // kryptonGroupBox2
             // 
@@ -145,35 +148,36 @@
             // 
             // kryptonGroupBox2.Panel
             // 
-            this.kryptonGroupBox2.Panel.Controls.Add(this.kryptonWrapLabel2);
-            this.kryptonGroupBox2.Panel.Controls.Add(this.kryptonTrackBar2);
+            this.kryptonGroupBox2.Panel.Controls.Add(this.kwlVolume);
+            this.kryptonGroupBox2.Panel.Controls.Add(this.ktrkVolume);
             this.kryptonGroupBox2.Panel.Controls.Add(this.kryptonLabel2);
-            this.kryptonGroupBox2.Panel.Controls.Add(this.kryptonWrapLabel1);
-            this.kryptonGroupBox2.Panel.Controls.Add(this.kryptonTrackBar1);
+            this.kryptonGroupBox2.Panel.Controls.Add(this.kwlRate);
+            this.kryptonGroupBox2.Panel.Controls.Add(this.ktrkRate);
             this.kryptonGroupBox2.Panel.Controls.Add(this.kryptonLabel1);
             this.kryptonGroupBox2.Size = new System.Drawing.Size(433, 250);
             this.kryptonGroupBox2.TabIndex = 1;
+            this.kryptonGroupBox2.Values.Heading = "Adjustments";
             // 
-            // kryptonWrapLabel2
+            // kwlVolume
             // 
-            this.kryptonWrapLabel2.AutoSize = false;
-            this.kryptonWrapLabel2.Font = new System.Drawing.Font("Segoe UI", 9F);
-            this.kryptonWrapLabel2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(57)))), ((int)(((byte)(91)))));
-            this.kryptonWrapLabel2.Location = new System.Drawing.Point(12, 184);
-            this.kryptonWrapLabel2.Name = "kryptonWrapLabel2";
-            this.kryptonWrapLabel2.Size = new System.Drawing.Size(405, 23);
-            this.kryptonWrapLabel2.Text = "{0}";
-            this.kryptonWrapLabel2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.kwlVolume.AutoSize = false;
+            this.kwlVolume.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.kwlVolume.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(57)))), ((int)(((byte)(91)))));
+            this.kwlVolume.Location = new System.Drawing.Point(12, 184);
+            this.kwlVolume.Name = "kwlVolume";
+            this.kwlVolume.Size = new System.Drawing.Size(405, 23);
+            this.kwlVolume.Text = "{0}";
+            this.kwlVolume.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
-            // kryptonTrackBar2
+            // ktrkVolume
             // 
-            this.kryptonTrackBar2.DrawBackground = true;
-            this.kryptonTrackBar2.Location = new System.Drawing.Point(12, 139);
-            this.kryptonTrackBar2.Maximum = 100;
-            this.kryptonTrackBar2.Name = "kryptonTrackBar2";
-            this.kryptonTrackBar2.Size = new System.Drawing.Size(405, 33);
-            this.kryptonTrackBar2.TabIndex = 5;
-            this.kryptonTrackBar2.TickStyle = System.Windows.Forms.TickStyle.Both;
+            this.ktrkVolume.DrawBackground = true;
+            this.ktrkVolume.Location = new System.Drawing.Point(12, 139);
+            this.ktrkVolume.Maximum = 100;
+            this.ktrkVolume.Name = "ktrkVolume";
+            this.ktrkVolume.Size = new System.Drawing.Size(405, 33);
+            this.ktrkVolume.TabIndex = 5;
+            this.ktrkVolume.TickStyle = System.Windows.Forms.TickStyle.Both;
             // 
             // kryptonLabel2
             // 
@@ -184,26 +188,26 @@
             this.kryptonLabel2.TabIndex = 4;
             this.kryptonLabel2.Values.Text = "Volume:";
             // 
-            // kryptonWrapLabel1
+            // kwlRate
             // 
-            this.kryptonWrapLabel1.AutoSize = false;
-            this.kryptonWrapLabel1.Font = new System.Drawing.Font("Segoe UI", 9F);
-            this.kryptonWrapLabel1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(57)))), ((int)(((byte)(91)))));
-            this.kryptonWrapLabel1.Location = new System.Drawing.Point(12, 86);
-            this.kryptonWrapLabel1.Name = "kryptonWrapLabel1";
-            this.kryptonWrapLabel1.Size = new System.Drawing.Size(405, 23);
-            this.kryptonWrapLabel1.Text = "{0}";
-            this.kryptonWrapLabel1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.kwlRate.AutoSize = false;
+            this.kwlRate.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.kwlRate.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(57)))), ((int)(((byte)(91)))));
+            this.kwlRate.Location = new System.Drawing.Point(12, 86);
+            this.kwlRate.Name = "kwlRate";
+            this.kwlRate.Size = new System.Drawing.Size(405, 23);
+            this.kwlRate.Text = "{0}";
+            this.kwlRate.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
-            // kryptonTrackBar1
+            // ktrkRate
             // 
-            this.kryptonTrackBar1.DrawBackground = true;
-            this.kryptonTrackBar1.Location = new System.Drawing.Point(12, 41);
-            this.kryptonTrackBar1.Maximum = 100;
-            this.kryptonTrackBar1.Name = "kryptonTrackBar1";
-            this.kryptonTrackBar1.Size = new System.Drawing.Size(405, 33);
-            this.kryptonTrackBar1.TabIndex = 1;
-            this.kryptonTrackBar1.TickStyle = System.Windows.Forms.TickStyle.Both;
+            this.ktrkRate.DrawBackground = true;
+            this.ktrkRate.Location = new System.Drawing.Point(12, 41);
+            this.ktrkRate.Maximum = 100;
+            this.ktrkRate.Name = "ktrkRate";
+            this.ktrkRate.Size = new System.Drawing.Size(405, 33);
+            this.ktrkRate.TabIndex = 1;
+            this.ktrkRate.TickStyle = System.Windows.Forms.TickStyle.Both;
             // 
             // kryptonLabel1
             // 
@@ -223,29 +227,35 @@
             // 
             // kryptonGroupBox1.Panel
             // 
-            this.kryptonGroupBox1.Panel.Controls.Add(this.kryptonComboBox1);
+            this.kryptonGroupBox1.Panel.Controls.Add(this.kcmbInstalledVoices);
             this.kryptonGroupBox1.Size = new System.Drawing.Size(433, 102);
             this.kryptonGroupBox1.TabIndex = 0;
             this.kryptonGroupBox1.Values.Heading = "Installed Voices";
             // 
-            // kryptonComboBox1
+            // kcmbInstalledVoices
             // 
-            this.kryptonComboBox1.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawVariable;
-            this.kryptonComboBox1.DropDownWidth = 405;
-            this.kryptonComboBox1.IntegralHeight = false;
-            this.kryptonComboBox1.Location = new System.Drawing.Point(12, 25);
-            this.kryptonComboBox1.Name = "kryptonComboBox1";
-            this.kryptonComboBox1.Size = new System.Drawing.Size(405, 21);
-            this.kryptonComboBox1.StateCommon.ComboBox.Content.TextH = Krypton.Toolkit.PaletteRelativeAlign.Near;
-            this.kryptonComboBox1.TabIndex = 0;
-            this.kryptonComboBox1.Text = "kryptonComboBox1";
+            this.kcmbInstalledVoices.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawVariable;
+            this.kcmbInstalledVoices.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.kcmbInstalledVoices.DropDownWidth = 405;
+            this.kcmbInstalledVoices.IntegralHeight = false;
+            this.kcmbInstalledVoices.Location = new System.Drawing.Point(12, 25);
+            this.kcmbInstalledVoices.Name = "kcmbInstalledVoices";
+            this.kcmbInstalledVoices.Size = new System.Drawing.Size(405, 21);
+            this.kcmbInstalledVoices.StateCommon.ComboBox.Content.TextH = Krypton.Toolkit.PaletteRelativeAlign.Near;
+            this.kcmbInstalledVoices.TabIndex = 0;
             // 
             // KryptonTextToSpeechDialog
             // 
             this.ClientSize = new System.Drawing.Size(458, 602);
             this.Controls.Add(this.kryptonPanel2);
             this.Controls.Add(this.kryptonPanel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "KryptonTextToSpeechDialog";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.Text = "Text to Speech";
             ((System.ComponentModel.ISupportInitialize)(this.kryptonPanel1)).EndInit();
             this.kryptonPanel1.ResumeLayout(false);
             this.kryptonPanel1.PerformLayout();
@@ -264,7 +274,7 @@
             this.kryptonGroupBox1.Panel.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.kryptonGroupBox1)).EndInit();
             this.kryptonGroupBox1.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.kryptonComboBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.kcmbInstalledVoices)).EndInit();
             this.ResumeLayout(false);
 
         }
