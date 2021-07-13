ccc296a3d3d26c7c49f4ec4fbfcff91103f790de
Peter Wagner <peterwagner@live.co.uk>
2021-07-13 19:02:20 +0100

    * Fixes

---
+++ Source/Krypton Toolkit/Krypton.Toolkit.Suite.Extended.Specialised.Dialogs/Globals/GlobalDeclarations.cs
@@ -0,0 +1,3 @@
+﻿global using System;
+
+global using Krypton.Toolkit.Suite.Extended.Common;
---
+++ Source/Krypton Toolkit/Krypton.Toolkit.Suite.Extended.Specialised.Dialogs/UX/Run/KryptonRunDialog.cs
@@ -0,0 +1,53 @@
+﻿namespace Krypton.Toolkit.Suite.Extended.Specialised.Dialogs
+{
+    public class KryptonRunDialog : CommonExtendedKryptonForm
+    {
+        private KryptonPanel kryptonPanel1;
+        private KryptonBorderEdge kryptonBorderEdge1;
+
+        private void InitializeComponent()
+        {
+            this.kryptonPanel1 = new Krypton.Toolkit.KryptonPanel();
+            this.kryptonBorderEdge1 = new Krypton.Toolkit.KryptonBorderEdge();
+            ((System.ComponentModel.ISupportInitialize)(this.kryptonPanel1)).BeginInit();
+            this.kryptonPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // kryptonPanel1
+            // 
+            this.kryptonPanel1.Controls.Add(this.kryptonBorderEdge1);
+            this.kryptonPanel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.kryptonPanel1.Location = new System.Drawing.Point(0, 117);
+            this.kryptonPanel1.Name = "kryptonPanel1";
+            this.kryptonPanel1.PanelBackStyle = Krypton.Toolkit.PaletteBackStyle.PanelAlternate;
+            this.kryptonPanel1.Size = new System.Drawing.Size(383, 50);
+            this.kryptonPanel1.TabIndex = 0;
+            // 
+            // kryptonBorderEdge1
+            // 
+            this.kryptonBorderEdge1.BorderStyle = Krypton.Toolkit.PaletteBorderStyle.HeaderSecondary;
+            this.kryptonBorderEdge1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.kryptonBorderEdge1.Location = new System.Drawing.Point(0, 0);
+            this.kryptonBorderEdge1.Name = "kryptonBorderEdge1";
+            this.kryptonBorderEdge1.Size = new System.Drawing.Size(383, 1);
+            this.kryptonBorderEdge1.Text = "kryptonBorderEdge1";
+            // 
+            // KryptonRunDialog
+            // 
+            this.ClientSize = new System.Drawing.Size(383, 167);
+            this.Controls.Add(this.kryptonPanel1);
+            this.Name = "KryptonRunDialog";
+            this.Load += new System.EventHandler(this.KryptonRunDialog_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.kryptonPanel1)).EndInit();
+            this.kryptonPanel1.ResumeLayout(false);
+            this.kryptonPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        private void KryptonRunDialog_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
