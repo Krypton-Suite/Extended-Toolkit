43a1ecd2abc9f9a0b0115b17dbae7cfcb7d04b25
Peter Wagner <peterwagner@live.co.uk>
2021-07-17 08:18:11 +0100

    * Toolstrip items

---
+++ Source/Krypton Toolkit/Main/Krypton.Toolkit.Suite.Extended.Tool.Strip.Items/Controls/General/MRUMenuItem.cs
@@ -0,0 +1,28 @@
+﻿#region BSD License
+/*
+ * Use of this source code is governed by a BSD-style
+ * license or other governing licenses that can be found in the LICENSE.md file or at
+ * https://raw.githubusercontent.com/Krypton-Suite/Extended-Toolkit/master/LICENSE
+ */
+#endregion
+
+using System.Windows.Forms;
+using System.Windows.Forms.Design;
+
+namespace Krypton.Toolkit.Suite.Extended.Tool.Strip.Items
+{
+    [ToolStripItemDesignerAvailability(ToolStripItemDesignerAvailability.ContextMenuStrip | ToolStripItemDesignerAvailability.MenuStrip)]
+    public class MRUMenuItem : ToolStripItem
+    {
+        #region Variables
+        private string _defaultText = "Mo&st Recently Used...";
+        #endregion
+
+        #region Constructor
+        public MRUMenuItem()
+        {
+            Text = _defaultText;
+        }
+        #endregion
+    }
+}
