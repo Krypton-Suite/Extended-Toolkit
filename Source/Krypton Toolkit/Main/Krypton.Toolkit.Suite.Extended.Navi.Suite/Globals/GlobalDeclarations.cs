801de95748e3dc1f5400fcec36b41ce3ee3b54a3
Peter Wagner <peterwagner@live.co.uk>
2021-07-17 10:49:03 +0100

    * Fix Examples

--- Source/Krypton Toolkit/Main/Krypton.Toolkit.Suite.Extended.Navi.Suite/Globals/GlobalDeclarations.cs
+++ Source/Krypton Toolkit/Main/Krypton.Toolkit.Suite.Extended.Navi.Suite/Globals/GlobalDeclarations.cs
@@ -8,9 +8,14 @@ global using System.Drawing.Design;
 global using System.Drawing.Drawing2D;
 global using System.Drawing.Imaging;
 global using System.Globalization;
+global using System.Reflection;
+global using System.Resources;
 global using System.Runtime.InteropServices;
 global using System.Runtime.Serialization;
 global using System.Security.Permissions;
 global using System.Windows.Forms;
 global using System.Windows.Forms.Design;
+global using System.Windows.Forms.Layout;
 global using System.Xml.Serialization;
+
+global using Krypton.Toolkit.Suite.Extended.Navi.Suite.Resources;
