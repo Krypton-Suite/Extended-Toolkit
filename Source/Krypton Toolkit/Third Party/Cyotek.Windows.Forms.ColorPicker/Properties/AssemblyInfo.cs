bf96234bff8e311621f3ee8a9912975e74c78d6d
Peter Wagner <peterwagner@live.co.uk>
2021-07-18 15:53:52 +0100

    * Remove unused projects

--- Source/Krypton Toolkit/Third Party/Cyotek.Windows.Forms.ColorPicker/Properties/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-﻿using System;
-using System.Reflection;
-using System.Runtime.InteropServices;
-[assembly: ComVisible(false)]
-[assembly: Guid("de546619-59b3-438e-8b5a-3d149e146b22")]
-[assembly: CLSCompliant(true)]
