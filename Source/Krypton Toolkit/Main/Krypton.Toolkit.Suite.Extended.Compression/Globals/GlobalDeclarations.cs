f4d23fe288f92c9002757da880cda6dfbf34ecde
Peter Wagner <peterwagner@live.co.uk>
2021-07-19 16:56:42 +0100

    * Rollback on schoolboy error!

--- Source/Krypton Toolkit/Main/Krypton.Toolkit.Suite.Extended.Compression/Globals/GlobalDeclarations.cs
+++
@@ -1,9 +0,0 @@
-﻿global using System;
-global using System.IO;
-global using System.Threading;
-global using System.Windows.Forms;
-
-global using Ionic.Zip;
-
-global using Krypton.Toolkit.Suite.Extended.Developer.Utilities;
-global using Krypton.Toolkit.Suite.Extended.Tools;
