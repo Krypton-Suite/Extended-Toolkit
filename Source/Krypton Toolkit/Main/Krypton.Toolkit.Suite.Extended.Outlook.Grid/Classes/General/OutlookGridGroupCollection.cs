3086cd0eea57724c39e4f595dd9cbcc5b9559486
Peter Wagner <peterwagner@live.co.uk>
2021-09-03 18:09:10 +0100

    * Files

--- Source/Krypton Toolkit/Main/Krypton.Toolkit.Suite.Extended.Outlook.Grid/Classes/General/OutlookGridGroupCollection.cs
+++ Source/Krypton Toolkit/Main/Krypton.Toolkit.Suite.Extended.Outlook.Grid/Classes/General/OutlookGridGroupCollection.cs
@@ -1,247 +1,148 @@
-﻿using System.Linq;
-
-namespace Krypton.Toolkit.Suite.Extended.Outlook.Grid
+﻿namespace Krypton.Toolkit.Suite.Extended.Outlook.Grid
 {
-    /// <summary>
-    /// List of the current columns of the OutlookGrid
+    // <summary>
+    /// List of IOutlookGridGroups
     /// </summary>
-    public class OutlookGridColumnCollection : List<OutlookGridColumn>
+    public class OutlookGridGroupCollection
     {
-        private int maxGroupIndex;
-        private int maxSortIndex;
+        #region "Variables"
+        private IOutlookGridGroup parentGroup;
+        private List<IOutlookGridGroup> groupList;
+        #endregion
 
+        #region "Constructor"        
         /// <summary>
-        /// Constructor
+        /// Initializes a new instance of the <see cref="OutlookGridGroupCollection"/> class.
         /// </summary>
-        public OutlookGridColumnCollection()
-            : base()
+        public OutlookGridGroupCollection()
         {
-            maxGroupIndex = -1;
-            maxSortIndex = -1;
+            groupList = new List<IOutlookGridGroup>();
         }
 
         /// <summary>
-        /// Gets the OutlookGridColumn in the list by its name
+        /// Initializes a new instance of the <see cref="OutlookGridGroupCollection"/> class.
         /// </summary>
-        /// <param name="columnName">The column name.</param>
-        /// <returns>OutlookGridColumn</returns>
-        public OutlookGridColumn this[string columnName]
+        /// <param name="parentGroup">The parent group, if any.</param>
+        public OutlookGridGroupCollection(IOutlookGridGroup parentGroup)
+        {
+            groupList = new List<IOutlookGridGroup>();
+            this.parentGroup = parentGroup;
+        }
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Gets or Sets the parent group
+        /// </summary>
+        public IOutlookGridGroup ParentGroup
         {
             get
             {
-                return this.Find(c => c.DataGridViewColumn.Name.Equals(columnName));
+                return this.parentGroup;
+            }
+            internal set
+            {
+                this.parentGroup = value;
             }
         }
 
         /// <summary>
-        /// Add an OutlookGridColumn to the collection.
+        /// Gets the list of IOutlookGridGroup.
         /// </summary>
-        /// <param name="item">The OutlookGridColumn to add.</param>
-        public new void Add(OutlookGridColumn item)
-        {
-            base.Add(item);
-
-            if (item.GroupIndex > -1)
-                maxGroupIndex++;
-            if (item.SortIndex > -1)
-                maxSortIndex++;
-        }
-
-        /// <summary>
-        /// Gets or Sets the maximum GroupIndex in the collection
-        /// </summary>
-        public int MaxGroupIndex
+        public List<IOutlookGridGroup> List
         {
             get
             {
-                return maxGroupIndex;
-            }
-            set
-            {
-                maxGroupIndex = value;
+                return groupList;
             }
         }
 
         /// <summary>
-        /// Gets or sets the maximum SortIndex in the collection
+        /// Gets the number of groups
         /// </summary>
-        public int MaxSortIndex
+        public int Count
         {
             get
             {
-                return maxSortIndex;
+                return groupList.Count;
             }
-            set
+        }
+
+        #endregion
+
+        #region "Public methods"
+
+        /// <summary>
+        /// Gets the Group object
+        /// </summary>
+        /// <param name="index">Index in the list of groups.</param>
+        /// <returns>The IOutlookGridGroup.</returns>
+        public IOutlookGridGroup this[int index]
+        {
+            get
             {
-                maxSortIndex = value;
+                return groupList[index];
             }
         }
 
         /// <summary>
-        /// Gets the number of columns grouped
+        /// Adds a new group
         /// </summary>
-        /// <returns>the number of columns grouped.</returns>
-        public int CountGrouped()
+        /// <param name="group">The IOutlookGridGroup.</param>
+        public void Add(IOutlookGridGroup group)
         {
-            return this.Count(c => c.IsGrouped == true);
+            groupList.Add(group);
         }
 
         /// <summary>
-        /// Gets the list of grouped columns
+        /// Sorts the groups
         /// </summary>
-        /// <returns>The list of grouped columns.</returns>
-        public List<OutlookGridColumn> FindGroupedColumns()
+        public void Sort()
         {
-            return this.Where(c => c.IsGrouped).OrderBy(c => c.GroupIndex).ToList();
+            groupList.Sort();
         }
 
         /// <summary>
-        /// Gets a list of columns which are sorted and not grouped.
+        /// Sorts the groups
         /// </summary>
-        /// <returns>List of Column indexes and SortDirection ordered by SortIndex.</returns>
-        public List<Tuple<int, SortOrder, IComparer>> GetIndexAndSortGroupedColumns()
+        internal void Sort(OutlookGridGroupCountComparer comparer)
         {
-            List<Tuple<int, SortOrder, IComparer>> res = new List<Tuple<int, SortOrder, IComparer>>();
-            var tmp = this.OrderBy(x => x.GroupIndex);
-            foreach (OutlookGridColumn col in tmp)
+            groupList.Sort(comparer);
+        }
+
+        /// <summary>
+        /// Find a group by its value
+        /// </summary>
+        /// <param name="value">The value of the group</param>
+        /// <returns>The IOutlookGridGroup.</returns>
+        public IOutlookGridGroup FindGroup(object value)
+        {
+            //We must return null if no group exist, then the OutlookGrid will create one. But we must return a group even for a null value.
+            if (value == null)
             {
-                if (col.IsGrouped && col.GroupIndex > -1)
-                    res.Add(Tuple.Create<int, SortOrder, IComparer>(col.DataGridViewColumn.Index, col.SortDirection, col.RowsComparer));
+                return groupList.Find(x => x.Value == null);
+                //return null;
             }
-            return res;
+            //return groupList.Find(x => x.Value.Equals(value));
+            return groupList.Find(x => x.Value != null && x.Value.Equals(value));
         }
 
-        /// <summary>
-        /// Gets the column from its real index (from the underlying DataGridViewColumn)
-        /// </summary>
-        /// <param name="index">The index</param>
-        /// <returns>The OutlookGridColumn.</returns>
-        public OutlookGridColumn FindFromColumnIndex(int index)
-        {
-            return this.FirstOrDefault(c => c.DataGridViewColumn.Index == index);
-        }
+        #endregion
 
-        /// <summary>
-        /// Gets the column from its name
-        /// </summary>
-        /// <param name="name">The name of the column.</param>
-        /// <returns>The associated OutlookGridColumn.</returns>
-        public OutlookGridColumn FindFromColumnName(string name)
-        {
-            return this.FirstOrDefault(x => x.Name == name);
-        }
+        #region "Internals"
 
-        /// <summary>
-        /// Gets a list of columns which are sorted and not grouped.
-        /// </summary>
-        /// <returns>List of Column indexes and SortDirection ordered by SortIndex.</returns>
-        public List<Tuple<int, SortOrder, IComparer>> GetIndexAndSortSortedOnlyColumns()
+        internal void Clear()
         {
-            var res = new List<Tuple<int, SortOrder, IComparer>>();
-            var tmp = this.OrderBy(x => x.SortIndex);
-            foreach (OutlookGridColumn col in tmp)
+            parentGroup = null;
+            //If a group is collapsed the rows will not appear. Then if we clear the group the rows should not remain "collapsed"
+            for (int i = 0; i < groupList.Count; i++)
             {
-                if (!col.IsGrouped && col.SortIndex > -1)
-                {
-                    res.Add(Tuple.Create(col.DataGridViewColumn.Index, col.SortDirection, col.RowsComparer));
-                }
+                groupList[i].Collapsed = false;
             }
-            return res;
+            groupList.Clear();
         }
 
-        /// <summary>
-        /// Removes a groupIndex and update the GroupIndex for all columns
-        /// </summary>
-        /// <param name="col">The OutlookGridColumn that will be removed.</param>
-        internal void RemoveGroupIndex(OutlookGridColumn col)
-        {
-            int removed = col.GroupIndex;
-            for (int i = 0; i < this.Count; i++)
-            {
-                if (this[i].GroupIndex > removed)
-                {
-                    this[i].GroupIndex--;
-                }
-            }
-            maxGroupIndex--;
-            col.GroupIndex = -1;
-        }
-
-        /// <summary>
-        ///  Removes a SortIndex and update the SortIndex for all columns
-        /// </summary>
-        /// <param name="col">The OutlookGridColumn that will be removed.</param>
-        internal void RemoveSortIndex(OutlookGridColumn col)
-        {
-            int removed = col.SortIndex;
-            for (int i = 0; i < this.Count; i++)
-            {
-                if (this[i].SortIndex > removed)
-                {
-                    this[i].SortIndex--;
-                }
-            }
-            maxSortIndex--;
-            col.SortIndex = -1;
-        }
-
-        internal void ChangeGroupIndex(OutlookGridColumn outlookGridColumn)
-        {
-            int currentGroupIndex = -1;
-            int newGroupIndex = outlookGridColumn.GroupIndex;
-
-            for (int i = 0; i < this.Count; i++)
-            {
-                if (this[i].Name == outlookGridColumn.Name)
-                {
-                    currentGroupIndex = this[i].GroupIndex;
-                }
-            }
-
-            if (currentGroupIndex == -1)
-                throw new Exception("OutlookGrid : Unable to interpret the change of GroupIndex!");
-
-#if (DEBUG)
-            Console.WriteLine("currentGroupIndex=" + currentGroupIndex.ToString());
-            Console.WriteLine("newGroupIndex=" + newGroupIndex.ToString());
-            Console.WriteLine("Before");
-            DebugOutput();
-#endif
-
-            for (int i = 0; i < this.Count; i++)
-            {
-                if (this[i].IsGrouped)
-                {
-                    if (this[i].GroupIndex == currentGroupIndex)
-                    {
-                        this[i].GroupIndex = newGroupIndex;
-                    }
-                    else if ((this[i].GroupIndex >= newGroupIndex) && (this[i].GroupIndex < currentGroupIndex))
-                    {
-                        this[i].GroupIndex++;
-                    }
-                    else if ((this[i].GroupIndex <= newGroupIndex) && (this[i].GroupIndex > currentGroupIndex))
-                    {
-                        this[i].GroupIndex--;
-                    }
-                }
-            }
-#if (DEBUG)
-            Console.WriteLine("After");
-            DebugOutput();
-#endif
-        }
-
-        /// <summary>
-        /// Outputs Debug information to the console.
-        /// </summary>
-        public void DebugOutput()
-        {
-            for (int i = 0; i < this.Count; i++)
-            {
-                Console.WriteLine(this[i].Name + " , GroupIndex=" + this[i].GroupIndex.ToString() + ", SortIndex=" + this[i].SortIndex.ToString());
-            }
-            Console.WriteLine("MaxGroupIndex=" + maxGroupIndex.ToString() + ", MaxSortIndex=" + maxSortIndex.ToString());
-        }
+        #endregion
     }
 }
