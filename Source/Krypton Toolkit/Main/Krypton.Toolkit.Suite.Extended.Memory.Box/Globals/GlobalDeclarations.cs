57ef13515451026ce740cebd70b099d5288ab749
Peter Wagner <peterwagner@live.co.uk>
2021-09-28 19:05:48 +0100

    * Purge

--- Source/Krypton Toolkit/Main/Krypton.Toolkit.Suite.Extended.Memory.Box/Globals/GlobalDeclarations.cs
+++
@@ -1,6 +0,0 @@
-﻿global using System;
-global using System.Drawing;
-global using System.Windows.Forms;
-
-global using Krypton.Toolkit.Suite.Extended.Common;
-global using Krypton.Toolkit.Suite.Extended.Memory.Box.Properties;
