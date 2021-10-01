7c3a4ebbd3276cd32d47ab8bf01804fc1843fc68
Peter Wagner <peterwagner@live.co.uk>
2021-10-01 12:03:55 +0100

    * Simplify

--- Source/Krypton Toolkit/Main/Krypton.Toolkit.Suite.Extended.CheckSum.Tools/Globals/GlobalDeclarations.cs
+++
@@ -1,9 +0,0 @@
-﻿global using System;
-global using System.ComponentModel;
-global using System.IO;
-global using System.Security.Cryptography;
-global using System.Text;
-global using System.Windows.Forms;
-
-global using Krypton.Toolkit.Suite.Extended.CheckSum.Tools.Properties;
-global using Krypton.Toolkit.Suite.Extended.Tools;
--- Source/Krypton Toolkit/Shared/Krypton.Toolkit.Suite.Extended.Common/Classes/General/GlobalValues.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Krypton.Toolkit.Suite.Extended.Common
-{
-    public class GlobalValues
-    {
-        #region Constants
-        public const int DEFAULT_CORNER_ROUNDING_VALUE = 5;
-        #endregion
-    }
-}
