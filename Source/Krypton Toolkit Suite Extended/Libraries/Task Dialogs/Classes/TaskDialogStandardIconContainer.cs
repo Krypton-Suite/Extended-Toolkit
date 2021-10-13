f5fa88354ad82a843857f77a735ac7f8eb6caed6
Peter Wagner <peterwagner@live.co.uk>
2021-10-13 12:22:50 +0100

    * Purge

--- Source/Krypton Toolkit Suite Extended/Libraries/Task Dialogs/Classes/TaskDialogStandardIconContainer.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Krypton.Toolkit.Suite.Extended.Task.Dialogs
-{
-    internal class TaskDialogStandardIconContainer : TaskDialogIcon
-    {
-        public TaskDialogStandardIconContainer(TaskDialogStandardIcon icon) : base() => Icon = icon;
-
-        public TaskDialogStandardIcon Icon { get; }
-    }
-}
