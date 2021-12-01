4e0b40bd491ffb9afc11e12dcbe58dd250c1bd7b
Peter Wagner <peterwagner@live.co.uk>
2021-12-01 11:16:45 +0000

    * Purge

--- Source/Krypton Toolkit/Main/Krypton.Toolkit.Suite.Extended.IO/Globals/GlobalDeclarations.cs
+++
@@ -1,6 +0,0 @@
-﻿global using System;
-global using System.Collections;
-global using System.ComponentModel;
-global using System.Drawing;
-global using System.IO;
-global using System.Windows.Forms;
--- Source/Krypton Toolkit/Main/Krypton.Toolkit.Suite.Extended.Toggle.Switch/Events/BeforeRenderingEventArgs.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Krypton.Toolkit.Suite.Extended.Toggle.Switch
-{
-    public class BeforeRenderingEventArgs
-    {
-        public ToggleSwitchRendererBase Renderer { get; set; }
-
-        public BeforeRenderingEventArgs(ToggleSwitchRendererBase renderer) => Renderer = renderer;
-    }
-}
