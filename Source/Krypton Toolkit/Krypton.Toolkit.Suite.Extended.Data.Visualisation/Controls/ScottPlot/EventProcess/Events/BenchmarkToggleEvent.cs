3a4745f252b00332aaa241b052855b6992b81185
Peter Wagner <peterwagner@live.co.uk>
2021-12-09 16:17:13 +0000

    * Classes

---
+++ Source/Krypton Toolkit/Krypton.Toolkit.Suite.Extended.Data.Visualisation/Controls/ScottPlot/EventProcess/Events/BenchmarkToggleEvent.cs
@@ -0,0 +1,24 @@
+﻿namespace Krypton.Toolkit.Suite.Extended.Data.Visualisation.ScottPlot
+{
+    /// <summary>
+    /// This event toggles visibility of the benchmark.
+    /// This event is typically called after double-clicking the plot.
+    /// </summary>
+    public class BenchmarkToggleEvent : IUIEvent
+    {
+        private readonly Plot Plot;
+        private readonly Configuration Configuration;
+        public RenderType RenderType => Configuration.QualityConfiguration.BenchmarkToggle;
+
+        public BenchmarkToggleEvent(Plot plt, Configuration config)
+        {
+            Plot = plt;
+            Configuration = config;
+        }
+
+        public void ProcessEvent()
+        {
+            Plot.Benchmark(!Plot.Benchmark(null));
+        }
+    }
+}
---
+++ Source/Krypton Toolkit/Krypton.Toolkit.Suite.Extended.Data.Visualisation/Controls/ScottPlot/EventProcess/Events/MouseAxisAutoEvent.cs
@@ -0,0 +1,31 @@
+﻿namespace Krypton.Toolkit.Suite.Extended.Data.Visualisation.ScottPlot
+{
+    /// <summary>
+    /// This event calls AxisAuto() on all axes.
+    /// This is typically called after middle-clicking.
+    /// </summary>
+    public class MouseAxisAutoEvent : IUIEvent
+    {
+        private readonly Configuration Configuration;
+        private readonly Settings Settings;
+        private readonly Plot Plot;
+        public RenderType RenderType => Configuration.QualityConfiguration.AutoAxis;
+
+        public MouseAxisAutoEvent(Configuration config, Settings settings, Plot plt)
+        {
+            Configuration = config;
+            Settings = settings;
+            Plot = plt;
+        }
+
+        public void ProcessEvent()
+        {
+            Settings.ZoomRectangle.Clear();
+
+            if (Configuration.LockVerticalAxis)
+                return;
+
+            Plot.AxisAuto();
+        }
+    }
+}
