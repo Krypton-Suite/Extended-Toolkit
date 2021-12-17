a6f388842da0acf92e2374f02aadb94657376e49
Wagnerp <peterwagner@live.co.uk>
2021-12-17 09:44:00 +0000

    * Files

--- Source/Krypton Toolkit/Examples/DataVisualisation/ScottPlotDemo.cs
+++ Source/Krypton Toolkit/Examples/DataVisualisation/ScottPlotDemo.cs
@@ -6,7 +6,18 @@ using System.Threading.Tasks;
 
 namespace DataVisualisation
 {
-    internal class ScottPlotDemo
+    public class ScottPlotDemo : Form
     {
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // ScottPlotDemo
+            // 
+            this.ClientSize = new System.Drawing.Size(1037, 637);
+            this.Name = "ScottPlotDemo";
+            this.ResumeLayout(false);
+
+        }
     }
 }
