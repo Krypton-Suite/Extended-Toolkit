3cfb4082cfe30c27704985da181784fcc6c23e54
Wagnerp <peterwagner@live.co.uk>
2021-12-18 11:46:50 +0000

    * Demos

---
+++ Source/Krypton Toolkit/Examples/DataVisualisation/ScottPlot/Demos/ShowValueOnHover2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Krypton.Toolkit;
+
+namespace DataVisualisation.ScottPlot.Demos
+{
+    public partial class ShowValueOnHover2 : KryptonForm
+    {
+        public ShowValueOnHover2()
+        {
+            InitializeComponent();
+        }
+    }
+}
