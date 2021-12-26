3f68a65893853c95e15337230d11cafb6d32add7
Wagnerp <peterwagner@live.co.uk>
2021-12-26 11:22:54 +0000

    * Build 2201

---
+++ Source/Krypton Toolkit/Krypton.Toolkit.Suite.Extended.Utilities/Classes/System/SAPI/Grammar Building/RuleElement.cs
@@ -0,0 +1,83 @@
+﻿#region BSD License
+/*
+ * Use of this source code is governed by a BSD-style
+ * license or other governing licenses that can be found in the LICENSE.md file or at
+ * https://raw.githubusercontent.com/Krypton-Suite/Extended-Toolkit/master/LICENSE
+ */
+#endregion
+
+using Krypton.Toolkit.Suite.Extended.Utilities.System.SrgsParser;
+
+namespace Krypton.Toolkit.Suite.Extended.Utilities.System.GrammarBuilding
+{
+    internal sealed class RuleElement : BuilderElements
+    {
+        private readonly string _name;
+
+        private string _ruleName;
+
+        private IRule _rule;
+
+        internal override string DebugSummary => _name + "=" + base.DebugSummary;
+
+        internal string Name => _name;
+
+        internal string RuleName => _ruleName;
+
+        internal RuleElement(string name)
+        {
+            _name = name;
+        }
+
+        internal RuleElement(GrammarBuilderBase builder, string name)
+            : this(name)
+        {
+            Add(builder);
+        }
+
+        public override bool Equals(object obj)
+        {
+            RuleElement ruleElement = obj as RuleElement;
+            if (ruleElement == null)
+            {
+                return false;
+            }
+            if (!base.Equals(obj))
+            {
+                return false;
+            }
+            return _name == ruleElement._name;
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        internal override GrammarBuilderBase Clone()
+        {
+            RuleElement ruleElement = new RuleElement(_name);
+            ruleElement.CloneItems(this);
+            return ruleElement;
+        }
+
+        internal override IElement CreateElement(IElementFactory elementFactory, IElement parent, IRule rule, IdentifierCollection ruleIds)
+        {
+            if (_rule == null)
+            {
+                IGrammar grammar = elementFactory.Grammar;
+                _ruleName = ruleIds.CreateNewIdentifier(Name);
+                _rule = grammar.CreateRule(_ruleName, RulePublic.False, RuleDynamic.NotSet, false);
+                CreateChildrenElements(elementFactory, _rule, ruleIds);
+                _rule.PostParse(grammar);
+            }
+            return _rule;
+        }
+
+        internal override int CalcCount(BuilderElements parent)
+        {
+            _rule = null;
+            return base.CalcCount(parent);
+        }
+    }
+}
---
+++ Source/Krypton Toolkit/Krypton.Toolkit.Suite.Extended.VirtualTreeColumnView/InternalTreeColumnView.Designer.cs
@@ -0,0 +1,49 @@
+﻿
+using System;
+
+namespace Krypton.Toolkit.Suite.Extended.VirtualTreeColumnView
+{
+    partial class InternalTreeColumnView
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // InternalTreeColumnView
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.DoubleBuffered = true;
+            this.Margin = new System.Windows.Forms.Padding(0);
+            this.Name = "InternalTreeColumnView";
+            this.Resize += new System.EventHandler(this.treePanel_Resize);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
