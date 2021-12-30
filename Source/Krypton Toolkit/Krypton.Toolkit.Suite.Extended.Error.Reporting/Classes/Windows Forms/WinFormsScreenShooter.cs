21cafa8d324c85c6cb83c0efbe62dd375bcb33da
Wagnerp <peterwagner@live.co.uk>
2021-12-30 16:25:52 +0000

    * Code

---
+++ Source/Krypton Toolkit/Krypton.Toolkit.Suite.Extended.Error.Reporting/Classes/Windows Forms/WinFormsScreenShooter.cs
@@ -0,0 +1,43 @@
+﻿namespace Krypton.Toolkit.Suite.Extended.Error.Reporting
+{
+    /// <summary>
+    /// Utility to take a screenshot and return as a graphic file 
+    /// </summary>
+    internal class WinFormsScreenShooter : IScreenShooter
+    {
+        private const string ScreenshotFileName = "exceptionreport-screenshot.jpg";
+
+        /// <summary> Take a screenshot (supports multiple monitors) </summary>
+        /// <returns>temp file name of JPEG image</returns>
+        public string TakeScreenShot()
+        {
+            var rectangle = Rectangle.Empty;
+
+            foreach (var screen in Screen.AllScreens)
+            {
+                rectangle = Rectangle.Union(rectangle, screen.Bounds);
+            }
+
+            using (var bitmap = new Bitmap(rectangle.Width, rectangle.Height, PixelFormat.Format32bppArgb))
+            {
+                using (var graphics = Graphics.FromImage(bitmap))
+                {
+                    graphics.CopyFromScreen(rectangle.X, rectangle.Y, 0, 0, rectangle.Size, CopyPixelOperation.SourceCopy);
+                }
+
+                return GetImageAsFile(bitmap);
+            }
+        }
+
+        /// <summary>
+        /// Return the supplied Bitmap, as a file on the system, in JPEG format
+        /// </summary>
+        /// <param name="bitmap">The Bitmap to save</param>
+        private static string GetImageAsFile(Image bitmap)
+        {
+            var tempFileName = Path.GetTempPath() + ScreenshotFileName; // image is not deleted but same file is reused
+            bitmap.Save(tempFileName, ImageFormat.Jpeg);
+            return tempFileName;
+        }
+    }
+}
---
+++ Source/Krypton Toolkit/Krypton.Toolkit.Suite.Extended.Error.Reporting/UX/MVP/Views/ExceptionReportView.cs
@@ -0,0 +1,91 @@
+﻿#pragma warning disable 1591
+namespace Krypton.Toolkit.Suite.Extended.Error.Reporting
+{
+    public partial class ExceptionReportView : KryptonForm, IExceptionReportView
+    {
+        #region Design Code
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // ExceptionReportView
+            // 
+            this.ClientSize = new System.Drawing.Size(551, 416);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "ExceptionReportView";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.ResumeLayout(false);
+
+        }
+        #endregion
+
+        #region IExceptionReportView Implementation
+        public string ProgressMessage { set => throw new NotImplementedException(); }
+        public bool EnableEmailButton { set => throw new NotImplementedException(); }
+        public bool ShowProgressBar { set => throw new NotImplementedException(); }
+        public bool ShowFullDetail { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public string UserExplanation => throw new NotImplementedException();
+
+        public void MapiSendCompleted()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetInProgressState()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void PopulateExceptionTab(IEnumerable<Exception> exceptions)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void PopulateAssembliesTab()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void PopulateSysInfoTab()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetProgressCompleteState()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ToggleShowFullDetail()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ShowWindow()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Completed(bool success)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ShowError(string message, Exception exception)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+
+        #region Constructor
+        public ExceptionReportView(ExceptionReportInfo reportInfo)
+        {
+            
+        }
+        #endregion
+    }
+}
