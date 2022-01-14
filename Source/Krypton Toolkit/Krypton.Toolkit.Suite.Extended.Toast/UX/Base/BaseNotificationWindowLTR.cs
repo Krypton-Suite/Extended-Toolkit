6e4c560fbc7c6f09ee7391c38e7f39190516a884
Peter Wagner <peterwagner@live.co.uk>
2022-01-14 16:41:16 +0000

    * Base UI for Toasts

---
+++ Source/Krypton Toolkit/Krypton.Toolkit.Suite.Extended.Toast/UX/Base/BaseNotificationWindowLTR.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Krypton.Toolkit.Suite.Extended.Toast
+{
+    internal class BaseNotificationWindowLTR : KryptonForm
+    {
+        private KryptonPanel kryptonPanel1;
+        private KryptonButton kbtnDismiss;
+        private KryptonBorderEdge kryptonBorderEdge1;
+        private KryptonPanel kryptonPanel2;
+
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // BaseNotificationWindow
+            // 
+            this.ClientSize = new System.Drawing.Size(609, 293);
+            this.Name = "BaseNotificationWindow";
+            this.ResumeLayout(false);
+
+        }
+    }
+}
---
+++ Source/Krypton Toolkit/Krypton.Toolkit.Suite.Extended.Toast/UX/Base/BaseNotificationWindowRTL.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Krypton.Toolkit.Suite.Extended.Toast
+{
+    internal class BaseNotificationWindowRTL : KryptonForm
+    {
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // BaseNotificationWindowRTL
+            // 
+            this.ClientSize = new System.Drawing.Size(609, 293);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "BaseNotificationWindowRTL";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.ResumeLayout(false);
+
+        }
+    }
+}
