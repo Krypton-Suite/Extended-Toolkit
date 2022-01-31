36c126368d8eeafa03036b1a2669d247b74e1364
Peter Wagner <peterwagner@live.co.uk>
2022-01-31 09:00:01 +0000

    Update KryptonBrowseButton.cs

--- Source/Krypton Toolkit/Krypton.Toolkit.Suite.Extended.Buttons/Controls Toolkit/General/KryptonBrowseButton.cs
+++ Source/Krypton Toolkit/Krypton.Toolkit.Suite.Extended.Buttons/Controls Toolkit/General/KryptonBrowseButton.cs
@@ -25,64 +25,56 @@ namespace Krypton.Toolkit.Suite.Extended.Buttons
         #endregion
 
         #region Properties
-        public bool OpenFile
-        {
-            get => _openFile;
-
-            set
-            {
-                if (_openFile != value)
-                {
-                    _openFile = value;
-                }
-
-                if (_openFile)
-                {
-                    SetBrowseType(_openFile, false);
-                }
-            }
-        }
-
-        public bool SaveFile
-        {
-            get => _saveFile;
-
-            set
-            {
-                if (_saveFile != value)
-                {
-                    _saveFile = value;
-                }
-
-                if (_saveFile)
-                {
-                    SetBrowseType(false, true);
-                }
-            }
-        }
-
+        /// <summary>Gets or sets a value indicating whether [use system folder browser].</summary>
+        /// <value><c>true</c> if [use system folder browser]; otherwise, <c>false</c>.</value>
+        [DefaultValue(true), Description("Use the standard WinForms folder browser dialog.")]
         public bool UseSystemFolderBrowser { get => _useSystemFolderBrowser; set => _useSystemFolderBrowser = value; }
 
+        /// <summary>Gets or sets a value indicating whether [show new folder button on dialog].</summary>
+        /// <value><c>true</c> if [show new folder button on dialog]; otherwise, <c>false</c>.</value>
+        [DefaultValue(false), Description("Shows the new folder button on the standard WinForms folder browser dialog.")]
         public bool ShowNewFolderButtonOnDialog { get => _showNewFolderButtonOnDialog; set => _showNewFolderButtonOnDialog = value; }
 
+        /// <summary>Gets or sets a value indicating whether [use windows API code pack dialogs].</summary>
+        /// <value><c>true</c> if [use windows API code pack dialogs]; otherwise, <c>false</c>.</value>
+        [DefaultValue(false), Description("Use the Windows API Code Pack versions of system dialogs. (These provide further extensibility over the standard Windows versions)")]
         public bool UseWindowsAPICodePackDialogs { get => _useSystemFolderBrowser; set => _useSystemFolderBrowser = value; }
 
+        [DefaultValue(false), Description("Allows the user to select multiple files in a open file dialog. To be used in conjunction with the Windows API Code Pack version.")]
         public bool AllowMultiSelect { get => _allowMultiSelect; set => _allowMultiSelect = value; }
 
+        /// <summary>Gets or sets the dialog title.</summary>
+        /// <value>The dialog title.</value>
+        [DefaultValue(""), Description("The title of the dialog.")]
         public string DialogTitle { get => _dialogTitle; set => _dialogTitle = value; }
 
+        /// <summary>Gets the file path.</summary>
+        /// <value>The file path.</value>
+        [DefaultValue(""), Description("Displays the complete file path of the object.")]
         public string FilePath { get => _filePath; private set => _filePath = value; }
 
+        /// <summary>Gets or sets the folder browser dialog description.</summary>
+        /// <value>The folder browser dialog description.</value>
         public string FolderBrowserDialogDescription { get => _folderBrowserDialogDescription; set => _folderBrowserDialogDescription = value; }
 
+        /// <summary>Gets or sets the display name of the raw.</summary>
+        /// <value>The display name of the raw.</value>
         public string RawDisplayName { get => _rawDisplayName; set => _rawDisplayName = value; }
 
+        /// <summary>Gets or sets the extension list.</summary>
+        /// <value>The extension list.</value>
         public string ExtensionList { get => _extensionList; set => _extensionList = value; }
 
+        /// <summary>Gets or sets the dialog filter.</summary>
+        /// <value>The dialog filter.</value>
         public string[] DialogFilter { get => _dialogFilter; set => _dialogFilter = value; }
 
+        /// <summary>Gets or sets the browse button action.</summary>
+        /// <value>The browse button action.</value>
         public BrowseButtonAction BrowseButtonAction { get => _browseButtonAction; set => _browseButtonAction = value; }
 
+        /// <summary>Gets or sets the file names.</summary>
+        /// <value>The file names.</value>
         public IEnumerable<string> FileNames { get => _fileNames; set => _fileNames = value; }
         #endregion
 
@@ -96,15 +88,6 @@ namespace Krypton.Toolkit.Suite.Extended.Buttons
             Size = new Size(34, 25);
 
             BrowseButtonAction = BrowseButtonAction.SAVEFILE;
-        }
-        #endregion
-
-        #region Methods
-        private void SetBrowseType(bool openFile, bool saveFile)
-        {
-            _openFile = openFile;
-
-            _saveFile = saveFile;
         }
         #endregion
 
