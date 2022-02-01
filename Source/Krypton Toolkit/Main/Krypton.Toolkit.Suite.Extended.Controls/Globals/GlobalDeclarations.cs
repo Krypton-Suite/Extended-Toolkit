b89e236ae6eb62a9867bfa206d8817abf83a63de
Peter Wagner <peterwagner@live.co.uk>
2022-02-01 16:47:46 +0000

    * Purge

--- Source/Krypton Toolkit/Main/Krypton.Toolkit.Suite.Extended.Controls/Globals/GlobalDeclarations.cs
+++
@@ -1,7 +0,0 @@
-﻿global using System;
-global using System.ComponentModel;
-global using System.Drawing;
-global using System.Drawing.Drawing2D;
-global using System.Windows.Forms;
-
-global using Krypton.Toolkit.Suite.Extended.Developer.Utilities;
