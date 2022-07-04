3c1e76be2680d1fc4daa845aa16437d25a6ac6e8
Peter Wagner <peterwagner@live.co.uk>
2022-07-04 08:15:02 +0100

    * Last minute fixes

---
+++ Source/Krypton Toolkit/Krypton.Toolkit.Suite.Extended.IO/Classes/MRU/MostRecentlyUsedFileManager.cs
@@ -0,0 +1,180 @@
+﻿#region BSD License
+/*
+ * Use of this source code is governed by a BSD-style
+ * license or other governing licenses that can be found in the LICENSE.md file or at
+ * https://raw.githubusercontent.com/Krypton-Suite/Extended-Toolkit/master/LICENSE
+ */
+#endregion
+
+using Krypton.Toolkit.Suite.Extended.Developer.Utilities;
+
+using Microsoft.Win32;
+
+namespace Krypton.Toolkit.Suite.Extended.IO
+{
+    public class MostRecentlyUsedFileManager
+    {
+        #region Instance Fields
+
+        private ToolStripMenuItem _parentMenuItem;
+
+        private string _applicationName;
+
+        private string _clearListText;
+
+        private string _subKeyName;
+
+        #endregion
+
+        #region Events
+
+        private Action<object, EventArgs> OnRecentFileClick;
+
+        private Action<object, EventArgs> OnClearRecentFilesClick;
+
+        #endregion
+
+        #region Identity
+
+        static MostRecentlyUsedFileManager() {}
+
+        public MostRecentlyUsedFileManager()
+        {
+            
+        }
+
+        #endregion
+
+        #region Implementation
+
+        private void OnRecentFile_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void OnClearRecentFiles_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                RegistryKey key = Registry.CurrentUser.OpenSubKey(_subKeyName, true);
+
+                if (key == null)
+                {
+                    return;
+                }
+
+                string[] keyValues = key.GetValueNames();
+
+                foreach (string keyValue in keyValues)
+                {
+                    key.DeleteValue(keyValue);
+                }
+
+                key.Close();
+
+                _parentMenuItem.DropDownItems.Clear();
+
+                _parentMenuItem.Enabled = false;
+            }
+            catch (Exception exc)
+            {
+                ExceptionCapture.CaptureException(exc);
+            }
+
+            if (OnClearRecentFilesClick != null)
+            {
+                OnClearRecentFilesClick(sender, e);
+            }
+        }
+
+        private void RefreshRecentFilesMenu()
+        {
+            RegistryKey key;
+
+            string value;
+
+            ToolStripItem item;
+
+            try
+            {
+                key = Registry.CurrentUser.OpenSubKey(_subKeyName, false);
+
+                if (key == null)
+                {
+                    _parentMenuItem.Enabled = false;
+
+                    return;
+                }
+            }
+            catch (Exception e)
+            {
+                ExceptionCapture.CaptureException(e);
+
+                return;
+            }
+
+            _parentMenuItem.DropDownItems.Clear();
+
+            string[] valueNames = key.GetValueNames();
+
+            foreach (string valueName in valueNames)
+            {
+                value = key.GetValue(valueName, null) as string;
+
+                if (value == null)
+                {
+                    continue;
+                }
+
+                item = _parentMenuItem.DropDownItems.Add(value);
+
+                item.Click += new EventHandler(OnRecentFileClick);
+            }
+
+            if (_parentMenuItem.DropDownItems.Count == 0)
+            {
+                _parentMenuItem.Enabled = false;
+
+                return;
+            }
+
+            _parentMenuItem.DropDownItems.Add("-");
+
+            item = _parentMenuItem.DropDownItems.Add(_clearListText);
+
+            item.Click += OnClearRecentFiles_Click;
+
+            _parentMenuItem.Enabled = true;
+        }
+
+        public static void AddRecentFile(string fileNameWithFullPath)
+        {
+            MostRecentlyUsedFileManager manager = new MostRecentlyUsedFileManager();
+
+            string value;
+
+            try
+            {
+                RegistryKey key = Registry.CurrentUser.CreateSubKey(manager._subKeyName, RegistryKeyPermissionCheck.ReadWriteSubTree);
+
+                for (int i = 0; true; i++)
+                {
+                    value = key.GetValue(i.ToString(), null) as string;
+
+                    if (value == null)
+                    {
+                        
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                ExceptionCapture.CaptureException(e);
+            }
+
+            manager.RefreshRecentFilesMenu();
+        }
+
+        #endregion
+    }
+}
--- Source/Krypton Toolkit/Krypton.Toolkit.Suite.Extended.Tool.Strip.Items/Controls/General/MRUOpenFileMenuItem.cs
+++ Source/Krypton Toolkit/Krypton.Toolkit.Suite.Extended.Tool.Strip.Items/Controls/General/MRUOpenFileMenuItem.cs
@@ -35,42 +35,56 @@ namespace Krypton.Toolkit.Suite.Extended.Tool.Strip.Items
         
         private MostRecentlyUsedFileManager _recentlyUsedFileManager = null;
 
-        private GlobalMethods _globalMethods = new GlobalMethods();
-
-        private UtilityMethods _utilityMethods = new UtilityMethods();
-
         #endregion
 
         #region Public
-        [DefaultValue(false), Description("")]
+        /// <summary>Gets or sets a value indicating whether to use the native system dialogs over the Windows API CodePack types.</summary>
+        /// <value> <c>true</c> if [use system dialogs]; otherwise, <c>false</c>.</value>
+        [DefaultValue(false), Description("Use the native system dialogs over the Windows API CodePack types.")]
         public bool UseSystemDialogs { get => _useSystemDialogs; set => _useSystemDialogs = value; }
 
-        [DefaultValue(null), Description("")]
+        /// <summary>Gets or sets the control to load the file content text into.</summary>
+        /// <value>The control to load the file content text into.</value>
+        [DefaultValue(null), Description("The control to load the file content text into.")]
         public Control OutputControl { get => _outputControl; set => _outputControl = value; }
 
-        [DefaultValue("O&pen"), Description("")]
+        /// <summary>Gets or sets the text displayed on the tool strip menu item.</summary>
+        /// <value>The text displayed on the tool strip menu item.</value>
+        [DefaultValue("O&pen"), Description("The text displayed on the tool strip menu item.")]
         public string DefaultText { get => _defaultText; set => _defaultText = value; }
 
-        [DefaultValue(null), Description("")]
+        /// <summary>Gets or sets the name of the name of your application. This is used to store the MRU list in the Windows registry.</summary>
+        /// <value>The name of the application.</value>
+        [DefaultValue(null), Description("The name of your application. This is used to store the MRU list in the Windows registry.")]
         public string ApplicationName { get => _applicationName; set => _applicationName = value; }
 
-        [DefaultValue("Open:"), Description("")]
+        /// <summary>Gets or sets the open file dialog title.</summary>
+        /// <value>The open file dialog title.</value>
+        [DefaultValue("Open:"), Description("The title of the open file dialog.")]
         public string OpenFileDialogTitle { get => _openFileDialogTitle; set => _openFileDialogTitle = value; }
 
-        [DefaultValue(null), Description("")]
+        /// <summary>Gets or sets the display name of the raw filter. Please refer to <see cref="CommonFileDialogFilter"/> documentation for more information.</summary>
+        /// <value>The display name of the raw filter.</value>
+        [DefaultValue(null), Description("The display name of the raw filter.")]
         public string RawFilterDisplayName { get => _rawFilterDisplayName; set => _rawFilterDisplayName = value; }
 
-        [DefaultValue(null), Description("")]
+        /// <summary>Gets or sets the raw extension list. Please refer to <see cref="CommonFileDialogFilter"/> documentation for more information.</summary>
+        /// <value>The raw extension list.</value>
+        [DefaultValue(null), Description("The raw extension list.")]
         public string RawExtensionList { get => _rawExtensionList; set => _rawExtensionList = value; }
 
         [DefaultValue(null), Description("")]
         public string StandardDialogFilter { get => _standardDialogFilter; set => _standardDialogFilter = value; }
 
-        [DefaultValue(Environment.SpecialFolder.MyDocuments), Description("")]
+        /// <summary>Gets or sets the starting directory of the open file dialog.</summary>
+        /// <value>The starting directory of the open file dialog.</value>
+        [DefaultValue(Environment.SpecialFolder.MyDocuments), Description("The starting directory of the open file dialog.")]
         public string StartingDirectory { get => _startingDirectory; set => _startingDirectory = value; }
 
-        [DefaultValue(null), Description("")]
-        public MRUMenuItem ParentMruMenuItem { get => _parentMruMenuItem; set => _parentMruMenuItem = value; }
+        /// <summary>Gets or sets the parent MRU menu item.</summary>
+        /// <value>The parent MRU menu item.</value>
+        [DefaultValue(null), Description("The parent MRU menu item.")]
+        public MRUMenuItem ParentMRUMenuItem { get => _parentMruMenuItem; set => _parentMruMenuItem = value; }
 
         #endregion
 
@@ -103,6 +117,10 @@ namespace Krypton.Toolkit.Suite.Extended.Tool.Strip.Items
             _parentMruMenuItem = null;
 
             _recentlyUsedFileManager = new MostRecentlyUsedFileManager(_parentMruMenuItem, _applicationName, null, null);
+
+            ShortcutKeys = Keys.Control | Keys.O;
+
+            ShortcutKeyDisplayString = "Ctrl + O";
         }
 
         public MRUOpenFileMenuItem(string defaultText)
