d0825c6a4295acf25eca442d378ffebc7a5e3265
Peter Wagner <peterwagner@live.co.uk>
2022-08-13 15:21:28 +0100

    * Updates

--- Source/Krypton Toolkit/TestApp/ToastNotificationExample.Designer.cs
+++ Source/Krypton Toolkit/TestApp/ToastNotificationExample.Designer.cs
@@ -30,11 +30,14 @@
         {
             this.kryptonToastNotificationManager1 = new Krypton.Toolkit.Suite.Extended.Notifications.KryptonToastNotificationManager();
             this.kryptonToastNotificationPopup1 = new Krypton.Toolkit.Suite.Extended.Notifications.KryptonToastNotificationPopup();
+            this.kryptonToastNotificationManager2 = new Krypton.Toolkit.Suite.Extended.Toast.KryptonToastNotificationManager();
+            this.kryptonToastNotificationManager3 = new Krypton.Toolkit.Suite.Extended.Notifications.KryptonToastNotificationManager();
+            this.kryptonToastNotificationPopup2 = new Krypton.Toolkit.Suite.Extended.Notifications.KryptonToastNotificationPopup();
             this.SuspendLayout();
             // 
             // kryptonToastNotificationManager1
             // 
-            this.kryptonToastNotificationManager1.Action = Krypton.Toolkit.Suite.Extended.Notifications.KryptonNotificationActionType.Default;
+            this.kryptonToastNotificationManager1.Action = Krypton.Toolkit.Suite.Extended.Notifications.ActionType.DEFAULT;
             this.kryptonToastNotificationManager1.ActionButtonText = null;
             this.kryptonToastNotificationManager1.BorderColourOne = System.Drawing.Color.Empty;
             this.kryptonToastNotificationManager1.BorderColourTwo = System.Drawing.Color.Empty;
@@ -43,7 +46,7 @@
             this.kryptonToastNotificationManager1.CustomIconImage = null;
             this.kryptonToastNotificationManager1.DismissButtonText = null;
             this.kryptonToastNotificationManager1.HeaderText = null;
-            this.kryptonToastNotificationManager1.IconType = Krypton.Toolkit.Suite.Extended.Notifications.KryptonNotificationIconType.Custom;
+            this.kryptonToastNotificationManager1.IconType = Krypton.Toolkit.Suite.Extended.Notifications.IconType.CUSTOM;
             this.kryptonToastNotificationManager1.ProcessPath = null;
             this.kryptonToastNotificationManager1.Seconds = 0;
             this.kryptonToastNotificationManager1.ShowActionButton = false;
@@ -65,6 +68,50 @@
             this.kryptonToastNotificationPopup1.TitleFont = new System.Drawing.Font("Segoe UI", 9F);
             this.kryptonToastNotificationPopup1.TitleText = null;
             // 
+            // kryptonToastNotificationManager2
+            // 
+            this.kryptonToastNotificationManager2.ContentAreaType = Krypton.Toolkit.Suite.Extended.Toast.KryptonToastNotificationContentAreaType.WRAPPEDLABEL;
+            this.kryptonToastNotificationManager2.DismissText = "&Dismiss";
+            this.kryptonToastNotificationManager2.IconType = Krypton.Toolkit.Suite.Extended.Toast.KryptonToastNotificationIconType.None;
+            this.kryptonToastNotificationManager2.RightToLeftSupport = Krypton.Toolkit.Suite.Extended.Toast.KryptonToastNotificationRightToLeftSupport.LEFTTORIGHT;
+            this.kryptonToastNotificationManager2.UserResponsePromptAlignHorizontal = Krypton.Toolkit.PaletteRelativeAlign.Center;
+            this.kryptonToastNotificationManager2.UserResponsePromptAlignVertical = Krypton.Toolkit.PaletteRelativeAlign.Center;
+            this.kryptonToastNotificationManager2.UserResponsePromptColour = System.Drawing.Color.Gray;
+            this.kryptonToastNotificationManager2.UserResponsePromptFont = new System.Drawing.Font("Segoe UI", 8.25F);
+            // 
+            // kryptonToastNotificationManager3
+            // 
+            this.kryptonToastNotificationManager3.Action = Krypton.Toolkit.Suite.Extended.Notifications.ActionType.DEFAULT;
+            this.kryptonToastNotificationManager3.ActionButtonText = null;
+            this.kryptonToastNotificationManager3.BorderColourOne = System.Drawing.Color.Empty;
+            this.kryptonToastNotificationManager3.BorderColourTwo = System.Drawing.Color.Empty;
+            this.kryptonToastNotificationManager3.ContentText = null;
+            this.kryptonToastNotificationManager3.CornerRadius = 0;
+            this.kryptonToastNotificationManager3.CustomIconImage = null;
+            this.kryptonToastNotificationManager3.DismissButtonText = null;
+            this.kryptonToastNotificationManager3.HeaderText = null;
+            this.kryptonToastNotificationManager3.IconType = Krypton.Toolkit.Suite.Extended.Notifications.IconType.CUSTOM;
+            this.kryptonToastNotificationManager3.ProcessPath = null;
+            this.kryptonToastNotificationManager3.Seconds = 0;
+            this.kryptonToastNotificationManager3.ShowActionButton = false;
+            this.kryptonToastNotificationManager3.ShowControlBox = false;
+            this.kryptonToastNotificationManager3.ShowSubScript = false;
+            this.kryptonToastNotificationManager3.ShowTimeOutProgress = false;
+            this.kryptonToastNotificationManager3.SoundPath = null;
+            this.kryptonToastNotificationManager3.SoundStream = null;
+            this.kryptonToastNotificationManager3.TimeOutProgress = 0;
+            // 
+            // kryptonToastNotificationPopup2
+            // 
+            this.kryptonToastNotificationPopup2.ContentFont = new System.Drawing.Font("Tahoma", 8F);
+            this.kryptonToastNotificationPopup2.ContentText = null;
+            this.kryptonToastNotificationPopup2.Image = null;
+            this.kryptonToastNotificationPopup2.IsRightToLeft = false;
+            this.kryptonToastNotificationPopup2.OptionsMenu = null;
+            this.kryptonToastNotificationPopup2.Size = new System.Drawing.Size(400, 100);
+            this.kryptonToastNotificationPopup2.TitleFont = new System.Drawing.Font("Segoe UI", 9F);
+            this.kryptonToastNotificationPopup2.TitleText = null;
+            // 
             // ToastNotificationExample
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -80,5 +127,8 @@
 
         private Krypton.Toolkit.Suite.Extended.Notifications.KryptonToastNotificationManager kryptonToastNotificationManager1;
         private Krypton.Toolkit.Suite.Extended.Notifications.KryptonToastNotificationPopup kryptonToastNotificationPopup1;
+        private Krypton.Toolkit.Suite.Extended.Toast.KryptonToastNotificationManager kryptonToastNotificationManager2;
+        private Krypton.Toolkit.Suite.Extended.Notifications.KryptonToastNotificationManager kryptonToastNotificationManager3;
+        private Krypton.Toolkit.Suite.Extended.Notifications.KryptonToastNotificationPopup kryptonToastNotificationPopup2;
     }
 }
