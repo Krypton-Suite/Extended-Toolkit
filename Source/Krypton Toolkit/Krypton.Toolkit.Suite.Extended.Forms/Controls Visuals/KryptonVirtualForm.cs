0e0784056ee0d8effff2b1e72249a4df63b57d2d
Peter Wagner <peterwagner@live.co.uk>
2022-10-11 17:33:59 +0100

    * Add `KryptonVirtualForm`

---
+++ Source/Krypton Toolkit/Krypton.Toolkit.Suite.Extended.Forms/Controls Visuals/KryptonVirtualForm.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Krypton.Toolkit.Suite.Extended.Forms
+{
+    public abstract class KryptonVirtualForm : Form, IKryptonDebug
+    {
+        public void KryptonResetCounters()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int KryptonLayoutCounter { get; }
+        public int KryptonPaintCounter { get; }
+    }
+}
