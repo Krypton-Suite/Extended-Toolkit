689818c8882bcca9b7cdf8a82777bedbf5b38a3e
Peter Wagner <peterwagner@live.co.uk>
2022-11-17 16:18:52 +0000

    * Add Tooltip framewwork

--- Source/Krypton Toolkit/Krypton.Toolkit.Suite.Extended.Shared/EventArgs/ToolTipEventArgs.cs
+++ Source/Krypton Toolkit/Krypton.Toolkit.Suite.Extended.Shared/EventArgs/ToolTipEventArgs.cs
@@ -26,13 +26,41 @@
  */
 #endregion
 
-global using System;
-global using System.ComponentModel;
-global using System.Diagnostics.CodeAnalysis;
-global using System.Drawing;
-global using System.Diagnostics;
-global using System.Reflection;
-global using System.Runtime.InteropServices;
-global using System.Runtime.Versioning;
-global using System.Text;
-global using System.Windows.Forms;
+namespace Krypton.Toolkit.Suite.Extended.Shared
+{
+    /// <summary>Details for a tooltip related event.</summary>
+    public class ToolTipEventArgs : EventArgs
+    {
+        #region Public
+
+        /// <summary>
+        /// Gets the view element that is related to the tooltip.
+        /// </summary>
+        public ViewBase Target { get; }
+
+        /// <summary>
+        /// Gets the screen point of the mouse where tooltip is required.
+        /// </summary>
+        public Point ControlMousePosition { get; }
+
+        #endregion
+
+        #region Identity
+
+        /// <summary>
+        /// Initialize a new instance of the ToolTipEventArgs class.
+        /// </summary>
+        /// <param name="target">Reference to view element that requires tooltip.</param>
+        /// <param name="controlMousePosition">Screen location of mouse when tooltip was required.</param>
+        public ToolTipEventArgs(ViewBase target, Point controlMousePosition)
+        {
+            //Debug.Assert(target != null);
+
+            // Remember parameter details
+            Target = target;
+            ControlMousePosition = controlMousePosition;
+        }
+
+        #endregion
+    }
+}
