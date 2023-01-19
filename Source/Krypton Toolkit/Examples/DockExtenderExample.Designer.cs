80744d2d3f8f7ec4a33c8bec4434b8550c1879a1
Peter Wagner <peterwagner@live.co.uk>
2023-01-19 08:52:37 +0000

    * Redirection

---
+++ Source/Krypton Toolkit/Examples/DockExtenderExample.Designer.cs
@@ -0,0 +1,346 @@
+﻿namespace Examples
+{
+    partial class DockExtenderExample
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(DockExtenderExample));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.closeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.viewToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton2 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripLabel1 = new System.Windows.Forms.ToolStripLabel();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripComboBox1 = new System.Windows.Forms.ToolStripComboBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.kryptonPanel1 = new Krypton.Toolkit.KryptonPanel();
+            this.kryptonLabel2 = new Krypton.Toolkit.KryptonLabel();
+            this.kryptonSeparator1 = new Krypton.Toolkit.KryptonSeparator();
+            this.kryptonPanel2 = new Krypton.Toolkit.KryptonPanel();
+            this.kryptonPanel3 = new Krypton.Toolkit.KryptonPanel();
+            this.kryptonLabel1 = new Krypton.Toolkit.KryptonLabel();
+            this.kryptonSeparator2 = new Krypton.Toolkit.KryptonSeparator();
+            this.kryptonListView1 = new Krypton.Toolkit.KryptonListView();
+            this.kbtnCloseButton = new Krypton.Toolkit.KryptonCheckButton();
+            this.kryptonCheckButton1 = new Krypton.Toolkit.KryptonCheckButton();
+            this.kryptonLabel3 = new Krypton.Toolkit.KryptonLabel();
+            this.kryptonMonthCalendar1 = new Krypton.Toolkit.KryptonMonthCalendar();
+            this.menuStrip1.SuspendLayout();
+            this.toolStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.kryptonPanel1)).BeginInit();
+            this.kryptonPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.kryptonSeparator1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.kryptonPanel2)).BeginInit();
+            this.kryptonPanel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.kryptonPanel3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.kryptonSeparator2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.viewToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(876, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.closeToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "&File";
+            // 
+            // closeToolStripMenuItem
+            // 
+            this.closeToolStripMenuItem.Name = "closeToolStripMenuItem";
+            this.closeToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.closeToolStripMenuItem.Text = "C&lose";
+            // 
+            // viewToolStripMenuItem
+            // 
+            this.viewToolStripMenuItem.Name = "viewToolStripMenuItem";
+            this.viewToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.viewToolStripMenuItem.Text = "Vie&w";
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButton1,
+            this.toolStripButton2,
+            this.toolStripLabel1,
+            this.toolStripSeparator1,
+            this.toolStripComboBox1});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 24);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(876, 25);
+            this.toolStrip1.TabIndex = 1;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton1.Text = "toolStripButton1";
+            // 
+            // toolStripButton2
+            // 
+            this.toolStripButton2.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton2.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton2.Image")));
+            this.toolStripButton2.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton2.Name = "toolStripButton2";
+            this.toolStripButton2.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton2.Text = "toolStripButton2";
+            // 
+            // toolStripLabel1
+            // 
+            this.toolStripLabel1.Name = "toolStripLabel1";
+            this.toolStripLabel1.Size = new System.Drawing.Size(55, 22);
+            this.toolStripLabel1.Text = "Drag Me!";
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 25);
+            // 
+            // toolStripComboBox1
+            // 
+            this.toolStripComboBox1.Name = "toolStripComboBox1";
+            this.toolStripComboBox1.Size = new System.Drawing.Size(121, 25);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.statusStrip1.Location = new System.Drawing.Point(0, 473);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.RenderMode = System.Windows.Forms.ToolStripRenderMode.ManagerRenderMode;
+            this.statusStrip1.Size = new System.Drawing.Size(876, 22);
+            this.statusStrip1.TabIndex = 2;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // kryptonPanel1
+            // 
+            this.kryptonPanel1.Controls.Add(this.kryptonLabel3);
+            this.kryptonPanel1.Controls.Add(this.kbtnCloseButton);
+            this.kryptonPanel1.Controls.Add(this.kryptonLabel2);
+            this.kryptonPanel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.kryptonPanel1.Location = new System.Drawing.Point(0, 49);
+            this.kryptonPanel1.Name = "kryptonPanel1";
+            this.kryptonPanel1.Size = new System.Drawing.Size(247, 424);
+            this.kryptonPanel1.TabIndex = 3;
+            // 
+            // kryptonLabel2
+            // 
+            this.kryptonLabel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.kryptonLabel2.LabelStyle = Krypton.Toolkit.LabelStyle.NormalPanel;
+            this.kryptonLabel2.Location = new System.Drawing.Point(0, 0);
+            this.kryptonLabel2.Name = "kryptonLabel2";
+            this.kryptonLabel2.Size = new System.Drawing.Size(247, 20);
+            this.kryptonLabel2.TabIndex = 1;
+            this.kryptonLabel2.Values.Text = "kryptonLabel2";
+            // 
+            // kryptonSeparator1
+            // 
+            this.kryptonSeparator1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.kryptonSeparator1.Location = new System.Drawing.Point(247, 49);
+            this.kryptonSeparator1.Name = "kryptonSeparator1";
+            this.kryptonSeparator1.Size = new System.Drawing.Size(5, 424);
+            this.kryptonSeparator1.TabIndex = 4;
+            // 
+            // kryptonPanel2
+            // 
+            this.kryptonPanel2.Controls.Add(this.kryptonCheckButton1);
+            this.kryptonPanel2.Controls.Add(this.kryptonPanel3);
+            this.kryptonPanel2.Controls.Add(this.kryptonLabel1);
+            this.kryptonPanel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.kryptonPanel2.Location = new System.Drawing.Point(252, 283);
+            this.kryptonPanel2.Name = "kryptonPanel2";
+            this.kryptonPanel2.Size = new System.Drawing.Size(624, 190);
+            this.kryptonPanel2.TabIndex = 5;
+            // 
+            // kryptonPanel3
+            // 
+            this.kryptonPanel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.kryptonPanel3.Location = new System.Drawing.Point(0, 20);
+            this.kryptonPanel3.Name = "kryptonPanel3";
+            this.kryptonPanel3.Size = new System.Drawing.Size(624, 170);
+            this.kryptonPanel3.TabIndex = 4;
+            // 
+            // kryptonLabel1
+            // 
+            this.kryptonLabel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.kryptonLabel1.LabelStyle = Krypton.Toolkit.LabelStyle.NormalPanel;
+            this.kryptonLabel1.Location = new System.Drawing.Point(0, 0);
+            this.kryptonLabel1.Name = "kryptonLabel1";
+            this.kryptonLabel1.Size = new System.Drawing.Size(624, 20);
+            this.kryptonLabel1.TabIndex = 0;
+            this.kryptonLabel1.Values.Text = "kryptonLabel1";
+            // 
+            // kryptonSeparator2
+            // 
+            this.kryptonSeparator2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.kryptonSeparator2.Location = new System.Drawing.Point(252, 278);
+            this.kryptonSeparator2.Name = "kryptonSeparator2";
+            this.kryptonSeparator2.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            this.kryptonSeparator2.Size = new System.Drawing.Size(624, 5);
+            this.kryptonSeparator2.TabIndex = 6;
+            // 
+            // kryptonListView1
+            // 
+            this.kryptonListView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.kryptonListView1.FullRowSelect = true;
+            this.kryptonListView1.HideSelection = false;
+            this.kryptonListView1.ItemStyle = Krypton.Toolkit.ButtonStyle.ListItem;
+            this.kryptonListView1.Location = new System.Drawing.Point(252, 49);
+            this.kryptonListView1.Name = "kryptonListView1";
+            this.kryptonListView1.OwnerDraw = true;
+            this.kryptonListView1.Size = new System.Drawing.Size(624, 229);
+            this.kryptonListView1.StateCommon.Item.Content.ShortText.MultiLine = Krypton.Toolkit.InheritBool.True;
+            this.kryptonListView1.StateCommon.Item.Content.ShortText.MultiLineH = Krypton.Toolkit.PaletteRelativeAlign.Center;
+            this.kryptonListView1.StateCommon.Item.Content.ShortText.TextH = Krypton.Toolkit.PaletteRelativeAlign.Center;
+            this.kryptonListView1.TabIndex = 7;
+            this.kryptonListView1.View = System.Windows.Forms.View.SmallIcon;
+            // 
+            // kbtnCloseButton
+            // 
+            this.kbtnCloseButton.AutoSize = true;
+            this.kbtnCloseButton.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.kbtnCloseButton.CornerRoundingRadius = -1F;
+            this.kbtnCloseButton.CustomUACShieldSize = new System.Drawing.Size(16, 16);
+            this.kbtnCloseButton.Location = new System.Drawing.Point(225, 1);
+            this.kbtnCloseButton.Name = "kbtnCloseButton";
+            this.kbtnCloseButton.Size = new System.Drawing.Size(16, 22);
+            this.kbtnCloseButton.TabIndex = 8;
+            this.kbtnCloseButton.UACShieldIconSize = Krypton.Toolkit.UACShieldIconSize.ExtraSmall;
+            this.kbtnCloseButton.Values.Text = "X";
+            // 
+            // kryptonCheckButton1
+            // 
+            this.kryptonCheckButton1.AutoSize = true;
+            this.kryptonCheckButton1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.kryptonCheckButton1.CornerRoundingRadius = -1F;
+            this.kryptonCheckButton1.CustomUACShieldSize = new System.Drawing.Size(16, 16);
+            this.kryptonCheckButton1.Location = new System.Drawing.Point(605, -1);
+            this.kryptonCheckButton1.Name = "kryptonCheckButton1";
+            this.kryptonCheckButton1.Size = new System.Drawing.Size(16, 22);
+            this.kryptonCheckButton1.TabIndex = 9;
+            this.kryptonCheckButton1.UACShieldIconSize = Krypton.Toolkit.UACShieldIconSize.ExtraSmall;
+            this.kryptonCheckButton1.Values.Text = "X";
+            // 
+            // kryptonLabel3
+            // 
+            this.kryptonLabel3.AutoSize = false;
+            this.kryptonLabel3.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.kryptonLabel3.LabelStyle = Krypton.Toolkit.LabelStyle.NormalPanel;
+            this.kryptonLabel3.Location = new System.Drawing.Point(0, 287);
+            this.kryptonLabel3.Name = "kryptonLabel3";
+            this.kryptonLabel3.Size = new System.Drawing.Size(247, 137);
+            this.kryptonLabel3.TabIndex = 9;
+            this.kryptonLabel3.Values.Text = "kryptonLabel3";
+            // 
+            // kryptonMonthCalendar1
+            // 
+            this.kryptonMonthCalendar1.Location = new System.Drawing.Point(316, 193);
+            this.kryptonMonthCalendar1.Name = "kryptonMonthCalendar1";
+            this.kryptonMonthCalendar1.Size = new System.Drawing.Size(230, 182);
+            this.kryptonMonthCalendar1.TabIndex = 10;
+            // 
+            // DockExtenderExample
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(876, 495);
+            this.Controls.Add(this.kryptonMonthCalendar1);
+            this.Controls.Add(this.kryptonListView1);
+            this.Controls.Add(this.kryptonSeparator2);
+            this.Controls.Add(this.kryptonPanel2);
+            this.Controls.Add(this.kryptonSeparator1);
+            this.Controls.Add(this.kryptonPanel1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "DockExtenderExample";
+            this.Text = "Dock Extender Example";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.kryptonPanel1)).EndInit();
+            this.kryptonPanel1.ResumeLayout(false);
+            this.kryptonPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.kryptonSeparator1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.kryptonPanel2)).EndInit();
+            this.kryptonPanel2.ResumeLayout(false);
+            this.kryptonPanel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.kryptonPanel3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.kryptonSeparator2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem fileToolStripMenuItem;
+        private ToolStripMenuItem closeToolStripMenuItem;
+        private ToolStripMenuItem viewToolStripMenuItem;
+        private ToolStrip toolStrip1;
+        private ToolStripButton toolStripButton1;
+        private ToolStripButton toolStripButton2;
+        private ToolStripLabel toolStripLabel1;
+        private ToolStripSeparator toolStripSeparator1;
+        private ToolStripComboBox toolStripComboBox1;
+        private StatusStrip statusStrip1;
+        private KryptonPanel kryptonPanel1;
+        private KryptonSeparator kryptonSeparator1;
+        private KryptonPanel kryptonPanel2;
+        private KryptonSeparator kryptonSeparator2;
+        private KryptonListView kryptonListView1;
+        private KryptonLabel kryptonLabel1;
+        private KryptonLabel kryptonLabel2;
+        private KryptonPanel kryptonPanel3;
+        private KryptonCheckButton kbtnCloseButton;
+        private KryptonCheckButton kryptonCheckButton1;
+        private KryptonLabel kryptonLabel3;
+        private KryptonMonthCalendar kryptonMonthCalendar1;
+    }
+}
---
+++ Source/Krypton Toolkit/Krypton.Toolkit.Suite.Extended.Dock.Extender/Controls Toolkit/KryptonOverlayForm.cs
@@ -0,0 +1,18 @@
+﻿namespace Krypton.Toolkit.Suite.Extended.Dock.Extender
+{
+    public partial class KryptonOverlayForm : KryptonForm
+    {
+        #region Public
+
+        public new DockStyle Dock;
+
+        public Control DockHostControl;
+
+        #endregion
+
+        public KryptonOverlayForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
