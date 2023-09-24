53c386c9cc7057fed4992edb28c8667a68fcdbbc
Peter Wagner <peterwagner@live.co.uk>
2023-09-24 12:28:04 +0100

    Update KryptonMessageBoxExtendedForm.cs

--- Source/Krypton Toolkit/Krypton.Toolkit.Suite.Extended.Messagebox/Controls Visuals/KryptonMessageBoxExtendedForm.cs
+++ Source/Krypton Toolkit/Krypton.Toolkit.Suite.Extended.Messagebox/Controls Visuals/KryptonMessageBoxExtendedForm.cs
@@ -216,7 +216,7 @@ namespace Krypton.Toolkit.Suite.Extended.Messagebox
 
             using (_timeOutTimer)
             {
-                _result = KryptonMessageBoxExtended.Show(text, caption, buttons, icon, null, null);
+                _result = KryptonMessageBoxExtended.Show(text, caption, buttons, icon, null, null, null);
             }
 
             if (_timedOut)
