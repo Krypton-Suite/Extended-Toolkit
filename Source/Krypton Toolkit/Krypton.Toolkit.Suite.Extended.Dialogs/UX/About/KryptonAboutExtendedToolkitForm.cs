4ab61a537bb15c8e0ca0e872f108c66f0df81e6b
Peter Wagner <peterwagner@live.co.uk>
2023-11-30 17:52:27 +0000

    * Data

--- Source/Krypton Toolkit/Krypton.Toolkit.Suite.Extended.Dialogs/UX/About/KryptonAboutExtendedToolkitForm.cs
+++ Source/Krypton Toolkit/Krypton.Toolkit.Suite.Extended.Dialogs/UX/About/KryptonAboutExtendedToolkitForm.cs
@@ -1,20 +1,100 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+﻿#region MIT License
+/*
+ * MIT License
+ *
+ * Copyright (c) 2017 - 2023 Krypton Suite
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
+#endregion
 
 namespace Krypton.Toolkit.Suite.Extended.Dialogs
 {
-    public partial class KryptonAboutExtendedToolkitForm : KryptonForm
+    internal partial class KryptonAboutExtendedToolkitForm : KryptonForm
     {
-        public KryptonAboutExtendedToolkitForm()
+        #region Instance Fields
+
+        private readonly KryptonAboutExtendedToolkitData _aboutExtendedToolkitData;
+
+        #endregion
+
+        public KryptonAboutExtendedToolkitForm(KryptonAboutExtendedToolkitData aboutExtendedToolkitData)
         {
+            _aboutExtendedToolkitData = aboutExtendedToolkitData;
+
             InitializeComponent();
+        }
+
+        private void klwlblDemos_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+
+        }
+
+        private void klwlblDocumentation_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+
+        }
+
+        private void klwlblRepositories_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+
+        }
+
+        private void klwlblDiscord_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+
+        }
+
+        private void klwlblGeneralInformation_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+
+        }
+
+        private void tsbtnToolkitGeneralInformation_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tsbtnDiscord_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tsbtnDeveloperInformation_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tsbtnVersions_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void kbtnSystemInformation_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void kbtnOk_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
