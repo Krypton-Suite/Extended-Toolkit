a10072472a96892b2ff73dfb0e041f4b92b8134e
Peter Wagner <peterwagner@live.co.uk>
2024-07-13 11:31:32 +0100

    * Correct years

--- Source/Krypton Toolkit/Krypton.Toolkit.Suite.Extended.ToastNotification/Controls Visuals/Basic/LTR/VisualToastNotificationBasicForm.cs
+++ Source/Krypton Toolkit/Krypton.Toolkit.Suite.Extended.ToastNotification/Controls Visuals/Basic/LTR/VisualToastNotificationBasicForm.cs
@@ -3,7 +3,7 @@
 /*
  * MIT License
  *
- * Copyright (c) 2017 - 2024 Krypton Suite
+ * Copyright (c) 2024 - 2024 Krypton Suite
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
