e50103b15e7505bc828b0dbd90c2200971fb263f
Peter Wagner <peterwagner@live.co.uk>
2026-04-19 17:36:39 +0100

    Update GanttChartExample.cs

--- Source/Krypton Toolkit/Examples/GanttChartExample.cs
+++ Source/Krypton Toolkit/Examples/GanttChartExample.cs
@@ -3,7 +3,7 @@
 /*
  * MIT License
  *
- * Copyright (c) 2017 - 2026 Krypton Suite
+ * Copyright (c) 2026 - 2026 Krypton Suite
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
